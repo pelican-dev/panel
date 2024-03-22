0c5b449c4e6451f8223abd71afe6888f2982c1af
Lance Pioch <git@lance.sh>
2024-03-22 18:02:07 -0400

    Fix password resets

--- config/auth.php
+++ config/auth.php
@@ -14,4 +14,13 @@ return [
         ],
     ],
 
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+    ],
+
 ];
