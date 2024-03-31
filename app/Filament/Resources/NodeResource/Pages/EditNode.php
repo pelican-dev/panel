766c6c08f48b0d651215e38a4e993c4ccc4446eb
Lance Pioch <git@lance.sh>
2024-03-31 01:39:24 -0400

    Easy to copy and paste

--- app/Filament/Resources/NodeResource/Pages/EditNode.php
+++ app/Filament/Resources/NodeResource/Pages/EditNode.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Resources\NodeResource\Pages;
 
 use App\Filament\Resources\NodeResource;
+use App\Models\Node;
 use Filament\Actions;
 use Filament\Forms;
 use Filament\Forms\Components\Wizard;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\HtmlString;
+use Webbingbrasil\FilamentCopyActions\Forms\Actions\CopyAction;
 
 class EditNode extends EditRecord
 {
@@ -23,7 +26,17 @@ class EditNode extends EditRecord
                     Forms\Components\Wizard\Step::make('Configuration')
                         ->description('')
                         ->schema([
-
+                            Forms\Components\Placeholder::make('instructions')
+                                ->columnSpanFull()
+                                ->content(new HtmlString('
+                                  This file should be placed in your daemon\'s root directory (usually <code>/etc/pelican</code>) in a file called <code>config.yml</code>.
+                            ')),
+                            Forms\Components\Textarea::make('config')
+                                ->label('Configuration File')
+                                ->disabled()
+                                ->rows(19)
+                                ->hintAction(CopyAction::make())
+                                ->columnSpanFull(),
                         ]),
                 ])
                     ->columns(4)
@@ -34,6 +47,15 @@ class EditNode extends EditRecord
                     // ->submitAction($this->getSubmitFormAction())
                     // ->skippable($this->hasSkippableSteps()),
             ]);
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $node = Node::findOrFail($data['id']);
+
+        $data['config'] = $node->getYamlConfiguration();
+
+        return $data;
     }
 
     protected function getSteps(): array
