f660611ed3bcec816f42c3240e164a45495662b9
Lance Pioch <git@lance.sh>
2024-04-10 17:10:25 -0400

    Add skeleton new page

---
+++ app/Filament/Pages/Introduction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Introduction extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.introduction';
+}
