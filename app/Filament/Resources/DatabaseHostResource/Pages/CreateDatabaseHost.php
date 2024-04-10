195557373c1a99a2e22267d579ae112b4b92c4d2
Lance Pioch <git@lance.sh>
2024-04-09 21:01:30 -0400

    Clarify the resource name

--- app/Filament/Resources/DatabaseHostResource/Pages/CreateDatabaseHost.php
+++ app/Filament/Resources/DatabaseHostResource/Pages/CreateDatabaseHost.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDatabaseHost extends CreateRecord
 {
     protected static string $resource = DatabaseHostResource::class;
+
+    protected ?string $heading = 'Database Hosts';
+
+    protected ?string $subheading = '(database servers that can have individual databases)';
 }
