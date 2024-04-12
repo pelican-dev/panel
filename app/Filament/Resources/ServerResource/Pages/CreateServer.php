695ebd35a47932af45fb4905f1ea46b4450c4a0e
Lance Pioch <git@lance.sh>
2024-04-12 02:03:36 -0400

    No another create

--- app/Filament/Resources/ServerResource/Pages/CreateServer.php
+++ app/Filament/Resources/ServerResource/Pages/CreateServer.php
@@ -9,4 +9,5 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateServer extends CreateRecord
 {
     protected static string $resource = ServerResource::class;
+    protected static bool $canCreateAnother = false;
 }
