1421d9bc099ee690ca714e01c956f28ba29ca16b
Turkidev <114973795+Turkidev@users.noreply.github.com>
2024-04-12 22:39:29 +0000

    fix the file name

--- lang/en/admin/node.php
+++ lang/en/admin/node.php
@@ -2,14 +2,14 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'النطاق (FQDN) أو عنوان IP المقدم لا يُحل إلى عنوان IP صالح.',
-        'fqdn_required_for_ssl' => 'يتطلب اسم نطاق كامل يُحل إلى عنوان IP عام لاستخدام SSL لهذه العقدة.',
+        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
+        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
     ],
     'notices' => [
-        'allocations_added' => 'تم إضافة التخصيصات بنجاح إلى هذه العقدة.',
-        'node_deleted' => 'تم إزالة العقدة بنجاح من اللوحة.',
-        'node_created' => 'تم إنشاء عقدة جديدة بنجاح. يمكنك تكوين الدايمون تلقائيًا على هذه الآلة بزيارة علامة التبويب "التكوين". <strong>قبل أن تتمكن من إضافة أي خوادم يجب عليك أولاً تخصيص عنوان IP واحد على الأقل ومنفذ.</strong>',
-        'node_updated' => 'تم تحديث معلومات العقدة. إذا تم تغيير أي إعدادات دايمون ستحتاج إلى إعادة تشغيله لكي تصبح هذه التغييرات فعالة.',
-        'unallocated_deleted' => 'تم حذف جميع المنافذ غير المخصصة لـ <code>:ip</code>.',
+        'allocations_added' => 'Allocations have successfully been added to this node.',
+        'node_deleted' => 'Node has been successfully removed from the panel.',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
+        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
+        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
     ],
 ];
