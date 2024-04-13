774687255e3724a5ddfa23ffa8a43dac3763eb8b
MikkelHebel <mikkelhhebel@gmail.com>
2024-04-13 21:18:53 +0200

    Translated account.php and index.php

---
+++ lang/da/dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'search' => 'Søg efter servere...',
+    'no_matches' => 'Der blev ikke fundet nogen servere, der matcher de angivne søgekriterier.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Hukommelse',
+];
