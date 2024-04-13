2512281c5c0edc6fd6233f2743b44affcfde22dc
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-04-13 12:57:12 +0200

    Update eggs.php

--- lang/nl/admin/eggs.php
+++ lang/nl/admin/eggs.php
@@ -3,18 +3,17 @@
 return [
     'notices' => [
         'imported' => 'Het importeren van dit ei en de bijbehorende variabelen is geslaagd.',
-        'updated_via_import' => 'This Egg has been updated using the file provided.',
-        'deleted' => 'Successfully deleted the requested egg from the Panel.',
-        'updated' => 'Egg configuration has been updated successfully.',
-        'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-        'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+        'updated_via_import' => 'Dit Egg is bijgewerkt met behulp van het opgegeven bestand.',
+        'deleted' => 'Het gevraagde Egg is met succes uit het paneel verwijderd.',
+        'updated' => 'Egg configuratie is met succes bijgewerkt.',
+        'script_updated' => 'Egg install script is bijgewerkt en wordt uitgevoerd wanneer er servers worden geïnstalleerd.',
+        'egg_created' => 'Een nieuw Egg is met succes geplaatst. U moet elke lopende daemons opnieuw opstarten om dit nieuwe ei toe te passen.',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'De variabele ":variable" is verwijderd en zal niet meer beschikbaar zijn voor servers na rebuilt.',
+            'variable_updated' => 'De variabele ":variable" is bijgewerkt. Je moet elke server opnieuw opbouwen met deze variabele om wijzigingen toe te passen.',
+            'variable_created' => 'Er is een nieuwe variabele aangemaakt en toegewezen aan deze Egg.',
         ],
     ],
 ];
-
