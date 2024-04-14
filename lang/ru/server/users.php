c1ef48835f616a4d7e93a22410b5cf20f5851e41
Lance Pioch <lancepioch@gmail.com>
2024-04-14 17:41:00 -0400

    New translations users.php (Russian)

--- lang/ru/server/users.php
+++ lang/ru/server/users.php
@@ -21,7 +21,7 @@ return [
         'allocation_read' => 'Allows access to the server allocation management pages.',
         'allocation_update' => 'Allows user permission to make modifications to the server\'s allocations.',
         'database_create' => 'Allows user permission to create a new database for the server.',
-        'database_read' => 'Allows user permission to view the server databases.',
+        'database_read' => 'Разрешает пользователю просматривать базы данных сервера.',
         'database_update' => 'Allows a user permission to make modifications to a database. If the user does not have the "View Password" permission as well they will not be able to modify the password.',
         'database_delete' => 'Allows a user permission to delete a database instance.',
         'database_view_password' => 'Allows a user permission to view a database password in the system.',
