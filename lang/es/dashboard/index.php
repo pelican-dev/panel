5280153526a0fe19aac84274616e77f79f16ffbf
Lance Pioch <lancepioch@gmail.com>
2024-04-14 14:40:08 -0400

    New translations index.php (Spanish)

--- lang/es/dashboard/index.php
+++ lang/es/dashboard/index.php
@@ -3,6 +3,6 @@
 return [
     'search' => 'Buscar servidores...',
     'no_matches' => 'No se encontraron servidores que coincidan con los criterios de búsqueda proporcionados.',
-    'cpu_title' => 'CPU',
+    'cpu_title' => 'UPC',
     'memory_title' => 'Memoria',
 ];
