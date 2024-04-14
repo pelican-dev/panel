2cb7bafff2713f19d0a0053d362f79fe6cc7e264
Lance Pioch <lancepioch@gmail.com>
2024-04-14 15:34:43 -0400

    New translations passwords.php (German)

--- lang/de/passwords.php
+++ lang/de/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwörter müssen mindestens sechs Zeichen lang sein und mit der Bestätigung übereinstimmen.',
-    'reset' => 'Ihr Passwort wurde zurückgesetzt!',
-    'sent' => 'Wir haben Ihren Link zum Zurücksetzen des Passworts per E-Mail gesendet!',
+    'password' => 'Passwörter müssen aus mindestens 6 Zeichen bestehen und übereinstimmen.',
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir einen Link zum Zurücksetzen des Passworts per E-Mail zugesendet!',
     'token' => 'Das Passwort Reset Token ist ungültig.',
     'user' => "Es konnte kein Benutzer mit dieser E-Mail-Adresse gefunden werden.",
 ];
