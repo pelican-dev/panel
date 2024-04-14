e9852e67d061711ca65db9a273594dbcef43f6ea
Lance Pioch <lancepioch@gmail.com>
2024-04-14 15:35:02 -0400

    New translations user.php (German)

--- lang/de/admin/user.php
+++ lang/de/admin/user.php
@@ -2,8 +2,8 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Ein Benutzer mit aktiven Servern, die mit seinem Konto verbunden sind, kann nicht gelöscht werden. Bitte löschen Sie seine Server, bevor Sie fortfahren.',
-        'user_is_self' => 'Ihr eigenes Benutzerkonto kann nicht gelöscht werden.',
+        'user_has_servers' => 'Ein Benutzer mit aktiven Servern, die mit seinem Konto verbunden sind, kann nicht gelöscht werden. Bitte lösche seine Server, bevor du fortfährst.',
+        'user_is_self' => 'Du kannst dein eigenes Benutzerkonto nicht löschen.',
     ],
     'notices' => [
         'account_created' => 'Konto wurde erfolgreich erstellt.',
