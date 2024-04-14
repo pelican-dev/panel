c45f4892e1e2679f5b2cbc18cf09e404223da1eb
Lance Pioch <lancepioch@gmail.com>
2024-04-14 17:40:54 -0400

    New translations eggs.php (Russian)

--- lang/ru/admin/eggs.php
+++ lang/ru/admin/eggs.php
@@ -5,7 +5,7 @@ return [
         'imported' => 'Successfully imported this Egg and its associated variables.',
         'updated_via_import' => 'This Egg has been updated using the file provided.',
         'deleted' => 'Successfully deleted the requested egg from the Panel.',
-        'updated' => 'Egg configuration has been updated successfully.',
+        'updated' => 'Конфигурация яйца успешно изменена.',
         'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
         'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
     ],
