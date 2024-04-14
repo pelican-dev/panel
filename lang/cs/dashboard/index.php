a65aac556ee4432bf1a4f7cec8cc6d9cb6e8e744
Lance Pioch <lancepioch@gmail.com>
2024-04-14 14:40:11 -0400

    New translations index.php (Czech)

---
+++ lang/cs/dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'search' => 'Vyhledat servery...',
+    'no_matches' => 'Nebyly nalezeny žádné servery, které odpovídají zadaným kritériím.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Paměť',
+];
