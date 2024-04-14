d2ada3940dc8945f14d0b67e5b8d258a3e30b080
Lance Pioch <lancepioch@gmail.com>
2024-04-14 14:37:51 -0400

    New translations eggs.php (French)

---
+++ lang/fr/admin/eggs.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'notices' => [
+        'imported' => 'Cet Egg et ses variables ont été importés avec succès.',
+        'updated_via_import' => 'Cet Oeuf a été mis à jour en utilisant le fichier fourni.',
+        'deleted' => 'L\'Egg demandé a été supprimé du panneau.',
+        'updated' => 'La configuration de l\'oeuf a été mise à jour avec succès.',
+        'script_updated' => 'Le script d\'installation d\'oeuf a été mis à jour et s\'exécutera chaque fois que les serveurs sont installés.',
+        'egg_created' => 'Un nouvel œuf a été pondu avec succès. Vous devrez redémarrer tous les daemons en cours d\'exécution pour appliquer ce nouvel œuf.',
+    ],
+    'variables' => [
+        'notices' => [
+            'variable_deleted' => 'La variable ":variable" a été supprimée et ne sera plus disponible pour les serveurs une fois reconstruits.',
+            'variable_updated' => 'La variable ":variable" a été mise à jour. Vous devrez reconstruire tous les serveurs utilisant cette variable pour appliquer les modifications.',
+            'variable_created' => 'La nouvelle variable a été créée et affectée à cet œuf.',
+        ],
+    ],
+];
