8f032c9e05a1defa255e16db8c431db10c92c1f7
Lance Pioch <lancepioch@gmail.com>
2024-04-14 20:15:18 -0400

    New translations passwords.php (Japanese)

--- lang/ja/passwords.php
+++ lang/ja/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'パスワードは少なくとも 6 文字で、確認欄のものと一致しなければいけません。',
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'パスワードリセットのためのリンクをメールで送信しました。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "そのメールアドレスのユーザーは見つかりません。",
 ];
