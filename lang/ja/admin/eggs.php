66c6c5a4aa51546ec16dc1c604930b9f984e38a6
Lance Pioch <lancepioch@gmail.com>
2024-04-14 20:15:22 -0400

    New translations eggs.php (Japanese)

--- lang/ja/admin/eggs.php
+++ lang/ja/admin/eggs.php
@@ -2,18 +2,18 @@
 
 return [
     'notices' => [
-        'imported' => 'Successfully imported this Egg and its associated variables.',
-        'updated_via_import' => 'This Egg has been updated using the file provided.',
-        'deleted' => 'Successfully deleted the requested egg from the Panel.',
-        'updated' => 'Egg configuration has been updated successfully.',
-        'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-        'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+        'imported' => 'この卵と関連する変数を正常にインポートしました。',
+        'updated_via_import' => '指定されたファイルを使用してこの卵を更新しました。',
+        'deleted' => '要求された卵をパネルから削除しました。',
+        'updated' => '卵の設定が更新されました。',
+        'script_updated' => 'Eggのインストールスクリプトが更新され、サーバーがインストールされるたびに実行されます。',
+        'egg_created' => '新しい卵が生成されました。この新しい卵を適用するには、実行中のデーモンを再起動する必要があります。',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => '変数 ":variable" が削除され、再構築されると、サーバーで使用できなくなります。',
+            'variable_updated' => '変数 ":variable" が更新されました。変更を適用するには、この変数を使用しているサーバーを再構築する必要があります。',
+            'variable_created' => '新しい変数が作成され、この卵に割り当てられました。',
         ],
     ],
 ];
