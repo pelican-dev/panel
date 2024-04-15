bd3a762cd4f6f3b572ef35f0b2efb3740f779379
Lance Pioch <lancepioch@gmail.com>
2024-04-15 05:33:40 -0400

    New translations node.php (Turkish)

--- lang/tr/admin/node.php
+++ lang/tr/admin/node.php
@@ -2,11 +2,11 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'Sağlanan FQDN veya IP adresi geçerli bir IP adresine çözümlenmiyor.',
+        'fqdn_required_for_ssl' => 'Bu düğüm için SSL kullanmak amacıyla genel bir IP adresine çözümlenen tam nitelikli bir alan adı gereklidir.',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
+        'allocations_added' => 'Tahsisler bu node\'a başarıyla eklendi.',
         'node_deleted' => 'Node has been successfully removed from the panel.',
         'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
         'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
