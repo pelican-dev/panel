18dfb7ff024fd527085d48e511c9f5139901f413
Lance Pioch <lancepioch@gmail.com>
2024-04-15 08:42:52 -0400

    New translations eggs.php (Polish)

--- lang/pl/admin/eggs.php
+++ lang/pl/admin/eggs.php
@@ -3,11 +3,11 @@
 return [
     'notices' => [
         'imported' => 'Pomyślnie zaimportowano to jądro i związane z nim zmienne.',
-        'updated_via_import' => 'To jajko zostało zaktualizowane przy użyciu dostarczonego pliku.',
-        'deleted' => 'Pomyślnie usunięto żądane jajko z Panelu.',
-        'updated' => 'Konfiguracja jajka została pomyślnie zaktualizowana.',
-        'script_updated' => 'Skrypt instalacyjny jajka został zaktualizowany i zostanie uruchomiony za każdym razem, gdy serwery zostaną zainstalowane.',
-        'egg_created' => 'Nowe jajkozostało dodane. Musisz zrestartować wszystkie uruchomione Daemon\'y, aby zastosować nowe jądro.',
+        'updated_via_import' => 'To jądro zostało zaktualizowane przy użyciu dostarczonego pliku.',
+        'deleted' => 'Pomyślnie usunięto żądane jądro z Panelu.',
+        'updated' => 'Konfiguracja jądra została pomyślnie zaktualizowana.',
+        'script_updated' => 'Skrypt instalacyjny jądra został zaktualizowany i zostanie uruchomiony za każdym razem, gdy serwery zostaną zainstalowane.',
+        'egg_created' => 'Nowe jądro zostało dodane. Musisz zrestartować wszystkie uruchomione Daemon\'y, aby zastosować nowe jądro.',
     ],
     'variables' => [
         'notices' => [
