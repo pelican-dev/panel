8d417970a2f8a69c863916fd7ce73b9e2dca41bd
Lance Pioch <lancepioch@gmail.com>
2024-04-16 09:06:13 -0400

    New translations passwords.php (Hebrew)

--- lang/he/passwords.php
+++ lang/he/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'סיסמאות חייבות להיות לפחות שישה תווים ולהתאים לאישור.',
+    'reset' => 'הסיסמה שלך אופסה!',
+    'sent' => 'שלחנו באימייל קישור לאיפוס הסיסמה שלך!',
+    'token' => 'טוקן איפוס סיסמה זה אינו חוקי.',
+    'user' => "אנחנו לא יכולים למצוא משתמש עם כתובת האימייל הזו.",
 ];
