b801194f639f6b86f894826d6c4ce2c49cc40cd5
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:05 -0400

    New translations index.php (Hebrew)

--- lang/he/dashboard/index.php
+++ lang/he/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'חפש שרתים...',
-    'no_matches' => 'לא נמצאו שרתים התואמים לקריטריוני החיפוש שסופקו.',
-    'cpu_title' => 'מעבד',
-    'memory_title' => 'זיכרון',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Memory',
 ];
