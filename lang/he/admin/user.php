e2588a4e178e014b196698e91834ebb78b7f8d9e
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:11:56 -0400

    New translations user.php (Hebrew)

--- lang/he/admin/user.php
+++ lang/he/admin/user.php
@@ -2,11 +2,11 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'לא ניתן למחוק משתמש עם שרתים פעילים המחוברים לחשבון שלו. נא למחוק את השרתים שלהם לפני שתמשיך.',
-        'user_is_self' => 'לא ניתן למחוק את חשבון המשתמש שלך.',
+        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_is_self' => 'Cannot delete your own user account.',
     ],
     'notices' => [
-        'account_created' => 'החשבון נוצר בהצלחה.',
-        'account_updated' => 'החשבון עודכן בהצלחה.',
+        'account_created' => 'Account has been created successfully.',
+        'account_updated' => 'Account has been successfully updated.',
     ],
 ];
