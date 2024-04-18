044a7856d3b724fc16e6eb995964c1e9fadfbc6a
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:10:46 -0400

    New translations eggs.php (German)

--- lang/de/admin/eggs.php
+++ lang/de/admin/eggs.php
@@ -2,18 +2,18 @@
 
 return [
     'notices' => [
-        'imported' => 'Das Egg und die zugehörigen Variablen wurden erfolgreich importiert.',
-        'updated_via_import' => 'Dieses Egg wurde mit der angegebenen Datei aktualisiert.',
-        'deleted' => 'Das angeforderte Egg wurde erfolgreich aus dem Panel gelöscht.',
-        'updated' => 'Egg Konfiguration wurde erfolgreich aktualisiert.',
-        'script_updated' => 'Das Egg-Installationsskript wurde aktualisiert und wird bei der Installation von Servern ausgeführt.',
-        'egg_created' => 'Ein neues Egg wurde erfolgreich erstellt.',
+        'imported' => 'Successfully imported this Egg and its associated variables.',
+        'updated_via_import' => 'This Egg has been updated using the file provided.',
+        'deleted' => 'Successfully deleted the requested egg from the Panel.',
+        'updated' => 'Egg configuration has been updated successfully.',
+        'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
+        'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'Die Variable ":variable" wurde gelöscht und wird nach einem Serverneustart nicht mehr verfügbar sein.',
-            'variable_updated' => 'Die Variable ":variable" wurde aktualisiert. Du musst alle Server neustarten, die diese Variable verwenden, um die Änderungen zu übernehmen.',
-            'variable_created' => 'Neue Variable wurde erfolgreich erstellt und diesem Egg zugewiesen.',
+            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
+            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
+            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
         ],
     ],
 ];
