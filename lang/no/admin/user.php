8926139609e5661e4ee25c2dd6ab77ec14f03fd3
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:12:01 -0400

    New translations user.php (Norwegian)

--- lang/no/admin/user.php
+++ lang/no/admin/user.php
@@ -3,10 +3,10 @@
 return [
     'exceptions' => [
         'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
-        'user_is_self' => 'Kan ikke slette din egen brukerkonto.',
+        'user_is_self' => 'Cannot delete your own user account.',
     ],
     'notices' => [
-        'account_created' => 'Kontoen er opprettet.',
-        'account_updated' => 'Kontoen har blitt oppdatert.',
+        'account_created' => 'Account has been created successfully.',
+        'account_updated' => 'Account has been successfully updated.',
     ],
 ];
