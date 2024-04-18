26945247fc97bcf32169707d911c6eb8aabe5c4f
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:11 -0400

    New translations index.php (Polish)

--- lang/pl/dashboard/index.php
+++ lang/pl/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Wyszukaj serwery...',
-    'no_matches' => 'Nie znaleziono żadnych serwerów spełniających podane kryteria wyszukiwania.',
-    'cpu_title' => 'Procesor',
-    'memory_title' => 'Pamięć',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Memory',
 ];
