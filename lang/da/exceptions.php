c5f28c2ca324c4554ad441581998660fbc01cfcf
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:08:53 -0400

    New translations exceptions.php (Danish)

--- lang/da/exceptions.php
+++ lang/da/exceptions.php
@@ -1,55 +1,55 @@
 <?php
 
 return [
-    'daemon_connection_failed' => 'Der opstod en fejl under forsøget på at kommunikere med daemonen, hvilket resulterede i en HTTP/:code responskode. Denne fejl er blevet logget.',
+    'daemon_connection_failed' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged.',
     'node' => [
-        'servers_attached' => 'En node må ikke have nogen servere tilknyttet for at kunne slettes.',
-        'daemon_off_config_updated' => 'Daemon konfiguration <strong>er blevet opdateret</strong>, men der opstod en fejl under forsøget på automatisk at opdatere konfigurationsfilen på daemonen. Du skal manuelt opdatere konfigurationsfilen (config.yml) for at daemonen kan anvende disse ændringer.',
+        'servers_attached' => 'A node must have no servers linked to it in order to be deleted.',
+        'daemon_off_config_updated' => 'The daemon configuration <strong>has been updated</strong>, however there was an error encountered while attempting to automatically update the configuration file on the Daemon. You will need to manually update the configuration file (config.yml) for the daemon to apply these changes.',
     ],
     'allocations' => [
-        'server_using' => 'En server er i øjeblikket tildelt denne tildeling. En tildeling kan kun slettes, hvis ingen server i øjeblikket er tildelt.',
-        'too_many_ports' => 'Tilføjede af flere end 1000 porte i en enkelt række ad gangen understøttes ikke.',
-        'invalid_mapping' => 'Den angivne kortlægning for :port var ugyldig og kunne ikke behandles.',
-        'cidr_out_of_range' => 'CIDR notation tillader kun masker mellem /25 og /32.',
-        'port_out_of_range' => 'Portene i en tildeling skal være større end 1024 og mindre end eller lig med 65535.',
+        'server_using' => 'A server is currently assigned to this allocation. An allocation can only be deleted if no server is currently assigned.',
+        'too_many_ports' => 'Adding more than 1000 ports in a single range at once is not supported.',
+        'invalid_mapping' => 'The mapping provided for :port was invalid and could not be processed.',
+        'cidr_out_of_range' => 'CIDR notation only allows masks between /25 and /32.',
+        'port_out_of_range' => 'Ports in an allocation must be greater than 1024 and less than or equal to 65535.',
     ],
     'egg' => [
-        'delete_has_servers' => 'Et æg med aktive servere tilknyttet kan ikke slettes fra panelet.',
-        'invalid_copy_id' => 'Ægget valgt til kopiering af et script fra eksisterer ikke, eller kopierer et script selv.',
-        'has_children' => 'Dette æg er forælder til et eller flere andre æg. Slet disse æg, før du sletter dette æg.',
+        'delete_has_servers' => 'An Egg with active servers attached to it cannot be deleted from the Panel.',
+        'invalid_copy_id' => 'The Egg selected for copying a script from either does not exist, or is copying a script itself.',
+        'has_children' => 'This Egg is a parent to one or more other Eggs. Please delete those Eggs before deleting this Egg.',
     ],
     'variables' => [
-        'env_not_unique' => 'Environment variable :name skal være unik for dette æg.',
-        'reserved_name' => 'Environment variable :name er beskyttet og kan ikke bruges som en variabel.',
-        'bad_validation_rule' => 'Valideringsreglen ":rule" er ikke en gyldig regel for denne applikation.',
+        'env_not_unique' => 'The environment variable :name must be unique to this Egg.',
+        'reserved_name' => 'The environment variable :name is protected and cannot be assigned to a variable.',
+        'bad_validation_rule' => 'The validation rule ":rule" is not a valid rule for this application.',
     ],
     'importer' => [
-        'json_error' => 'Der skete en fejl under forsøget på at parse JSON-filen: :error.',
-        'file_error' => 'JSON filen var ikke gyldig.',
-        'invalid_json_provided' => 'JSON filen er ikke i et format, der kan genkendes.',
+        'json_error' => 'There was an error while attempting to parse the JSON file: :error.',
+        'file_error' => 'The JSON file provided was not valid.',
+        'invalid_json_provided' => 'The JSON file provided is not in a format that can be recognized.',
     ],
     'subusers' => [
-        'editing_self' => 'Ændring af din egen subbrugerkonto er ikke tilladt.',
-        'user_is_owner' => 'Du kan ikke tilføje server ejeren som en subbruger til denne server.',
-        'subuser_exists' => 'En bruger med denne e-mailadresse er allerede tildelt som en subbruger til denne server.',
+        'editing_self' => 'Editing your own subuser account is not permitted.',
+        'user_is_owner' => 'You cannot add the server owner as a subuser for this server.',
+        'subuser_exists' => 'A user with that email address is already assigned as a subuser for this server.',
     ],
     'databases' => [
-        'delete_has_databases' => 'Du kan ikke slette en database host server, der har aktive databaser tilknyttet.',
+        'delete_has_databases' => 'Cannot delete a database host server that has active databases linked to it.',
     ],
     'tasks' => [
-        'chain_interval_too_long' => 'Det maksimale interval for en kædet opgave er 15 minutter.',
+        'chain_interval_too_long' => 'The maximum interval time for a chained task is 15 minutes.',
     ],
     'locations' => [
-        'has_nodes' => 'Kan ikke slette en lokation, der har aktive noder tilknyttet.',
+        'has_nodes' => 'Cannot delete a location that has active nodes attached to it.',
     ],
     'users' => [
-        'node_revocation_failed' => 'Kunne ikke tilbagekalde nøgler på <a href=":link">Node #:node</a>. :error',
+        'node_revocation_failed' => 'Failed to revoke keys on <a href=":link">Node #:node</a>. :error',
     ],
     'deployment' => [
-        'no_viable_nodes' => 'Kunne ikke finde nogle noder, der opfylder kravene for automatisk implementering.',
-        'no_viable_allocations' => 'Ingen tildeling, der opfylder kravene for automatisk implementering, blev fundet.',
+        'no_viable_nodes' => 'No nodes satisfying the requirements specified for automatic deployment could be found.',
+        'no_viable_allocations' => 'No allocations satisfying the requirements for automatic deployment were found.',
     ],
     'api' => [
-        'resource_not_found' => 'Den anmodede ressource findes ikke på denne server.',
+        'resource_not_found' => 'The requested resource does not exist on this server.',
     ],
 ];
