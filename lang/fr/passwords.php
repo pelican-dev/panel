0d0a4a3f305e0285fb75467a167340d00debc22b
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:09:33 -0400

    New translations passwords.php (French)

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Les mots de passe doivent contenir au moins six caractères et doivent être identiques.',
-    'reset' => 'Votre mot de passe a été réinitialisé !',
-    'sent' => 'Nous avons envoyé par e-mail le lien de réinitialisation de votre mot de passe !',
-    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
-    'user' => "Nous n'avons pas trouvé d'utilisateur avec cette adresse email.",
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
 ];
