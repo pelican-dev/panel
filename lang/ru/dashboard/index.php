300add8c9fc6a717a5a0097745b4a70ef0eacf9a
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:13 -0400

    New translations index.php (Russian)

--- lang/ru/dashboard/index.php
+++ lang/ru/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Поиск серверов...',
-    'no_matches' => 'Не найдено серверов, соответствующих указанным критериям поиска.',
-    'cpu_title' => 'ЦПУ',
-    'memory_title' => 'ОЗУ',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Memory',
 ];
