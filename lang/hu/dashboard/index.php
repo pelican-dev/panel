b69dbc87755b8d84dfd4dbb2d2c2ff46c61e771a
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:06 -0400

    New translations index.php (Hungarian)

--- lang/hu/dashboard/index.php
+++ lang/hu/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Szerverek keresése...',
-    'no_matches' => 'Nem található szerver a megadott feltételekkel.',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Memória',
+    'memory_title' => 'Memory',
 ];
