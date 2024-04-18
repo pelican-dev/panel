0fdae4622d7784bbcd660faa427c5326e7e7936f
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:12:07 -0400

    New translations user.php (Turkish)

--- lang/tr/admin/user.php
+++ lang/tr/admin/user.php
@@ -2,11 +2,11 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Hesabına bağlı sunucular bulunuyor. Devam etmeden önce lütfen sunucularını silin',
-        'user_is_self' => 'Kendi kullanıcı hesabınızı silemezsiniz.',
+        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_is_self' => 'Cannot delete your own user account.',
     ],
     'notices' => [
-        'account_created' => 'Hesap başarıyla oluşturuldu.',
-        'account_updated' => 'Hesap başarıyla güncellendi.',
+        'account_created' => 'Account has been created successfully.',
+        'account_updated' => 'Account has been successfully updated.',
     ],
 ];
