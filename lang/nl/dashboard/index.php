6abf06ce1995c96657b8d5212d0e6cba3712bf48
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:09 -0400

    New translations index.php (Dutch)

--- lang/nl/dashboard/index.php
+++ lang/nl/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Zoeken naar servers...',
-    'no_matches' => 'Er zijn geen servers gevonden die voldoen aan de opgegeven zoekcriteria.',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Geheugen',
+    'memory_title' => 'Memory',
 ];
