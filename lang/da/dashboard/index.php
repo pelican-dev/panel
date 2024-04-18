a973215decad750b86cf750e5c3a097b8a4f2891
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:13:02 -0400

    New translations index.php (Danish)

--- lang/da/dashboard/index.php
+++ lang/da/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Søg efter servere...',
-    'no_matches' => 'Der blev ikke fundet nogen servere, der matcher de angivne søgekriterier.',
+    'search' => 'Search for servers...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Hukommelse',
+    'memory_title' => 'Memory',
 ];
