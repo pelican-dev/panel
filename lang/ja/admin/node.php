90be69cbffb631261df6bc9586de723c45f10b86
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:11:13 -0400

    New translations node.php (Japanese)

--- lang/ja/admin/node.php
+++ lang/ja/admin/node.php
@@ -2,14 +2,14 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => '提供された FQDN または IP アドレスは、有効な IP アドレスには解決しません。',
-        'fqdn_required_for_ssl' => 'このノードにSSLを使用するには、ドメイン名が必要です。',
+        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
+        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
     ],
     'notices' => [
-        'allocations_added' => 'このノードに割り当てを追加しました。',
-        'node_deleted' => 'ノードがパネルから削除されました。',
-        'node_created' => '正常に新しいノードを作成しました。「設定」タブでデーモンを自動的に設定できます。 <strong>サーバーを追加する前に、最初に少なくとも1つのIPアドレスとポートを割り当てる必要があります。</strong>',
-        'node_updated' => 'ノード情報が更新されました。デーモンの設定が変更された場合は、変更を反映するために再起動する必要があります。',
-        'unallocated_deleted' => '<code>:ip</code> に割り当てられていないポートをすべて削除しました。',
+        'allocations_added' => 'Allocations have successfully been added to this node.',
+        'node_deleted' => 'Node has been successfully removed from the panel.',
+        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
+        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
+        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
     ],
 ];
