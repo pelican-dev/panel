285cd5691291a45048ddabf9ebbfca0c2e81a646
Lance Pioch <lancepioch@gmail.com>
2024-04-18 17:09:01 -0400

    New translations exceptions.php (Polish)

--- lang/pl/exceptions.php
+++ lang/pl/exceptions.php
@@ -1,55 +1,55 @@
 <?php
 
 return [
-    'daemon_connection_failed' => 'Wystąpił wyjątek podczas próby komunikacji z demonem skutkujący kodem odpowiedzi HTTP/:code. Wyjątek ten został zarejestrowany.',
+    'daemon_connection_failed' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged.',
     'node' => [
-        'servers_attached' => 'Aby usunąć ten węzeł, nie możesz mieć podłączonych do niego serwerów.',
-        'daemon_off_config_updated' => 'Konfiguracja deamona <strong>została zaktualizowana</strong>, jednak wystąpił błąd podczas próby automatycznej aktualizacji pliku konfiguracyjnego deamona. Aby zastosować te zmiany, należy ręcznie zaktualizować plik konfiguracyjny (config.yml).',
+        'servers_attached' => 'A node must have no servers linked to it in order to be deleted.',
+        'daemon_off_config_updated' => 'The daemon configuration <strong>has been updated</strong>, however there was an error encountered while attempting to automatically update the configuration file on the Daemon. You will need to manually update the configuration file (config.yml) for the daemon to apply these changes.',
     ],
     'allocations' => [
-        'server_using' => 'Serwer jest obecnie przypisany do tej alokacji. Alokację można usunąć tylko wtedy, gdy żaden serwer nie jest do niej przypisany.',
-        'too_many_ports' => 'Dodawanie więcej niż 1000 portów w jednym zakresie nie jest obsługiwane.',
-        'invalid_mapping' => 'Mapowanie podane dla :port było nieprawidłowe i nie mogło zostać przetworzone.',
-        'cidr_out_of_range' => 'Notacja CIDR dopuszcza tylko maski od /25 do /32.',
-        'port_out_of_range' => 'Porty w alokacji muszą być większe niż 1024 i mniejsze lub równe 65535.',
+        'server_using' => 'A server is currently assigned to this allocation. An allocation can only be deleted if no server is currently assigned.',
+        'too_many_ports' => 'Adding more than 1000 ports in a single range at once is not supported.',
+        'invalid_mapping' => 'The mapping provided for :port was invalid and could not be processed.',
+        'cidr_out_of_range' => 'CIDR notation only allows masks between /25 and /32.',
+        'port_out_of_range' => 'Ports in an allocation must be greater than 1024 and less than or equal to 65535.',
     ],
     'egg' => [
-        'delete_has_servers' => 'Jajo z aktywnymi serwerami przypisanymi do niego nie może zostać usunięte z Panelu.',
-        'invalid_copy_id' => 'Jajo wybrane do skopiowania skryptu albo nie istnieje, albo samo posiada kopię skryptu.',
-        'has_children' => 'Jajo jest nadrzędne dla jednego lub więcej innych jajek. Proszę najpierw usunąć te jajka przed usunięciem tego.',
+        'delete_has_servers' => 'An Egg with active servers attached to it cannot be deleted from the Panel.',
+        'invalid_copy_id' => 'The Egg selected for copying a script from either does not exist, or is copying a script itself.',
+        'has_children' => 'This Egg is a parent to one or more other Eggs. Please delete those Eggs before deleting this Egg.',
     ],
     'variables' => [
-        'env_not_unique' => 'Zmienna środowiskowa :name musi być unikalna dla tego jajka.',
-        'reserved_name' => 'Zmienna środowiskowa :name jest chroniona i nie może być przypisana do zmiennej.',
-        'bad_validation_rule' => 'Reguła walidacji ":rule" nie jest prawidłową regułą dla tej aplikacji.',
+        'env_not_unique' => 'The environment variable :name must be unique to this Egg.',
+        'reserved_name' => 'The environment variable :name is protected and cannot be assigned to a variable.',
+        'bad_validation_rule' => 'The validation rule ":rule" is not a valid rule for this application.',
     ],
     'importer' => [
-        'json_error' => 'Wystąpił błąd podczas próby analizy pliku JSON: :error',
-        'file_error' => 'Podany plik JSON jest nieprawidłowy.',
-        'invalid_json_provided' => 'Podany plik JSON nie jest w formacie, który może być rozpoznany.',
+        'json_error' => 'There was an error while attempting to parse the JSON file: :error.',
+        'file_error' => 'The JSON file provided was not valid.',
+        'invalid_json_provided' => 'The JSON file provided is not in a format that can be recognized.',
     ],
     'subusers' => [
-        'editing_self' => 'Edytowanie własnego konta podużytkownika jest niedozwolone.',
-        'user_is_owner' => 'Nie można dodać właściciela serwera jako podużytkownika tego serwera.',
-        'subuser_exists' => 'Użytkownik z tym adresem e-mail jest już przypisany jako podużytkownik dla tego serwera.',
+        'editing_self' => 'Editing your own subuser account is not permitted.',
+        'user_is_owner' => 'You cannot add the server owner as a subuser for this server.',
+        'subuser_exists' => 'A user with that email address is already assigned as a subuser for this server.',
     ],
     'databases' => [
-        'delete_has_databases' => 'Nie można usunąć serwera hosta bazy danych, z którym powiązane są aktywne bazy danych.',
+        'delete_has_databases' => 'Cannot delete a database host server that has active databases linked to it.',
     ],
     'tasks' => [
-        'chain_interval_too_long' => 'Maksymalny odstęp czasu dla zadania, które zostało zablokowane wynosi 15 minut.',
+        'chain_interval_too_long' => 'The maximum interval time for a chained task is 15 minutes.',
     ],
     'locations' => [
-        'has_nodes' => 'Nie można usunąć lokalizacji, do której dołączone są aktywne węzły.',
+        'has_nodes' => 'Cannot delete a location that has active nodes attached to it.',
     ],
     'users' => [
-        'node_revocation_failed' => 'Nie udało się odwołać kluczy na <a href=":link">węźle #:node</a>. :error',
+        'node_revocation_failed' => 'Failed to revoke keys on <a href=":link">Node #:node</a>. :error',
     ],
     'deployment' => [
-        'no_viable_nodes' => 'Nie znaleziono węzłów spełniających wymagania dla automatycznego wdrażania.',
-        'no_viable_allocations' => 'Nie znaleziono portów spełniających wymagania dla automatycznego wdrażania.',
+        'no_viable_nodes' => 'No nodes satisfying the requirements specified for automatic deployment could be found.',
+        'no_viable_allocations' => 'No allocations satisfying the requirements for automatic deployment were found.',
     ],
     'api' => [
-        'resource_not_found' => 'Żądany zasób nie istnieje na tym serwerze.',
+        'resource_not_found' => 'The requested resource does not exist on this server.',
     ],
 ];
