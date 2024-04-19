151aaddd0dc1d3608cee4c75fae9fdfd08a67296
Lance Pioch <lancepioch@gmail.com>
2024-04-19 09:49:10 -0400

    New translations server.php (Hungarian)

--- lang/hu/admin/server.php
+++ lang/hu/admin/server.php
@@ -2,7 +2,7 @@
 
 return [
     'exceptions' => [
-        'no_new_default_allocation' => 'You are attempting to delete the default allocation for this server but there is no fallback allocation to use.',
+        'no_new_default_allocation' => 'Megpróbáltad törölni az allokációt, de nincs másik alapértelmezett allokáció hozzáadva a szerverhez.',
         'marked_as_failed' => 'This server was marked as having failed a previous installation. Current status cannot be toggled in this state.',
         'bad_variable' => 'There was a validation error with the :name variable.',
         'daemon_exception' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged. (request id: :request_id)',
@@ -10,8 +10,8 @@ return [
     ],
     'alerts' => [
         'startup_changed' => 'The startup configuration for this server has been updated. If this server\'s egg was changed a reinstall will be occurring now.',
-        'server_deleted' => 'Server has successfully been deleted from the system.',
-        'server_created' => 'Server was successfully created on the panel. Please allow the daemon a few minutes to completely install this server.',
+        'server_deleted' => 'Szerver sikeresen eltávolítva.',
+        'server_created' => 'Szerver sikeresen létrehozva. Várj néhány percet, amíg a daemon teljesen feltelepíti a szervert.',
         'build_updated' => 'The build details for this server have been updated. Some changes may require a restart to take effect.',
         'suspension_toggled' => 'Server suspension status has been changed to :status.',
         'rebuild_on_boot' => 'This server has been marked as requiring a Docker Container rebuild. This will happen the next time the server is started.',
@@ -19,9 +19,9 @@ return [
         'server_reinstalled' => 'This server has been queued for a reinstallation beginning now.',
         'details_updated' => 'Server details have been successfully updated.',
         'docker_image_updated' => 'Successfully changed the default Docker image to use for this server. A reboot is required to apply this change.',
-        'node_required' => 'You must have at least one node configured before you can add a server to this panel.',
-        'transfer_nodes_required' => 'You must have at least two nodes configured before you can transfer servers.',
-        'transfer_started' => 'Server transfer has been started.',
+        'node_required' => 'Legalább egy node-ot konfigurálni kell szerverek hozzáadásához.',
+        'transfer_nodes_required' => 'Legalább két node-nak kell lennie szerverek költöztetéséhez.',
+        'transfer_started' => 'Szerver költöztetés elindítva.',
         'transfer_not_viable' => 'The node you selected does not have the required disk space or memory available to accommodate this server.',
     ],
 ];
