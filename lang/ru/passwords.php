6fd618d09aa918f451dce617a4d41fe861c035fc
Lance Pioch <lancepioch@gmail.com>
2024-04-19 09:47:40 -0400

    New translations passwords.php (Russian)

--- lang/ru/passwords.php
+++ lang/ru/passwords.php
@@ -11,9 +11,9 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => 'We can\'t find a user with that e-mail address.',
+    'password' => 'Длина пароля менее шести символов или пароли не совпадают.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправили ссылку для сброса пароля на ваш адрес эл. почты!',
+    'token' => 'Этот токен сброса пароля недействителен.',
+    'user' => 'Мы не можем найти пользователя с таким адресом электронной почты.',
 ];
