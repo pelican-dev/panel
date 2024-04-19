f30a0ab129fca21eb52c9354de8746f23e4fa5dd
Lance Pioch <lancepioch@gmail.com>
2024-04-19 09:50:20 -0400

    New translations index.php (Japanese)

--- lang/ja/dashboard/index.php
+++ lang/ja/dashboard/index.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'search' => 'サーバーを検索...',
+    'search' => 'サーバーを検索',
     'no_matches' => '検索条件に一致するサーバーが見つかりませんでした。',
     'cpu_title' => 'CPU',
     'memory_title' => 'メモリ',
