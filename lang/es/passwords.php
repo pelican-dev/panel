49b8469c053a9d71d615c91662854a75acd53173
Lance Pioch <lancepioch@gmail.com>
2024-04-18 22:08:11 -0400

    New translations passwords.php (Spanish)

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -15,5 +15,5 @@ return [
     'reset' => '¡Tu contraseña ha sido restablecida!',
     'sent' => '¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
     'token' => 'Este token de restablecimiento de contraseña no es válido.',
-    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
 ];
