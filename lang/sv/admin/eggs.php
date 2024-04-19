b2a04fe39ef7aec32fd0790e11c0f9229b2ce12c
Lance Pioch <lancepioch@gmail.com>
2024-04-19 09:48:40 -0400

    New translations eggs.php (Swedish)

--- lang/sv/admin/eggs.php
+++ lang/sv/admin/eggs.php
@@ -2,10 +2,10 @@
 
 return [
     'notices' => [
-        'imported' => 'Successfully imported this Egg and its associated variables.',
-        'updated_via_import' => 'This Egg has been updated using the file provided.',
-        'deleted' => 'Successfully deleted the requested egg from the Panel.',
-        'updated' => 'Egg configuration has been updated successfully.',
+        'imported' => 'Lyckades importera detta ägg och dess associerade variabler.',
+        'updated_via_import' => 'Detta ägg har uppdaterats med den fil som tillhandahållits.',
+        'deleted' => 'Lyckades radera det begärda ägget från panelen.',
+        'updated' => 'Äggkonfigurationen har uppdaterats framgångsrikt.',
         'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
         'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
     ],
