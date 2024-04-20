bc246454210d394092339cb5b8cafcb22ad24cfa
Lance Pioch <lancepioch@gmail.com>
2024-04-20 18:28:15 -0400

    New translations eggs.php (Japanese)

--- lang/ja/admin/eggs.php
+++ lang/ja/admin/eggs.php
@@ -2,18 +2,18 @@
 
 return [
     'notices' => [
-        'imported' => 'この卵と関連する変数を正常にインポートしました。',
-        'updated_via_import' => '指定されたファイルを使用してこの卵を更新しました。',
-        'deleted' => '要求された卵をパネルから削除しました。',
-        'updated' => '卵の設定が更新されました。',
-        'script_updated' => 'Eggのインストールスクリプトが更新され、サーバーがインストールされるたびに実行されます。',
-        'egg_created' => '新しい卵が生成されました。この新しい卵を適用するには、実行中のデーモンを再起動する必要があります。',
+        'imported' => 'このEggと関連する変数を正常にインポートしました。',
+        'updated_via_import' => 'ファイルを使用してこのEggを更新しました。',
+        'deleted' => 'Eggを削除しました。',
+        'updated' => 'Eggの設定を更新しました。',
+        'script_updated' => 'Eggのインストールスクリプトが更新されました。サーバーのインストール時に実行されます。',
+        'egg_created' => 'Eggを作成しました。このEggを適用するには、実行中のDaemonを再起動してください。',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => '変数 ":variable" が削除され、再構築されると、サーバーで使用できなくなります。',
-            'variable_updated' => '変数 ":variable" が更新されました。変更を適用するには、この変数を使用しているサーバーを再構築する必要があります。',
-            'variable_created' => '新しい変数が作成され、この卵に割り当てられました。',
+            'variable_deleted' => '変数「:variable」を削除しました。再起動後は使用できなくなります。',
+            'variable_updated' => '変数「:variable」を更新しました。再起動後に適用されます。',
+            'variable_created' => '変数が作成され、このEggに割り当てられました。',
         ],
     ],
 ];
