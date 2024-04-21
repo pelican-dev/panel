f1009664a06ae43ca1cc5876e9e65a9708757765
Pascale Beier <mail@pascalebeier.de>
2024-04-21 17:39:40 -0400

    Update DispatchWebhooksJob.php
    Co-authored-by: Lance Pioch <lancepioch@gmail.com>

--- app/Jobs/DispatchWebhooksJob.php
+++ app/Jobs/DispatchWebhooksJob.php
@@ -15,9 +15,6 @@ class DispatchWebhooksJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct(private ShouldDispatchWebhooks $event)
     {
         //
