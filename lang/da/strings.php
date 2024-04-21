35b99d7d70454c02b49f7f6ad09b7ed8b3345c2b
Lance Pioch <lancepioch@gmail.com>
2024-04-20 20:25:38 -0400

    New translations strings.php (Danish)

--- lang/da/strings.php
+++ lang/da/strings.php
@@ -91,5 +91,5 @@ return [
     'enable' => 'Aktiver',
     'disable' => 'Deaktiver',
     'save' => 'Gem',
-    'copyright' => '&reg; 2024 - :year Pelican',
+    'copyright' => '&reg; 2024 - :year Pelican | Supported by PM-Kirill ',
 ];
