abc0ee9fa71f4b408810ca8cbf75df16b9b1e6ee
Lance Pioch <lancepioch@gmail.com>
2024-04-21 16:28:35 -0400

    New translations strings.php (Polish)

--- lang/pl/strings.php
+++ lang/pl/strings.php
@@ -91,5 +91,5 @@ return [
     'enable' => 'Włącz',
     'disable' => 'Wyłącz',
     'save' => 'Zapisz',
-    'copyright' => '&reg; 2024 - :year Pelican | Supported by PM-Kirill',
+    'copyright' => '&reg; 2024 - :year Pelican',
 ];
