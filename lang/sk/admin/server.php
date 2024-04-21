c0e65502f69bafb07c679ef0256b29650cab826b
Lance Pioch <lancepioch@gmail.com>
2024-04-21 03:29:27 -0400

    New translations server.php (Slovak)

--- lang/sk/admin/server.php
+++ lang/sk/admin/server.php
@@ -2,17 +2,17 @@
 
 return [
     'exceptions' => [
-        'no_new_default_allocation' => 'You are attempting to delete the default allocation for this server but there is no fallback allocation to use.',
-        'marked_as_failed' => 'This server was marked as having failed a previous installation. Current status cannot be toggled in this state.',
-        'bad_variable' => 'There was a validation error with the :name variable.',
-        'daemon_exception' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged. (request id: :request_id)',
-        'default_allocation_not_found' => 'The requested default allocation was not found in this server\'s allocations.',
+        'no_new_default_allocation' => 'Pokúšate sa odstrániť predvolené pridelenie pre tento server, ale nie je možné použiť žiadne záložné pridelenie.',
+        'marked_as_failed' => 'Tento server bol označený ako neúspešný pri predchádzajúcej inštalácii. V tomto stave nie je možné prepnúť aktuálny stav.',
+        'bad_variable' => 'Pri overení premennej :name sa vyskytla chyba.',
+        'daemon_exception' => 'Pri pokuse o komunikáciu s daémonom sa vyskytla chyba, čo malo za následok kód odpovede HTTP/:code. Táto chyba bola zaznamenaná. (id žiadosti: :request_id)',
+        'default_allocation_not_found' => 'Požadované predvolené pridelenie sa nenašlo v pridelení tohto servera.',
     ],
     'alerts' => [
-        'startup_changed' => 'The startup configuration for this server has been updated. If this server\'s egg was changed a reinstall will be occurring now.',
-        'server_deleted' => 'Server has successfully been deleted from the system.',
-        'server_created' => 'Server was successfully created on the panel. Please allow the daemon a few minutes to completely install this server.',
-        'build_updated' => 'The build details for this server have been updated. Some changes may require a restart to take effect.',
+        'startup_changed' => 'Konfigurácia spúšťania pre tento server bola aktualizovaná. Ak sa vajíčko tohto servera zmenilo, dôjde k preinštalovaniu.',
+        'server_deleted' => 'Server bol úspešne odstránený zo systému.',
+        'server_created' => 'Server bol úspešne vytvorený na paneli. Nechajte daémonovi niekoľko minút na úplnú inštaláciu tohto servera.',
+        'build_updated' => 'Podrobnosti zostavy pre tento server boli aktualizované. Niektoré zmeny môžu vyžadovať reštart, aby sa prejavili.',
         'suspension_toggled' => 'Server suspension status has been changed to :status.',
         'rebuild_on_boot' => 'This server has been marked as requiring a Docker Container rebuild. This will happen the next time the server is started.',
         'install_toggled' => 'The installation status for this server has been toggled.',
