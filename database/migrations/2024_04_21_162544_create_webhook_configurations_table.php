b66ca7ada3e640a55ced06060c311771f1028fc0
Pascale Beier <mail@pascalebeier.de>
2024-04-21 17:38:49 -0400

    Update 2024_04_21_162544_create_webhook_configurations_table.php
    Co-authored-by: Lance Pioch <lancepioch@gmail.com>

--- database/migrations/2024_04_21_162544_create_webhook_configurations_table.php
+++ database/migrations/2024_04_21_162544_create_webhook_configurations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('endpoint');
             $table->string('secret');
-            $table->string('description')->nullable();
+            $table->string('description');
             $table->json('events');
             $table->timestamps();
         });
