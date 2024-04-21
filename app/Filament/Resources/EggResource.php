53ad87a3494579ec45d1921e244fa7709580a8c2
notCharles <charles@pelican.dev>
2024-04-21 13:30:55 -0400

    update egg configuration tab

--- app/Filament/Resources/EggResource.php
+++ app/Filament/Resources/EggResource.php
@@ -27,23 +27,49 @@ class EggResource extends Resource
             ->schema([
                 Forms\Components\Tabs::make()->tabs([
                     Forms\Components\Tabs\Tab::make('Configuration')
-                        ->columns(2)
+                        ->columns(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 4])
                         ->schema([
-                            Forms\Components\TextInput::make('name')->required()->maxLength(191)
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->maxLength(191)
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2])
                                 ->helperText('A simple, human-readable name to use as an identifier for this Egg.'),
-                            Forms\Components\Textarea::make('description')->rows(5)
+                            Forms\Components\TextInput::make('uuid')
+                                ->disabled()
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2])
+                                ->helperText('This is the globally unique identifier for this Egg which Wings uses as an identifier.'),
+                            Forms\Components\Textarea::make('description')
+                                ->rows(3)
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2])
                                 ->helperText('A description of this Egg that will be displayed throughout the Panel as needed.'),
-                            Forms\Components\TextInput::make('uuid')->disabled()
-                                ->helperText('This is the globally unique identifier for this Egg which the Daemon uses as an identifier.'),
-                            Forms\Components\TextInput::make('author')->required()->maxLength(191)->disabled()
+                            Forms\Components\TextInput::make('author')
+                                ->required()
+                                ->maxLength(191)
+                                ->disabled()
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2])
                                 ->helperText('The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.'),
-                            Forms\Components\Textarea::make('startup')->rows(3)->columnSpanFull()
+                            Forms\Components\Textarea::make('startup')
+                                ->rows(2)
+                                ->columnSpanFull()
                                 ->helperText('The default startup command that should be used for new servers using this Egg.'),
-                            Forms\Components\TagsInput::make('file_denylist')->placeholder('denied-file.txt'),
-                            Forms\Components\Toggle::make('force_outgoing_ip')->required()
+                            Forms\Components\TagsInput::make('file_denylist')
+                                ->placeholder('denied-file.txt')
+                                ->helperText('A list of files that the end user is not allowed to edit.')
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2]),
+                            Forms\Components\TagsInput::make('features')
+                                ->placeholder('Add Feature')
+                                ->helperText('')
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2]),
+                            Forms\Components\Toggle::make('force_outgoing_ip')
                                 ->helperText("Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.
                                     Required for certain games to work properly when the Node has multiple public IP addresses.
                                     Enabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node."),
+                            Forms\Components\Toggle::make('script_is_privileged')
+                                ->helperText('The docker images available to servers using this egg.'),
+                            Forms\Components\TextInput::make('update_url')
+                                ->disabled()
+                                ->helperText('Not implemented.')
+                                ->columnSpan(['default' => 1, 'sm' => 1, 'md' => 2, 'lg' => 2]),
                             Forms\Components\KeyValue::make('docker_images')
                                 ->columnSpanFull()
                                 ->addActionLabel('Add Image')
@@ -52,29 +78,24 @@ class EggResource extends Resource
                                 ->helperText('The docker images available to servers using this egg.'),
                         ]),
 
-
                     Forms\Components\Tabs\Tab::make('Process Management')
-                        ->columns(2)
+                        ->columns()
                         ->schema([
                             Forms\Components\Select::make('config_from')
                                 ->label('Copy Settings From')
                                 ->placeholder('None')
                                 ->relationship('configFrom', 'name', ignoreRecord: true)
                                 ->helperText('If you would like to default to settings from another Egg select it from the menu above.'),
-
                             Forms\Components\TextInput::make('config_stop')
                                 ->maxLength(191)
                                 ->label('Stop Command')
                                 ->helperText('The command that should be sent to server processes to stop them gracefully. If you need to send a SIGINT you should enter ^C here.'),
-
                             Forms\Components\Textarea::make('config_startup')->rows(10)->json()
                                 ->label('Start Configuration')
                                 ->helperText('List of values the daemon should be looking for when booting a server to determine completion.'),
-
                             Forms\Components\Textarea::make('config_files')->rows(10)->json()
                                 ->label('Configuration Files')
                                 ->helperText('This should be a JSON representation of configuration files to modify and what parts should be changed.'),
-
                             Forms\Components\Textarea::make('config_logs')->rows(10)->json()
                                 ->label('Log Configuration')
                                 ->helperText('This should be a JSON representation of where log files are stored, and whether or not the daemon should be creating custom logs.'),
@@ -96,7 +117,7 @@ class EggResource extends Resource
                                     Forms\Components\TextInput::make('name')->live()->maxLength(191)->columnSpanFull(),
                                     Forms\Components\Textarea::make('description')->columnSpanFull(),
                                     Forms\Components\TextInput::make('env_variable')->maxLength(191)->required()->hint(fn ($state) => "{{{$state}}}"),
-                                    Forms\Components\TextInput::make('default_value')->maxLength(191),
+                                    Forms\Components\TextInput::make('default_value')->maxLength(191)->required(),
                                     Forms\Components\TextInput::make('rules')->columnSpanFull()->required(),
                                 ]),
                         ]),
@@ -126,10 +147,6 @@ class EggResource extends Resource
                         ]),
 
                 ])->columnSpanFull()->persistTabInQueryString(),
-
-                // Forms\Components\TagsInput::make('features'),
-                // Forms\Components\TextInput::make('update_url'),
-                // Forms\Components\Toggle::make('script_is_privileged')->required(),
             ]);
     }
 
