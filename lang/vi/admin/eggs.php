bac12a7a3cdd3ae362c382586a8424bccce3fb99
Lance Pioch <lancepioch@gmail.com>
2024-04-20 22:34:39 -0400

    New translations eggs.php (Vietnamese)

--- lang/vi/admin/eggs.php
+++ lang/vi/admin/eggs.php
@@ -3,17 +3,17 @@
 return [
     'notices' => [
         'imported' => 'Thành công thêm vào Egg này và những biến thể liên quan.',
-        'updated_via_import' => 'Egg này đã được thay đổi sử dụng tập tin được cung cấp.',
+        'updated_via_import' => 'Egg này đã được cập nhật sử dụng tập tin được cung cấp.',
         'deleted' => 'Thành công xóa egg được yêu cầu khỏi bảng điều khiển.',
         'updated' => 'Cấu hình cho Egg đã được cập nhật thành công.',
-        'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-        'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+        'script_updated' => 'Tập lệnh cài đặt Egg đã được cập nhật và sẽ chạy bất cứ khi nào máy chủ được cài đặt.',
+        'egg_created' => 'Một quả trứng mới đã được đẻ thành công. Bạn sẽ cần phải khởi động lại những daemon đang chạy để áp dụng egg mới này.',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'Biến ".variable" đã được xóa và sẽ không còn khả dụng với máy chủ một khi được dựng lại.',
+            'variable_updated' => 'Biến ".variable" đã được cập nhật. Bạn sẽ cần phải dựng lại những máy chủ nào sử dụng biến này để áp dụng các thay đổi.',
+            'variable_created' => 'Biến mới đã được tạo thành công và được gán cho egg này.',
         ],
     ],
 ];
