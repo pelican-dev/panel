7928838f259121c3318efacca980db4c1f33bcd1
Lance Pioch <lancepioch@gmail.com>
2024-04-21 19:07:04 -0400

    New translations account.php (Danish)

--- lang/da/dashboard/account.php
+++ lang/da/dashboard/account.php
@@ -11,18 +11,18 @@ return [
         'updated' => 'Din adgangskode er blevet opdateret.',
     ],
     'two_factor' => [
-        'button' => 'Konfigurer 2-Factor godkendelse',
-        'disabled' => '2-factor godkendelse er blevet deaktiveret på din konto. Du vil ikke længere blive bedt om at angive en token ved login.',
-        'enabled' => '2-factor godkendelse er blevet aktiveret på din konto! Fra nu af, når du logger ind, vil du blive bedt om at angive koden genereret af din enhed.',
-        'invalid' => 'Den angivne token var ugyldig.',
+        'button' => 'Konfigurer 2-Faktor godkendelse',
+        'disabled' => '2-faktor godkendelse er blevet deaktiveret på din konto. Du vil ikke længere blive bedt om at angive en token ved login.',
+        'enabled' => '2-faktor godkendelse er blevet aktiveret på din konto! Fra nu af, når du logger ind, vil du blive bedt om at angive koden genereret af din enhed.',
+        'invalid' => 'Den angivne nøgle var ugyldig.',
         'setup' => [
-            'title' => 'Opsætning af 2-factor godkendelse',
+            'title' => 'Opsætning af 2-faktor godkendelse',
             'help' => 'Kan ikke scanne koden? Indtast koden nedenfor i din applikation:',
             'field' => 'Indtast token',
         ],
         'disable' => [
-            'title' => 'Deaktiver 2-factor godkendelse',
-            'field' => 'Indtast token',
+            'title' => 'Deaktiver 2-faktor godkendelse',
+            'field' => 'Indtast nøgle',
         ],
     ],
 ];
