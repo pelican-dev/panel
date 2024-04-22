2a2de2ae29a5003e7fa228e02e50b55e640c8744
Miniontoby <tobias.gaarenstroom@gmail.com>
2024-04-22 15:42:13 +0200

    Translate ServersRelationManager's table

--- app/Filament/Resources/UserResource/RelationManagers/ServersRelationManager.php
+++ app/Filament/Resources/UserResource/RelationManagers/ServersRelationManager.php
@@ -18,35 +18,38 @@ class ServersRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('uuid')
                     ->hidden()
-                    ->label('UUID')
+                    ->label(trans('strings.uuid'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
                     ->icon('tabler-brand-docker')
+                    ->label(trans('strings.name'))
                     ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('node.name')
                     ->icon('tabler-server-2')
+                    ->label(trans_choice('strings.nodes', 2))
                     ->url(fn (Server $server): string => route('filament.admin.resources.nodes.edit', ['record' => $server->node]))
                     ->sortable(),
                 Tables\Columns\TextColumn::make('egg.name')
                     ->icon('tabler-egg')
+                    ->label(trans_choice('strings.eggs', 2))
                     ->url(fn (Server $server): string => route('filament.admin.resources.eggs.edit', ['record' => $server->egg]))
                     ->sortable(),
                 Tables\Columns\SelectColumn::make('allocation.id')
-                    ->label('Primary Allocation')
+                    ->label(trans('strings.primary_allocation'))
                     ->options(fn ($state, Server $server) => [$server->allocation->id => $server->allocation->address])
                     ->selectablePlaceholder(false)
                     ->sortable(),
                 Tables\Columns\TextColumn::make('image')->hidden(),
                 Tables\Columns\TextColumn::make('databases_count')
                     ->counts('databases')
-                    ->label('Databases')
+                    ->label(trans_choice('strings.databases', 2))
                     ->icon('tabler-database')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('backups_count')
                     ->counts('backups')
-                    ->label('Backups')
+                    ->label(trans_choice('strings.backups', 2))
                     ->icon('tabler-file-download')
                     ->numeric()
                     ->sortable(),
