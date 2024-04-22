380d80b63ea861af97be4b0cae784bf3b74cded0
Lance Pioch <lancepioch@gmail.com>
2024-04-22 18:39:18 -0400

    New translations node.php (Greek)

--- lang/el/admin/node.php
+++ lang/el/admin/node.php
@@ -2,14 +2,14 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'Η διεύθυνση FQDN ή IP που παρέχεται δεν επιλύει μια έγκυρη διεύθυνση IP.',
+        'fqdn_required_for_ssl' => 'Ένα πλήρως αναγνωρισμένο όνομα τομέα που επιλύει σε μια δημόσια διεύθυνση IP απαιτείται προκειμένου να χρησιμοποιηθεί SSL για αυτόν τον κόμβο.',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
-        'node_deleted' => 'Node has been successfully removed from the panel.',
-        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
-        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
-        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
+        'allocations_added' => 'Οι κατανομές προστέθηκαν με επιτυχία σε αυτόν τον κόμβο.',
+        'node_deleted' => 'Ο κόμβος αφαιρέθηκε με επιτυχία από το panel.',
+        'node_created' => 'Ο νέος κόμβος δημιουργήθηκε με επιτυχία. Μπορείτε να ρυθμίσετε αυτόματα το daemon σε αυτό το μηχάνημα, επισκεπτόμενοι την καρτέλα \'Διαμόρφωση\'. <strong>Για να μπορέσετε να προσθέσετε διακομιστές πρέπει πρώτα να διαθέσετε τουλάχιστον μία διεύθυνση IP και θύρα.</strong>',
+        'node_updated' => 'Οι πληροφορίες κόμβου έχουν ενημερωθεί. Αν έχει αλλάξει οποιαδήποτε ρύθμιση daemon, θα πρέπει να επανεκκινήσετε για να τεθούν σε ισχύ αυτές οι αλλαγές.',
+        'unallocated_deleted' => 'Διαγράφηκαν όλες οι μη κατανεμημένες θύρες για <code>:ip</code>.',
     ],
 ];
