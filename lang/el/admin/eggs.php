f386c91662b723ecea7ccb63a1ddaf88f61e31cd
Lance Pioch <lancepioch@gmail.com>
2024-04-22 18:39:17 -0400

    New translations eggs.php (Greek)

--- lang/el/admin/eggs.php
+++ lang/el/admin/eggs.php
@@ -2,18 +2,18 @@
 
 return [
     'notices' => [
-        'imported' => 'Successfully imported this Egg and its associated variables.',
-        'updated_via_import' => 'This Egg has been updated using the file provided.',
-        'deleted' => 'Successfully deleted the requested egg from the Panel.',
-        'updated' => 'Egg configuration has been updated successfully.',
-        'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-        'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+        'imported' => 'Επιτυχής εισαγωγή αυτού του Αυγού και των σχετικών μεταβλητών.',
+        'updated_via_import' => 'Αυτό το αυγό έχει ενημερωθεί χρησιμοποιώντας το αρχείο που παρέχεται.',
+        'deleted' => 'Διαγράφηκε με επιτυχία το αυγό που ζητήθηκε από το panel.',
+        'updated' => 'Η ρύθμιση παραμέτρων αυτού του αυγού έχει ενημερωθεί με επιτυχία.',
+        'script_updated' => 'Το σενάριο εγκατάστασης των αυγών έχει ενημερωθεί και θα εκτελείται όποτε εγκαθίστανται οι διακομιστές.',
+        'egg_created' => 'Ένα νέο αυγό ορίστηκε με επιτυχία. Θα πρέπει να επανεκκινήσετε τυχόν εκτελούμενα daemons για να εφαρμόσετε αυτό το νέο αυγό.',
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'Η μεταβλητή ":variable" έχει διαγραφεί και δεν θα είναι πλέον διαθέσιμη στους διακομιστές μετά την αναδόμηση.',
+            'variable_updated' => 'Η μεταβλητή ":variable" έχει ενημερωθεί. Θα χρειαστεί να αναδημιουργήσεις όλους τους διακομιστές που χρησιμοποιούν αυτήν τη μεταβλητή για να εφαρμοστούν οι αλλαγές.',
+            'variable_created' => 'Η νέα μεταβλητή δημιουργήθηκε και αντιστοιχίστηκε με επιτυχία σε αυτό το αυγό.',
         ],
     ],
 ];
