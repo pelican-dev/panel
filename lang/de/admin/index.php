73e8515da6e4a8aa052716bc0b779959df04c5d1
Lance Pioch <lancepioch@gmail.com>
2024-04-26 03:58:41 -0400

    New translations index.php (German)

--- lang/de/admin/index.php
+++ lang/de/admin/index.php
@@ -3,17 +3,17 @@
 return [
     'title' => 'Administration',
     'header' => [
-        'title' => 'Administrative Overview',
-        'subtitle' => 'A quick glance at your system.',
+        'title' => 'Administrative Übersicht',
+        'subtitle' => 'Ein kurzer Blick auf dein System.',
     ],
     'content' => [
-        'title' => 'System Information',
-        'up-to-date' => 'You are running Pelican version <code>:version</code>. Your panel is up-to-date!',
-        'not-up-to-date1' => 'Your panel is <strong>not up-to-date!</strong> The latest version is',
-        'not-up-to-date2' => 'and you are currently running version <code>:version</code>.',
-        'get-help' => 'Get Help <small>(via Discord)</small>',
-        'documentation' => 'Documentation',
+        'title' => 'System-Informationen',
+        'up-to-date' => 'Du verwendest die Pelican Version <code>:version</code>. Dein Panel ist aktuell!',
+        'not-up-to-date1' => 'Dein Panel ist <strong>nicht auf dem neuesten Stand!</strong> Die neueste Version ist',
+        'not-up-to-date2' => 'und du verwendest zur Zeit die Version <code>:version</code>.',
+        'get-help' => 'Erhalte Hilfe <small>(via Discord)</small>',
+        'documentation' => 'Dokumentation',
         'github' => 'Github',
-        'support-the-project' => 'Support the Project',
+        'support-the-project' => 'Unterstütze das Projekt',
     ],
 ];
