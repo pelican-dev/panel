f30e52e8bbd9839c713405cd4d2aea75f33f9879
Lance Pioch <lancepioch@gmail.com>
2024-04-26 03:58:37 -0400

    New translations account.php (Spanish)

--- lang/es/dashboard/account.php
+++ lang/es/dashboard/account.php
@@ -1,48 +1,48 @@
 <?php
 
 return [
-    'title' => 'Account Overview',
+    'title' => 'Resumen de la cuenta',
     'email' => [
-        'title' => 'Update Email Address',
-        'button' => 'Update Email',
-        'updated' => 'Your primary email has been updated.',
+        'title' => 'Actualizar dirección de correo',
+        'button' => 'Actualizar Correo',
+        'updated' => 'Su correo principal ha sido actualizado',
     ],
     'password' => [
-        'title' => 'Update Password',
-        'button' => 'Update Password',
-        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'title' => 'Actualizar Contraseña',
+        'button' => 'Actualizar Contraseña',
+        'requirements' => 'Su nueva contraseña debe tener al menos 8 caracteres de longitud y ser usada únicamente para este sitio web.',
         'validation' => [
-            'account_password' => 'You must provide your account password.',
-            'current_password' => 'You must provide your current password.',
-            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+            'account_password' => 'Debes proporcionar la contraseña de tu cuenta.',
+            'current_password' => 'Debes proporcionar tu contraseña actual.',
+            'password_confirmation' => 'La confirmación de contraseña no coincide con la contraseña introducida.',
         ],
         'updated' => 'Tu contraseña ha sido actualizada.',
     ],
     'two_factor' => [
-        'title' => 'Two-Step Verification',
+        'title' => 'Verificación en dos pasos',
         'button' => 'Configurar autenticación de 2 factores',
         'disabled' => 'La autenticación de dos factores ha sido desactivada en tu cuenta. Ya no se te pedirá que proporciones un token al iniciar sesión.',
         'enabled' => '¡La autenticación de dos factores ha sido activada en tu cuenta! A partir de ahora, al iniciar sesión, se te pedirá que proporciones el código generado por tu dispositivo.',
         'invalid' => 'El token proporcionado no era válido.',
         'enable' => [
-            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
-            'button' => 'Enable Two-Step',
+            'help' => 'Actualmente no tienes habilitada la verificación en dos pasos en tu cuenta. Haz clic en el botón de abajo para empezar a configurarla.',
+            'button' => 'Activar AF2',
         ],
         'disable' => [
-            'help' => 'Two-step verification is currently enabled on your account.',
+            'help' => 'La verificación en dos pasos está activada en tu cuenta.',
             'title' => 'Desactivar autenticación de dos factores',
             'field' => 'Introduce el token',
-            'button' => 'Disable Two-Step',
+            'button' => 'Desactivar AF2',
         ],
         'setup' => [
-            'title' => 'Enable Two-Step Verification',
-            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
-            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+            'title' => 'Habilitar verificación en dos pasos',
+            'subtitle' => "Ayuda a proteger tu cuenta de acceso no autorizado. Se te pedirá un código de verificación cada vez que inicies sesión.",
+            'help' => 'Escanee el código QR de arriba usando la aplicación de autenticación en dos pasos de su elección. Luego, introduzca el código de 6 dígitos generado en el campo de abajo.',
         ],
 
         'required' => [
-            'title' => '2-Factor Required',
-            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
+            'title' => '2-Factor Requerido',
+            'description' => 'Tu cuenta debe tener habilitada la autenticación de doble factor para continuar.',
         ],
     ],
 ];
