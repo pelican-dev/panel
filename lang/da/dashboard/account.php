17605d98442b9203bb362c9cd2a3a6b32f6bc8e2
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:05 -0400

    New translations account.php (Danish)

--- lang/da/dashboard/account.php
+++ lang/da/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'Opdater din e-mail',
-        'updated' => 'Din e-mailadresse er blevet opdateret.',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'Skift din adgangskode',
-        'requirements' => 'Din nye adgangskode skal være mindst 8 tegn lang.',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'Din adgangskode er blevet opdateret.',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => 'Konfigurer 2-Faktor godkendelse',
         'disabled' => '2-faktor godkendelse er blevet deaktiveret på din konto. Du vil ikke længere blive bedt om at angive en token ved login.',
         'enabled' => '2-faktor godkendelse er blevet aktiveret på din konto! Fra nu af, når du logger ind, vil du blive bedt om at angive koden genereret af din enhed.',
         'invalid' => 'Den angivne nøgle var ugyldig.',
-        'setup' => [
-            'title' => 'Opsætning af 2-faktor godkendelse',
-            'help' => 'Kan ikke scanne koden? Indtast koden nedenfor i din applikation:',
-            'field' => 'Indtast token',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => 'Deaktiver 2-faktor godkendelse',
             'field' => 'Indtast nøgle',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
