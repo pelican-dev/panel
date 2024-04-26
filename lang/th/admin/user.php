c03dae158fa3da2a7b5def19166582ac383acfb7
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:27:09 -0400

    New translations user.php (Thai)

--- lang/th/admin/user.php
+++ lang/th/admin/user.php
@@ -2,11 +2,20 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
-        'user_is_self' => 'Cannot delete your own user account.',
+        'user_has_servers' => 'ไม่สามารถลบผู้ใช้ที่มีเซิร์ฟเวอร์ที่ใช้งานอยู่แนบอยู่กับบัญชีของพวกเขาได้ โปรดลบเซิร์ฟเวอร์ของพวกเขาก่อนดำเนินการต่อ',
+        'user_is_self' => 'ไม่สามารถลบบัญชีผู้ใช้ของคุณเองได้',
     ],
     'notices' => [
-        'account_created' => 'Account has been created successfully.',
-        'account_updated' => 'Account has been successfully updated.',
+        'account_created' => 'สร้างบัญชีสำเร็จแล้ว',
+        'account_updated' => 'บัญชีได้รับการอัปเดตเรียบร้อยแล้ว',
+    ],
+    'last_admin' => [
+        'hint' => 'This is the last root administrator!',
+        'helper_text' => 'You must have at least one root administrator in your system.',
+    ],
+    'root_admin' => 'Administrator (Root)',
+    'language' => [
+        'helper_text1' => 'Your language (:state) has not been translated yet!\nBut never fear, you can help fix that by',
+        'helper_text2' => 'contributing directly here',
     ],
 ];
