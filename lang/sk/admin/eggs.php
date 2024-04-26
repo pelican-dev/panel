71aaf19464c695256d99983d3483631250516a0c
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:25:18 -0400

    New translations eggs.php (Slovak)

--- lang/sk/admin/eggs.php
+++ lang/sk/admin/eggs.php
@@ -16,4 +16,13 @@ return [
             'variable_created' => 'Nová premenná bola úspešne vytvorená a priradená k tomuto vajcu.',
         ],
     ],
+    'descriptions' => [
+        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
+        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
+        'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
+        'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
+        'startup' => 'The default startup command that should be used for new servers using this Egg.',
+        'docker_images' => 'The docker images available to servers using this egg.',
+    ],
 ];
