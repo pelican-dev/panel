c13a34e64fecca8dda2d5123397035b0b09f71bf
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:15 -0400

    New translations account.php (Dutch)

--- lang/nl/dashboard/account.php
+++ lang/nl/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'E-mailadres wijzigen',
-        'updated' => 'Het e-mailadres is succesvol gewijzigd.',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'Wachtwoord wijzigen',
-        'requirements' => 'Je nieuwe wachtwoord moet minstens 8 tekens bevatten.',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'Het wachtwoord is succesvol gewijzigd.',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => 'Tweestapsverificatie configureren',
         'disabled' => 'Tweestapsverificatie is uitgeschakeld voor je account. Je wordt niet meer gevraagd om een code op te geven bij het inloggen.',
         'enabled' => 'Tweestapsverificatie is ingeschakeld op je account! Vanaf nu moet je bij het inloggen de code opgeven die door je apparaat wordt gegenereerd.',
         'invalid' => 'De opgegeven code is ongeldig.',
-        'setup' => [
-            'title' => 'Tweestapsverificatie instellen',
-            'help' => 'Kan de code niet worden gescand? Voer de onderstaande code in je applicatie:',
-            'field' => 'Code invoeren',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => 'Tweestapsverificatie uitschakelen',
             'field' => 'Code invoeren',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
