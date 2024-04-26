bb01c876486230de47b4aea4be8d64e07ff3fda3
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:13 -0400

    New translations account.php (Japanese)

--- lang/ja/dashboard/account.php
+++ lang/ja/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'メールアドレスを変更',
-        'updated' => 'メールアドレスを変更しました。',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'パスワードを変更',
-        'requirements' => '新しいパスワードは8文字以上で入力してください。',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'パスワードを変更しました。',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => '二段階認証の設定',
         'disabled' => '二段階認証が無効化されています。ログイン時のトークン入力をスキップできます。',
         'enabled' => '二段階認証が有効化されています。ログイン時にトークン入力が必要です。',
         'invalid' => '入力されたトークンは無効です。',
-        'setup' => [
-            'title' => '二段階認証のセットアップ',
-            'help' => 'QRコードを読み込めませんか？以下のコードをアプリケーションに入力してください:',
-            'field' => 'トークンを入力',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => '二段階認証の無効化',
             'field' => 'トークンを入力',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
