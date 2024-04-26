86b034ce1d19f1c1a5e10442bec02f37f601a9a1
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:27:21 -0400

    New translations user.php (Portuguese, Brazilian)

--- lang/pt/admin/user.php
+++ lang/pt/admin/user.php
@@ -2,8 +2,8 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Não é possível excluir este usuário porque ele possui servidores ativos na conta. Remova os servidores na conta antes de continuar.',
-        'user_is_self' => 'Não é possível excluir a sua própria conta.',
+        'user_has_servers' => 'Não é possível excluir um usuário com servidores ativos vinculados à sua conta. Por favor, exclua os servidores antes de continuar.',
+        'user_is_self' => 'Não é possível excluir sua própria conta de usuário.',
     ],
     'notices' => [
         'account_created' => 'A conta foi criada com sucesso.',
