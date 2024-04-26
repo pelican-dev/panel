f83e2fae6e766cbaca96f698678d92045039fd75
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:54 -0400

    New translations index.php (Vietnamese)

--- lang/vi/dashboard/index.php
+++ lang/vi/dashboard/index.php
@@ -1,6 +1,45 @@
 <?php
 
 return [
+    'title' => 'Dashboard',
+    'showing-your-servers' => 'Showing your servers',
+    'showing-others-servers' => "Showing other's servers",
+    'no-other-servers' => 'There are no other servers to display.',
+    'no-servers-associated' => 'There are no servers associated with your account.',
+
+    'content_tabs' => 'Content tabs',
+    'overview' => 'Overview',
+    'heading' => 'Welcome to Pelican!',
+    'expand_sections' => 'You can expand the following sections:',
+    'sections' => [
+        'intro-developers' => [
+            'heading' => 'Information for Developers',
+            'content' => 'Thank you for trying out the development version!',
+            'extra_note' => 'If you run into any issues, please report them on GitHub.',
+            'button_issues' => 'Create Issue',
+            'button_features' => 'Discuss Features',
+        ],
+        'intro-first-node' => [
+            'heading' => 'No Nodes Detected',
+            'content' => "It looks like you don't have any Nodes set up yet, but don't worry because you click the action button to create your first one!",
+            'extra_note' => 'If you run into any issues, please report them on GitHub.',
+            'button_label' => 'Create first Node in Pelican',
+        ],
+        'intro-support' => [
+            'heading' => 'Support Pelican',
+            'content' => 'Thank you for using Pelican, this could only be achieved through the support of you, our contributors, and the rest of our supporters!',
+            'extra_note' => 'We appreciate any and all support from anybody.',
+            'button_translate' => 'Help Translate',
+            'button_donate' => 'Donate Directly',
+        ],
+        'intro-help' => [
+            'heading' => 'Need Help?',
+            'content' => 'Check out the documentation first! If you still need assistance then, fly onto our Discord server!',
+            'button_docs' => 'Read Documentation',
+            'button_discord' => 'Get Help in Discord',
+        ],
+    ],
+
     'search' => 'Tìm kiếm máy chủ...',
     'no_matches' => 'Không có máy chủ nào trùng khớp với tiêu chí tìm kiếm được cung cấp.',
     'cpu_title' => 'CPU',
