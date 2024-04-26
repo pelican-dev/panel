2f472ac62c45009ebf9a63358339e47760c51904
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:25:59 -0400

    New translations account.php (Romanian)

--- lang/ro/dashboard/account.php
+++ lang/ro/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'Actualizează emailul',
-        'updated' => 'Adresa ta de email a fost actualizată.',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'Schimbă-ți parola',
-        'requirements' => 'Noua ta parolă ar trebui să aibă cel puțin 8 caractere.',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'Parola ta a fost actualizată.',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => 'Configurează autentificarea cu doi factori',
         'disabled' => 'Autentificarea cu doi factori a fost dezactivată din contul tău Nu vei mai fi solicitat să furnizezi un token la autentificare.',
         'enabled' => 'Autentificarea cu doi factori a fost activată în contul tău! De acum înainte, când te conectezi, va trebui să introduci codul generat de pe dispozitivul tău.',
         'invalid' => 'Token-ul furnizat nu a fost valid.',
-        'setup' => [
-            'title' => 'Setează autentificarea cu doi factori',
-            'help' => 'Nu poți scana codul? Introdu codul de mai jos din aplicație:',
-            'field' => 'Introdu token-ul',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => 'Dezactivează autentificarea cu doi factori',
             'field' => 'Introdu token-ul',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
