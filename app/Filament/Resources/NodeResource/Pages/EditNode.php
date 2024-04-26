933e693897d2b2c08ad189e1902a5677037c72cc
Lance Pioch <git@lance.sh>
2024-04-25 20:31:38 -0400

    Put something here

--- app/Filament/Resources/NodeResource/Pages/EditNode.php
+++ app/Filament/Resources/NodeResource/Pages/EditNode.php
@@ -33,7 +33,10 @@ class EditNode extends EditRecord
                         ->icon('tabler-server')
                         ->schema((new CreateNode())->form($form)->getComponents()),
                     Tabs\Tab::make('Advanced Settings')
-                        ->icon('tabler-server-cog'),
+                        ->icon('tabler-server-cog')
+                        ->schema([
+                            Forms\Components\Placeholder::make('Coming soon!'),
+                        ]),
                     Tabs\Tab::make('Configuration')
                         ->icon('tabler-code')
                         ->schema([
