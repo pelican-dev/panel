e4750fcecb46ed5cb9bfb83184bce4e5d5a59028
Lance Pioch <lancepioch@gmail.com>
2024-04-26 03:58:32 -0400

    New translations eggs.php (Finnish)

--- lang/fi/admin/eggs.php
+++ lang/fi/admin/eggs.php
@@ -17,12 +17,12 @@ return [
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'name' => 'Yksinkertainen, ihmisluettavissa oleva nimi, jota käytetään tämän munan tunnisteena.',
+        'description' => 'Tämän munan kuvaus, joka näkyy tarpeen mukaan.',
         'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
         'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
         'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
-        'startup' => 'The default startup command that should be used for new servers using this Egg.',
-        'docker_images' => 'The docker images available to servers using this egg.',
+        'startup' => 'Käynnistyskomento, jota käytetään uusille palvelimille, jotka käyttävät tätä munaa.',
+        'docker_images' => 'Docker-kuvat, jotka ovat käytettävissä palvelimille, jotka käyttävät tätä munaa.',
     ],
 ];
