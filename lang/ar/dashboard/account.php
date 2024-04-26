7d353816fbd06ecc13b7b0b367b17786f164a2ad
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:02 -0400

    New translations account.php (Arabic)

--- lang/ar/dashboard/account.php
+++ lang/ar/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'تحديث بريدك الإلكتروني',
-        'updated' => 'تم تحديث عنوان بريدك الإلكتروني.',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'تغيير كلمة مرورك',
-        'requirements' => 'يجب أن تكون كلمة المرور الجديدة مكونة من 8 أحرف على الأقل.',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'تم تحديث كلمة المرور الخاصة بك.',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => 'إعداد التوثيق الثنائي',
         'disabled' => 'تم تعطيل التوثيق الثنائي في حسابك. لن يُطلب منك تقديم رمز عند تسجيل الدخول.',
         'enabled' => 'تم تفعيل التوثيق الثنائي في حسابك! من الآن فصاعدًا، عند تسجيل الدخول، سيُطلب منك تقديم الرمز الذي يُنتجه جهازك.',
         'invalid' => 'الرمز المقدم غير صالح.',
-        'setup' => [
-            'title' => 'إعداد التوثيق الثنائي',
-            'help' => 'لا يمكن مسح الرمز؟ أدخل الرمز أدناه في تطبيقك:',
-            'field' => 'أدخل الرمز',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => 'تعطيل التوثيق الثنائي',
             'field' => 'أدخل الرمز',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
