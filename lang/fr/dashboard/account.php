21fb1768787812893a9dd1f8fc7ae5533c0c4bde
Lance Pioch <lancepioch@gmail.com>
2024-04-26 00:26:00 -0400

    New translations account.php (French)

--- lang/fr/dashboard/account.php
+++ lang/fr/dashboard/account.php
@@ -1,28 +1,48 @@
 <?php
 
 return [
+    'title' => 'Account Overview',
     'email' => [
-        'title' => 'Mettre à jour votre adresse e-mail',
-        'updated' => 'Votre adresse e-mail a été mise à jour.',
+        'title' => 'Update Email Address',
+        'button' => 'Update Email',
+        'updated' => 'Your primary email has been updated.',
     ],
     'password' => [
-        'title' => 'Modifier votre mot de passe',
-        'requirements' => 'Votre nouveau mot de passe doit comporter au moins 8 caractères.',
+        'title' => 'Update Password',
+        'button' => 'Update Password',
+        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'validation' => [
+            'account_password' => 'You must provide your account password.',
+            'current_password' => 'You must provide your current password.',
+            'password_confirmation' => 'Password confirmation does not match the password you entered.',
+        ],
         'updated' => 'Votre mot de passe a été mis à jour.',
     ],
     'two_factor' => [
+        'title' => 'Two-Step Verification',
         'button' => 'Configurer l\'authentificateur à deux facteurs',
         'disabled' => 'L\'authentification à deux facteurs a été désactivée sur votre compte. Vous ne serez plus invité à fournir le code généré par votre appareil lors de votre connexion.',
         'enabled' => 'L\'authentification à deux facteurs a été activée sur votre compte ! Désormais, lorsque vous vous connectez, vous devrez fournir le code généré par votre appareil.',
         'invalid' => 'Le jeton fourni est invalide.',
-        'setup' => [
-            'title' => 'Configurer l\'authentification à deux facteurs',
-            'help' => 'Impossible de scanner le code QR ? Entrez le code ci-dessous dans votre application :',
-            'field' => 'Saisir un jeton',
+        'enable' => [
+            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
+            'button' => 'Enable Two-Step',
         ],
         'disable' => [
+            'help' => 'Two-step verification is currently enabled on your account.',
             'title' => 'Désactiver l\'authentification à deux facteurs',
             'field' => 'Saisir un jeton',
+            'button' => 'Disable Two-Step',
+        ],
+        'setup' => [
+            'title' => 'Enable Two-Step Verification',
+            'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
+            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+        ],
+
+        'required' => [
+            'title' => '2-Factor Required',
+            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
         ],
     ],
 ];
