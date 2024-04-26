5c0127102f5aa1058d6304c49a676ac984288108
Lance Pioch <lancepioch@gmail.com>
2024-04-26 03:58:30 -0400

    New translations eggs.php (Czech)

--- lang/cs/admin/eggs.php
+++ lang/cs/admin/eggs.php
@@ -17,8 +17,8 @@ return [
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'name' => 'Jednoduchý, člověkem čitelný název pro toto vejce.',
+        'description' => 'Popis tohoto vejce, který se zobrazí v panelu podle potřeby.',
         'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
         'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
         'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
