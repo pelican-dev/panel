f1640e2ed33a65d649069267fcf9b7bb09058089
Lance Pioch <lancepioch@gmail.com>
2024-04-25 23:28:10 -0400

    New translations node.php (Thai)

--- lang/th/admin/node.php
+++ lang/th/admin/node.php
@@ -2,12 +2,12 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'FQDN หรือที่อยู่ IP ที่ระบุไม่สามารถติดต่อได้หรือที่อยู่ IP ที่ถูกต้อง',
+        'fqdn_required_for_ssl' => 'สำหรับการใช้ SSL กับโหนดนี้ จำเป็นต้องมีชื่อโดเมนที่ระบุอย่างเต็มรูปแบบซึ่งสามารถแปลเป็นที่อยู่ IP สาธารณะ',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
-        'node_deleted' => 'Node has been successfully removed from the panel.',
+        'allocations_added' => 'เพิ่มพอร์ตไปยังโหนดนี้สำเร็จแล้ว',
+        'node_deleted' => 'โหนดถูกลบออกจากแผงเรียบร้อยแล้ว',
         'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
         'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
         'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
