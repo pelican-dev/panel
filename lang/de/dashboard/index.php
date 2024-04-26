f16896eced9567507667744c445c12a95de5c820
Lance Pioch <lancepioch@gmail.com>
2024-04-26 11:45:29 -0400

    New translations index.php (German)

--- lang/de/dashboard/index.php
+++ lang/de/dashboard/index.php
@@ -2,14 +2,14 @@
 
 return [
     'title' => 'Dashboard',
-    'showing-your-servers' => 'Showing your servers',
-    'showing-others-servers' => "Showing other's servers",
-    'no-other-servers' => 'There are no other servers to display.',
+    'showing-your-servers' => 'Zeige deine Server',
+    'showing-others-servers' => "Anderen Server anzeigen",
+    'no-other-servers' => 'Es gibt keine anderen Server zum Anzeigen.',
     'no-servers-associated' => 'There are no servers associated with your account.',
 
-    'content_tabs' => 'Content tabs',
-    'overview' => 'Overview',
-    'heading' => 'Welcome to Pelican!',
+    'content_tabs' => 'Inhaltsreiter',
+    'overview' => 'Übersicht',
+    'heading' => 'Willkommen zu Pelican!',
     'expand_sections' => 'You can expand the following sections:',
     'sections' => [
         'intro-developers' => [
@@ -17,7 +17,7 @@ return [
             'content' => 'Thank you for trying out the development version!',
             'extra_note' => 'If you run into any issues, please report them on GitHub.',
             'button_issues' => 'Create Issue',
-            'button_features' => 'Discuss Features',
+            'button_features' => 'Funktionen diskutieren',
         ],
         'intro-first-node' => [
             'heading' => 'No Nodes Detected',
@@ -26,17 +26,17 @@ return [
             'button_label' => 'Create first Node in Pelican',
         ],
         'intro-support' => [
-            'heading' => 'Support Pelican',
+            'heading' => 'Pelican unterstützen',
             'content' => 'Thank you for using Pelican, this could only be achieved through the support of you, our contributors, and the rest of our supporters!',
             'extra_note' => 'We appreciate any and all support from anybody.',
-            'button_translate' => 'Help Translate',
-            'button_donate' => 'Donate Directly',
+            'button_translate' => 'Hilf beim Übersetzen',
+            'button_donate' => 'Direkt spenden',
         ],
         'intro-help' => [
-            'heading' => 'Need Help?',
+            'heading' => 'Brauchst du Hilfe?',
             'content' => 'Check out the documentation first! If you still need assistance then, fly onto our Discord server!',
-            'button_docs' => 'Read Documentation',
-            'button_discord' => 'Get Help in Discord',
+            'button_docs' => 'Dokumentation lesen',
+            'button_discord' => 'Erhalte Hilfe auf Discord',
         ],
     ],
 
