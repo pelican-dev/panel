ce320efbf97839b4e144d25a0b9434a1b004c10c
Lance Pioch <lancepioch@gmail.com>
2024-04-26 15:11:36 -0400

    New translations account.php (French)

--- lang/fr/dashboard/account.php
+++ lang/fr/dashboard/account.php
@@ -1,48 +1,48 @@
 <?php
 
 return [
-    'title' => 'Account Overview',
+    'title' => 'Aperçu du compte',
     'email' => [
         'title' => 'Update Email Address',
         'button' => 'Update Email',
-        'updated' => 'Your primary email has been updated.',
+        'updated' => 'Votre adresse e-mail principale a été mise à jour.',
     ],
     'password' => [
-        'title' => 'Update Password',
-        'button' => 'Update Password',
-        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'title' => 'Mettre à jour le mot de passe',
+        'button' => 'Mettre à jour le mot de passe',
+        'requirements' => 'Votre nouveau mot de passe doit comporter au moins 8 caractères et être unique à ce site web.',
         'validation' => [
             'account_password' => 'You must provide your account password.',
-            'current_password' => 'You must provide your current password.',
+            'current_password' => 'Vous devez fournir votre mot de passe actuel.',
             'password_confirmation' => 'Password confirmation does not match the password you entered.',
         ],
         'updated' => 'Votre mot de passe a été mis à jour.',
     ],
     'two_factor' => [
-        'title' => 'Two-Step Verification',
+        'title' => 'Vérification en deux étapes',
         'button' => 'Configurer l\'authentificateur à deux facteurs',
         'disabled' => 'L\'authentification à deux facteurs a été désactivée sur votre compte. Vous ne serez plus invité à fournir le code généré par votre appareil lors de votre connexion.',
         'enabled' => 'L\'authentification à deux facteurs a été activée sur votre compte ! Désormais, lorsque vous vous connectez, vous devrez fournir le code généré par votre appareil.',
         'invalid' => 'Le jeton fourni est invalide.',
         'enable' => [
-            'help' => 'You do not currently have two-step verification enabled on your account. Click the button below to begin configuring it.',
-            'button' => 'Enable Two-Step',
+            'help' => 'Vous n\'avez pas encore activé la vérification en deux étapes sur votre compte. Cliquez sur le bouton ci-dessous pour commencer à la configurer.',
+            'button' => 'Vérification en deux étapes',
         ],
         'disable' => [
-            'help' => 'Two-step verification is currently enabled on your account.',
+            'help' => 'La vérification en deux étapes est actuellement activée sur votre compte.',
             'title' => 'Désactiver l\'authentification à deux facteurs',
             'field' => 'Saisir un jeton',
-            'button' => 'Disable Two-Step',
+            'button' => 'Désactiver la vérification en deux étapes',
         ],
         'setup' => [
-            'title' => 'Enable Two-Step Verification',
+            'title' => 'Activer la vérification en deux étapes',
             'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
             'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
         ],
 
         'required' => [
             'title' => '2-Factor Required',
-            'description' => 'Your account must have two-factor authentication enabled in order to continue.',
+            'description' => 'Votre compte doit avoir l\'authentification à deux facteurs activée pour continuer.',
         ],
     ],
 ];
