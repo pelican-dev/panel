401e5c0c943d71bbb9408f754ba8815d901a577e
Lance Pioch <lancepioch@gmail.com>
2024-04-27 14:22:01 -0400

    New translations eggs.php (Indonesian)

--- lang/id/admin/eggs.php
+++ lang/id/admin/eggs.php
@@ -11,18 +11,18 @@ return [
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'Variabel “:variable” telah dihapus dan tidak akan lagi tersedia untuk server setelah dibangun kembali.',
+            'variable_updated' => 'Variabel “:variable” telah diperbarui. Anda perlu membangun ulang semua server yang menggunakan variabel ini untuk menerapkan perubahan.',
+            'variable_created' => 'Variabel baru telah berhasil dibuat dan ditetapkan ke telur ini.',
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'name' => 'Nama yang simpel dan mudah dibaca oleh manusia untuk digunakan sebagai pengenal Telur ini.',
+        'description' => 'Deskripsi Telur yang akan ditampilkan di seluruh Panel sesuai kebutuhan.',
         'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
-        'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
+        'author' => 'Penulis versi Telur ini. Mengunggah konfigurasi Telur baru dari pembuat yang berbeda akan mengubahnya.',
         'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
-        'startup' => 'The default startup command that should be used for new servers using this Egg.',
+        'startup' => 'Perintah startup default yang sebaiknya digunakan untuk server baru yang menggunakan Egg ini.',
         'docker_images' => 'The docker images available to servers using this egg.',
     ],
 ];
