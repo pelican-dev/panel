6db3e5135cec5d5eba420ccf46fecae02ddf633f
Lance Pioch <lancepioch@gmail.com>
2024-04-27 14:22:00 -0400

    New translations validation.php (Indonesian)

--- lang/id/validation.php
+++ lang/id/validation.php
@@ -65,7 +65,7 @@ return [
     'numeric' => 'The :attribute must be a number.',
     'present' => 'The :attribute field must be present.',
     'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field is required.',
+    'required' => 'Bidang :attribute wajib diisi.',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
