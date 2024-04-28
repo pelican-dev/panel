5bef99611b0e1725710789a849d6b91afc79b0a5
Lance Pioch <git@lance.sh>
2024-04-27 23:08:49 -0400

    Remove unused imports

--- app/Filament/Resources/ServerResource/RelationManagers/AllocationsRelationManager.php
+++ app/Filament/Resources/ServerResource/RelationManagers/AllocationsRelationManager.php
@@ -8,8 +8,6 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class AllocationsRelationManager extends RelationManager
 {
