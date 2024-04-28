1705cb42119a16258c3b0f0f4d08cbc550fc2a97
Lance Pioch <lancepioch@gmail.com>
2024-04-28 06:33:04 -0400

    New translations index.php (Japanese)

--- lang/ja/admin/index.php
+++ lang/ja/admin/index.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'title' => 'Administration',
+    'title' => '管理',
     'header' => [
-        'title' => 'Administrative Overview',
-        'subtitle' => 'A quick glance at your system.',
+        'title' => '管理概要',
+        'subtitle' => 'システムを一目で確認できます。',
     ],
     'content' => [
-        'title' => 'System Information',
-        'up-to-date' => 'You are running Pelican version <code>:version</code>. Your panel is up-to-date!',
-        'not-up-to-date1' => 'Your panel is <strong>not up-to-date!</strong> The latest version is',
-        'not-up-to-date2' => 'and you are currently running version <code>:version</code>.',
-        'get-help' => 'Get Help <small>(via Discord)</small>',
-        'documentation' => 'Documentation',
-        'github' => 'Github',
-        'support-the-project' => 'Support the Project',
+        'title' => 'システム情報',
+        'up-to-date' => '最新のPelicanバージョン<code>:version</code>を使用しています！ ',
+        'not-up-to-date1' => 'このPanelは<strong>最新ではありません！</strong>現在の最新バージョンは',
+        'not-up-to-date2' => 'で、現在Pelicanバージョン<code>:version</code>を使用しています。',
+        'get-help' => 'ヘルプを得る<small>(Discord経由)</small>',
+        'documentation' => 'ドキュメント',
+        'github' => 'GitHub',
+        'support-the-project' => 'プロジェクトを支援',
     ],
 ];
