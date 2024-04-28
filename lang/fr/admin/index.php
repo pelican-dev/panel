753ef7f77d522c71348d29cd5aa1a0c342b0be7c
Lance Pioch <lancepioch@gmail.com>
2024-04-28 10:18:59 -0400

    New translations index.php (French)

--- lang/fr/admin/index.php
+++ lang/fr/admin/index.php
@@ -3,13 +3,13 @@
 return [
     'title' => 'Administration',
     'header' => [
-        'title' => 'Aperçu administratif',
+        'title' => 'Vue d\'ensemble administratif',
         'subtitle' => 'Un rapide coup d\'œil à votre système.',
     ],
     'content' => [
         'title' => 'Informations systèmes',
-        'up-to-date' => 'Vous utilisez la version <code>:version</code> de Pelican. Votre panel est à jour !',
-        'not-up-to-date1' => 'Votre panel n\'est <strong>pas à jour !</strong> La dernière version est',
+        'up-to-date' => 'Vous utilisez la version <code>:version</code> de Pelican. Votre logiciel est à jour !',
+        'not-up-to-date1' => 'Votre logiciel n\'est <strong>pas à jour !</strong> La dernière version est',
         'not-up-to-date2' => 'et vous utilisez actuellement la version <code>:version</code>.',
         'get-help' => 'Obtenir de l\'aide <small>(via Discord)</small>',
         'documentation' => 'Documentation',
