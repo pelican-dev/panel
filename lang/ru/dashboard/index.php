6e89cdd174fc81c4be651489ac5a5e8327eacfd1
Lance Pioch <lancepioch@gmail.com>
2024-04-27 20:52:16 -0400

    New translations index.php (Russian)

--- lang/ru/dashboard/index.php
+++ lang/ru/dashboard/index.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'title' => 'Dashboard',
+    'title' => 'Панель Управления',
     'showing-your-servers' => 'Showing your servers',
     'showing-others-servers' => "Showing other's servers",
     'no-other-servers' => 'There are no other servers to display.',
     'no-servers-associated' => 'There are no servers associated with your account.',
 
     'content_tabs' => 'Content tabs',
-    'overview' => 'Overview',
+    'overview' => 'Обзор',
     'heading' => 'Добро пожаловать в Pelican!',
     'expand_sections' => 'You can expand the following sections:',
     'sections' => [
@@ -22,14 +22,14 @@ return [
         'intro-first-node' => [
             'heading' => 'No Nodes Detected',
             'content' => "It looks like you don't have any Nodes set up yet, but don't worry because you click the action button to create your first one!",
-            'extra_note' => 'If you run into any issues, please report them on GitHub.',
+            'extra_note' => 'Если вы столкнулись с какими-либо проблемами, пожалуйста, сообщите о них на GitHub.',
             'button_label' => 'Create first Node in Pelican',
         ],
         'intro-support' => [
             'heading' => 'Поддержать Pelican',
             'content' => 'Thank you for using Pelican, this could only be achieved through the support of you, our contributors, and the rest of our supporters!',
             'extra_note' => 'We appreciate any and all support from anybody.',
-            'button_translate' => 'Help Translate',
+            'button_translate' => 'Помочь с Переводом',
             'button_donate' => 'Donate Directly',
         ],
         'intro-help' => [
