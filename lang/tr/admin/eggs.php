5df72c0aaf09b49bf0beb2fc083403ac9862e7b5
Lance Pioch <lancepioch@gmail.com>
2024-04-28 10:18:57 -0400

    New translations eggs.php (Turkish)

--- lang/tr/admin/eggs.php
+++ lang/tr/admin/eggs.php
@@ -17,12 +17,12 @@ return [
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'name' => 'Bu Egg için tanımlayıcı olarak kullanılacak basit ve okunabilir bir isim.',
+        'description' => 'Bu Egg için gerekli oldukça Panelde gösterilecek bir açıklama.',
         'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
-        'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
+        'author' => 'Bu Egg\'in bu sürümünün sahibi. Başkası tarafından yapılan bir Egg dosyası yüklenirse bu alan değişir.',
         'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
-        'startup' => 'The default startup command that should be used for new servers using this Egg.',
-        'docker_images' => 'The docker images available to servers using this egg.',
+        'startup' => 'Bu Egg\'i kullanan sunucuların varsayılan başlatma komutu.',
+        'docker_images' => 'Bu Egg\'i kullanan sunuculara mevcut Docker Image\'leri',
     ],
 ];
