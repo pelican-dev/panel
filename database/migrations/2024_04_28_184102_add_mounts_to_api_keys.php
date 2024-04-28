32905cf05b94c2560bcc12274a07a45f8a2f8de6
Boy132 <mail@boy132.de>
2024-04-28 18:43:17 +0200

    add migration for "r_mounts" column

---
+++ database/migrations/2024_04_28_184102_add_mounts_to_api_keys.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('api_keys', function (Blueprint $table) {
+            $table->unsignedTinyInteger('r_mounts')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('api_keys', function (Blueprint $table) {
+            $table->dropColumn('r_mounts');
+        });
+    }
+};
