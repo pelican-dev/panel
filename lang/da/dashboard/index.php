700861fee0b6edb3cbd491683a45ed0de0c32ff6
Lance Pioch <lancepioch@gmail.com>
2024-04-29 20:37:12 -0400

    New translations index.php (Danish)

--- lang/da/dashboard/index.php
+++ lang/da/dashboard/index.php
@@ -2,21 +2,21 @@
 
 return [
     'title' => 'Dashboard',
-    'showing-your-servers' => 'Showing your servers',
-    'showing-others-servers' => "Showing other's servers",
-    'no-other-servers' => 'There are no other servers to display.',
-    'no-servers-associated' => 'There are no servers associated with your account.',
+    'showing-your-servers' => 'Viser dine servere',
+    'showing-others-servers' => "Viser andres servere",
+    'no-other-servers' => 'Der er ingen andre servere at vise.',
+    'no-servers-associated' => 'Der er ingen servere tilknyttet din konto.',
 
     'content_tabs' => 'Content tabs',
-    'overview' => 'Overview',
-    'heading' => 'Welcome to Pelican!',
+    'overview' => 'Oversigt',
+    'heading' => 'Velkommen til Pelican!',
     'expand_sections' => 'You can expand the following sections:',
     'sections' => [
         'intro-developers' => [
-            'heading' => 'Information for Developers',
+            'heading' => 'Information til udviklere',
             'content' => 'Thank you for trying out the development version!',
             'extra_note' => 'If you run into any issues, please report them on GitHub.',
-            'button_issues' => 'Create Issue',
+            'button_issues' => 'Opret issue',
             'button_features' => 'Discuss Features',
         ],
         'intro-first-node' => [
@@ -26,17 +26,17 @@ return [
             'button_label' => 'Create first Node in Pelican',
         ],
         'intro-support' => [
-            'heading' => 'Support Pelican',
+            'heading' => 'Støt Pelican',
             'content' => 'Thank you for using Pelican, this could only be achieved through the support of you, our contributors, and the rest of our supporters!',
             'extra_note' => 'We appreciate any and all support from anybody.',
-            'button_translate' => 'Help Translate',
-            'button_donate' => 'Donate Directly',
+            'button_translate' => 'Hjælp os med at oversætte',
+            'button_donate' => 'Doner Direkte',
         ],
         'intro-help' => [
-            'heading' => 'Need Help?',
+            'heading' => 'Brug for hjælp?',
             'content' => 'Check out the documentation first! If you still need assistance then, fly onto our Discord server!',
-            'button_docs' => 'Read Documentation',
-            'button_discord' => 'Get Help in Discord',
+            'button_docs' => 'Læs dokumentation',
+            'button_discord' => 'Få hjælp på Discord',
         ],
     ],
 
