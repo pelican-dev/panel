083e75d0e53f17ce3441ba5bfaae1b996f9ab15b
Lance Pioch <lancepioch@gmail.com>
2024-04-30 00:01:08 -0400

    New translations eggs.php (Thai)

--- lang/th/admin/eggs.php
+++ lang/th/admin/eggs.php
@@ -2,27 +2,27 @@
 
 return [
     'notices' => [
-        'imported' => 'นำเข้าไข่นี้และตัวแปรที่เกี่ยวข้องเรียบร้อยแล้ว',
-        'updated_via_import' => 'ไข่นี้ได้รับการอัปเดตโดยใช้ไฟล์ที่นำเข้ามา',
-        'deleted' => 'ลบไข่ออกจากแผงควบคุมเรียบร้อยแล้ว',
-        'updated' => 'อัปเดตการตั้งค่าไข่สำเร็จแล้ว',
-        'script_updated' => 'สคริปต์การติดตั้งไข่ได้รับการอัปเดตแล้ว และจะทำงานทุกครั้งที่ติดตั้งเซิร์ฟเวอร์',
-        'egg_created' => 'วางไข่ใหม่เรียบร้อยแล้ว คุณจะต้องรีสตาร์ทดีมอนที่กำลังวิ่งอยู่เพื่อใช้ไข่ใหม่นี้',
+        'imported' => 'นำเข้า Egg นี้และตัวแปรที่เกี่ยวข้องเรียบร้อยแล้ว',
+        'updated_via_import' => 'Egg นี้ได้รับการอัปเดตโดยใช้ไฟล์ที่นำเข้ามา',
+        'deleted' => 'ลบ Egg ออกจากแผงควบคุมเรียบร้อยแล้ว',
+        'updated' => 'อัปเดตการตั้งค่า Egg สำเร็จแล้ว',
+        'script_updated' => 'สคริปต์การติดตั้ง Egg ได้รับการอัปเดตแล้ว และจะทำงานทุกครั้งที่ติดตั้งเซิร์ฟเวอร์',
+        'egg_created' => 'วาง Egg ใหม่เรียบร้อยแล้ว คุณจะต้องรีสตาร์ทดีมอนที่กำลังวิ่งอยู่เพื่อใช้ Egg ใหม่นี้',
     ],
     'variables' => [
         'notices' => [
             'variable_deleted' => 'ตัวแปร ":variable" ถูกลบแล้ว และจะไม่สามารถใช้ได้อีกต่อไปสำหรับเซิร์ฟเวอร์เมื่อสร้างใหม่แล้ว',
             'variable_updated' => 'อัปเดตตัวแปร ":variable" แล้ว คุณจะต้องสร้างเซิร์ฟเวอร์ใหม่โดยใช้ตัวแปรนี้เพื่อใช้การเปลี่ยนแปลง',
-            'variable_created' => 'สร้างตัวแปรใหม่และกำหนดให้กับไข่นี้สำเร็จแล้ว',
+            'variable_created' => 'สร้างตัวแปรใหม่และกำหนดให้กับ Egg นี้สำเร็จแล้ว',
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
-        'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
-        'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
-        'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
-        'startup' => 'The default startup command that should be used for new servers using this Egg.',
-        'docker_images' => 'The docker images available to servers using this egg.',
+        'name' => 'ชื่อที่เรียบง่ายและมนุษย์สามารถอ่านได้เพื่อใช้เป็นตัวระบุ Egg นี้',
+        'description' => 'คำอธิบายของ Egg นี้ที่จะแสดงทั่วทั้งแผงควบคุมตามความจำเป็น',
+        'uuid' => 'นี่คือตัวระบุสำหรับ Egg นี้ซึ่ง Wings ใช้เป็นตัวระบุ',
+        'author' => 'ผู้เขียน Egg เวอร์ชันนี้ การอัปโหลดการกำหนดค่า Egg ใหม่จากผู้เขียนรายอื่นจะเปลี่ยนผู้เขียน',
+        'force_outgoing_ip' => "บังคับให้การรับส่งข้อมูลเครือข่ายขาออกทั้งหมดมี IP ต้นทาง NATed เป็น IP ของ IP การจัดสรรหลักของเซิร์ฟเวอร์\nจำเป็นสำหรับเกมบางเกมเพื่อให้ทำงานได้อย่างถูกต้องเมื่อโหนดมีที่อยู่ IP สาธารณะหลายรายการ\nการเปิดใช้งานตัวเลือกนี้จะปิดใช้งานเครือข่ายภายในสำหรับเซิร์ฟเวอร์ใดๆ ที่ใช้ Egg นี้ ส่งผลให้ไม่สามารถเข้าถึงเซิร์ฟเวอร์อื่นๆ ภายในบนโหนดเดียวกันได้",
+        'startup' => 'คำสั่งเริ่มต้นที่ควรใช้กับเซิร์ฟเวอร์ใหม่ที่ใช้ Egg นี้',
+        'docker_images' => 'อิมเมจ Docker พร้อมใช้งานสำหรับเซิร์ฟเวอร์ที่ใช้ Egg นี้',
     ],
 ];
