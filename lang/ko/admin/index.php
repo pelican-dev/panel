4da3d7042d4b6bdafb34541e441bf6d92552866b
Lance Pioch <lancepioch@gmail.com>
2024-04-29 20:37:15 -0400

    New translations index.php (Korean)

--- lang/ko/admin/index.php
+++ lang/ko/admin/index.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'title' => 'Administration',
+    'title' => '관리',
     'header' => [
-        'title' => 'Administrative Overview',
-        'subtitle' => 'A quick glance at your system.',
+        'title' => '관리',
+        'subtitle' => '시스템 한눈에 살펴보기.',
     ],
     'content' => [
-        'title' => 'System Information',
-        'up-to-date' => 'You are running Pelican version <code>:version</code>. Your panel is up-to-date!',
-        'not-up-to-date1' => 'Your panel is <strong>not up-to-date!</strong> The latest version is',
-        'not-up-to-date2' => 'and you are currently running version <code>:version</code>.',
-        'get-help' => 'Get Help <small>(via Discord)</small>',
-        'documentation' => 'Documentation',
+        'title' => '시스템 정보',
+        'up-to-date' => 'Pelican 버전 <code>:version</code> 을(를) 사용중입니다. 패널이 최신버전입니다!',
+        'not-up-to-date1' => '패널이 <strong>최신버전이 아닙니다!</strong> 최신 버전은',
+        'not-up-to-date2' => '이고 현재 버전은 <code>:version</code> 입니다.',
+        'get-help' => '도움 받기 <small>(Discord 에서)</small>',
+        'documentation' => '문서',
         'github' => 'Github',
-        'support-the-project' => 'Support the Project',
+        'support-the-project' => '프로젝트 지원하기',
     ],
 ];
