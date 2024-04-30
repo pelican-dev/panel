58733d6925e53d7198b0e4d65f06b4c223a482cc
Lance Pioch <lancepioch@gmail.com>
2024-04-29 20:37:13 -0400

    New translations index.php (Danish)

--- lang/da/admin/index.php
+++ lang/da/admin/index.php
@@ -9,11 +9,11 @@ return [
     'content' => [
         'title' => 'Systemoplysninger',
         'up-to-date' => 'Du kører Pelican version <code>:version</code>. Dit panel er up-to-date!',
-        'not-up-to-date1' => 'Your panel is <strong>not up-to-date!</strong> The latest version is',
-        'not-up-to-date2' => 'and you are currently running version <code>:version</code>.',
-        'get-help' => 'Get Help <small>(via Discord)</small>',
-        'documentation' => 'Documentation',
-        'github' => 'Github',
-        'support-the-project' => 'Support the Project',
+        'not-up-to-date1' => 'Dit panel er <strong>ikke up-to-date!</strong> Den nyeste version er',
+        'not-up-to-date2' => 'og du kører i øjeblikket version <code>:version</code>.',
+        'get-help' => 'Få Hjælp <small>(via Discord)</small>',
+        'documentation' => 'Dokumentation',
+        'github' => 'GitHub',
+        'support-the-project' => 'Støt projektet',
     ],
 ];
