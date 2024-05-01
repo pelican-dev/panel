002f6c0003c8adcba1fed0e9e151416b9f4a64e7
Lance Pioch <lancepioch@gmail.com>
2024-05-01 08:53:39 -0400

    New translations user.php (Bulgarian)

---
+++ lang/bg/admin/user.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'exceptions' => [
+        'user_has_servers' => 'Cannot delete a user with active servers attached to their account. Please delete their servers before continuing.',
+        'user_is_self' => 'Cannot delete your own user account.',
+    ],
+    'notices' => [
+        'account_created' => 'Account has been created successfully.',
+        'account_updated' => 'Account has been successfully updated.',
+    ],
+    'last_admin' => [
+        'hint' => 'This is the last root administrator!',
+        'helper_text' => 'You must have at least one root administrator in your system.',
+    ],
+    'root_admin' => 'Administrator (Root)',
+    'language' => [
+        'helper_text1' => 'Your language (:state) has not been translated yet!\nBut never fear, you can help fix that by',
+        'helper_text2' => 'contributing directly here',
+    ],
+];
