aa8d78c13da3b2945056f09b49cbe354362a1013
Lance Pioch <lancepioch@gmail.com>
2024-05-01 08:53:43 -0400

    New translations index.php (Bulgarian)

---
+++ lang/bg/admin/index.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Администрация',
+    'header' => [
+        'title' => 'Администрационен преглед',
+        'subtitle' => 'Бърз поглед към система ви.',
+    ],
+    'content' => [
+        'title' => 'Системна информация',
+        'up-to-date' => 'Използваш Pelican версия <code>:Версия</code>. Твоят панел е актуален!',
+        'not-up-to-date1' => 'Your panel is <strong>not up-to-date!</strong> The latest version is',
+        'not-up-to-date2' => 'и ти използваш версия <code>:Версия</code>.',
+        'get-help' => 'Получи малка помощ <small>(Чрез Discord)</small>',
+        'documentation' => 'Документация',
+        'github' => 'Github',
+        'support-the-project' => 'Подкрепи проекта',
+    ],
+];
