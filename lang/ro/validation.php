674f4b6d3165c3f105690e49a95d3c1bd4f8723c
Lance Pioch <lancepioch@gmail.com>
2024-05-02 11:03:54 -0400

    New translations validation.php (Romanian)

--- lang/ro/validation.php
+++ lang/ro/validation.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'accepted' => 'The :attribute must be accepted.',
+    'accepted' => ':attribute trebuie să fie acceptat.',
     'active_url' => 'The :attribute is not a valid URL.',
     'after' => 'The :attribute must be a date after :date.',
     'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
