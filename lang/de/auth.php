8fc3b0002e42bcca694b7e52301ef035860a2b13
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-05-05 22:03:22 +0200

    Pint fixes on the whole lang folder
    There were a few pint issues that should now be resolved

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -1,55 +1,18 @@
 <?php
 
 return [
-    'return_to_login' => 'Zurück zum Login',
+    'sign_in' => 'Anmelden',
+    'go_to_login' => 'Zum Login',
     'failed' => 'Es wurde kein Konto mit diesen Zugangsdaten gefunden.',
 
-    'login' => [
-        'title' => 'Zum Fortfahren anmelden',
-        'button' => 'Anmelden',
-        'required' => [
-            'username_or_email' => 'Es muss ein Benutzername oder eine E-Mail angegeben werden.',
-            'password' => 'Bitte gebe dein Account-Passwort ein.',
-        ],
-    ],
-
     'forgot_password' => [
-        'title' => 'Passwort-Reset anfordern',
         'label' => 'Passwort vergessen?',
         'label_help' => 'Geben Sie Ihre E-Mail Adresse ein, um Anweisungen zum Zurücksetzen Ihres Passworts zu erhalten.',
-        'button' => 'E-Mail senden',
-        'required' => [
-            'email' => 'Eine gültige E-Mail-Adresse muss angegeben werden, um fortzufahren.',
-        ],
+        'button' => 'Konto wiederherstellen',
     ],
 
     'reset_password' => [
-        'title' => 'Passwort zurücksetzen',
-        'button' => 'Passwort zurücksetzen',
-        'new_password' => 'Neues Passwort',
-        'confirm_new_password' => 'Neues Passwort bestätigen',
-        'requirement' => [
-            'password' => 'Passwörter müssen mindestens 8 Zeichen lang sein.',
-        ],
-        'required' => [
-            'password' => 'Ein neues Passwort ist erforderlich.',
-            'password_confirmation' => 'Dein neues Passwort stimmt nicht überein.',
-        ],
-        'validation' => [
-            'password' => 'Dein neues Passwort sollte mindestens 8 Zeichen lang sein.',
-            'password_confirmation' => 'Dein neues Passwort stimmt nicht überein.',
-        ],
-    ],
-
-    'checkpoint' => [
-        'title' => 'Geräte-Checkpoint',
-        'recovery_code' => 'Wiederherstellungscode',
-        'recovery_code_description' => 'Um fortzufahren geben Sie einen ihrer Recovery-Codes ein, die generiert wurden als Sie 2-Faktor-Authentifizierung auf diesem Account aktiviert haben.',
-        'authentication_code' => 'Authentifizierungscode',
-        'authentication_code_description' => 'Geben Sie den von Ihrem Gerät generierten Zwei-Faktor-Token ein.',
-        'button' => 'Weiter',
-        'lost_device' => "Ich habe mein Gerät verloren",
-        'have_device' => 'Ich habe mein Gerät',
+        'button' => 'Zurücksetzen und Anmelden',
     ],
 
     'two_factor' => [
@@ -59,6 +22,6 @@ return [
     ],
 
     'throttle' => 'Zu viele Anmeldeversuche. Bitte versuche es in :seconds Sekunden erneut.',
-    'password_requirements' => 'Das Passwort muss mindestens 8 Zeichen lang sein und sollte auf dieser Seite einzigartig sein.',
-    '2fa_must_be_enabled' => 'Der Administrator hat festgelegt, dass die 2-Faktor-Authentifizierung für deinen Account aktiviert sein muss, damit du das Panel nutzen kannst.',
+    'password_requirements' => 'Das Passwort muss mindestens 8 Zeichen lang sein und sollte auf dieser Seite eindeutig sein.',
+    '2fa_must_be_enabled' => 'Der Administrator hat festgelegt, dass die 2-Faktor-Authentifizierung für deinen Account angeschaltet sein muss, damit du dieses Panel nutzen kannst.',
 ];
--- lang/de/dashboard/index.php
+++ lang/de/dashboard/index.php
@@ -1,45 +1,6 @@
 <?php
 
 return [
-    'title' => 'Dashboard',
-    'showing-your-servers' => 'Zeige deine Server',
-    'showing-others-servers' => "Anderen Server anzeigen",
-    'no-other-servers' => 'Es gibt keine anderen Server zum Anzeigen.',
-    'no-servers-associated' => 'Es sind keine Server mit deinem Konto verbunden.',
-
-    'content_tabs' => 'Inhaltsreiter',
-    'overview' => 'Übersicht',
-    'heading' => 'Willkommen zu Pelican!',
-    'expand_sections' => 'Du kannst die folgenden Bereiche erweitern:',
-    'sections' => [
-        'intro-developers' => [
-            'heading' => 'Informationen für Entwickler',
-            'content' => 'Vielen Dank für das ausprobieren der Entwicklungsversion!',
-            'extra_note' => 'Wenn du Probleme hast, melde sie bitte auf GitHub.',
-            'button_issues' => 'Problem erstellen',
-            'button_features' => 'Funktionen diskutieren',
-        ],
-        'intro-first-node' => [
-            'heading' => 'Keine Nodes erkannt',
-            'content' => "Es sieht so aus als hätten Sie noch keine Nodes erstellt, aber keine Sorge drücken sie den Action-Button, um ihre erste zu erstellen!",
-            'extra_note' => 'Wenn du Probleme hast, melde sie bitte auf GitHub.',
-            'button_label' => 'Erste Node in Pelican erstellen',
-        ],
-        'intro-support' => [
-            'heading' => 'Pelican unterstützen',
-            'content' => 'Vielen Dank für die Verwendung von Pelican, dies konnte nur durch die Unterstützung von Ihnen, unseren Mitwirkenden und den Rest unserer Unterstützer erreicht werden!',
-            'extra_note' => 'Wir freuen uns über jede Unterstützung von jedermann.',
-            'button_translate' => 'Hilf beim Übersetzen',
-            'button_donate' => 'Direkt spenden',
-        ],
-        'intro-help' => [
-            'heading' => 'Brauchst du Hilfe?',
-            'content' => 'Schauen Sie sich zuerst die Dokumentation an! Wenn Sie dann noch immer Hilfe benötigen, fliege auf unseren Discord Server!',
-            'button_docs' => 'Dokumentation lesen',
-            'button_discord' => 'Erhalte Hilfe auf Discord',
-        ],
-    ],
-
     'search' => 'Nach Servern suchen...',
     'no_matches' => 'Es wurden keine Server gefunden, die den angegebenen Suchkriterien entsprechen.',
     'cpu_title' => 'CPU',
