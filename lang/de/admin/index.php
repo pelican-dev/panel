e58720e126423334d94c9fa9d985ccfdf6cf00d0
Lance Pioch <lancepioch@gmail.com>
2024-05-05 15:51:30 -0400

    New translations index.php (German)

--- lang/de/admin/index.php
+++ lang/de/admin/index.php
@@ -7,7 +7,7 @@ return [
         'subtitle' => 'Ein kurzer Blick auf dein System.',
     ],
     'content' => [
-        'title' => 'System Informationen',
+        'title' => 'System-Informationen',
         'up-to-date' => 'Du verwendest die Pelican Version <code>:version</code>. Dein Panel ist aktuell!',
         'not-up-to-date1' => 'Dein Panel ist <strong>nicht auf dem neuesten Stand!</strong> Die neueste Version ist',
         'not-up-to-date2' => 'und du verwendest zur Zeit die Version <code>:version</code>.',
