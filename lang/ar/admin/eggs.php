ce9135b58f7ac84bbdaf3e1833f02733ec879b54
Lance Pioch <lancepioch@gmail.com>
2024-05-05 09:55:11 -0400

    New translations eggs.php (Arabic)

--- lang/ar/admin/eggs.php
+++ lang/ar/admin/eggs.php
@@ -17,12 +17,12 @@ return [
         ],
     ],
     'descriptions' => [
-        'name' => 'A simple, human-readable name to use as an identifier for this Egg.',
-        'description' => 'A description of this Egg that will be displayed throughout the Panel as needed.',
+        'name' => 'اسم بسيط قابل للقراءة البشرية لاستخدامه كمعرف لهذه البيضة.',
+        'description' => 'وصف لهذه البيضة التي سيتم عرضها في جميع أقسام اللوحة حسب الحاجة.',
         'uuid' => 'This is the globally unique identifier for this Egg which Wings uses as an identifier.',
         'author' => 'The author of this version of the Egg. Uploading a new Egg configuration from a different author will change this.',
         'force_outgoing_ip' => "Forces all outgoing network traffic to have its Source IP NATed to the IP of the server's primary allocation IP.\nRequired for certain games to work properly when the Node has multiple public IP addresses.\nEnabling this option will disable internal networking for any servers using this egg, causing them to be unable to internally access other servers on the same node.",
-        'startup' => 'The default startup command that should be used for new servers using this Egg.',
+        'startup' => 'أمر بدء التشغيل الافتراضي الذي يجب استخدامه للخوادم الجديدة باستخدام هذه البيضة.',
         'docker_images' => 'The docker images available to servers using this egg.',
     ],
 ];
