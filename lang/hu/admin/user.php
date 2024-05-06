6c2074194933a7c2cb23860f72b9dd685df162a3
Lance Pioch <lancepioch@gmail.com>
2024-05-06 01:40:41 -0400

    New translations user.php (Hungarian)

--- lang/hu/admin/user.php
+++ lang/hu/admin/user.php
@@ -2,7 +2,7 @@
 
 return [
     'exceptions' => [
-        'user_has_servers' => 'Nem törölhető olyan felhasználó, amihez aktív szerver van társítva. Kérlek előbb töröld a szerverét a folytatáshoz.',
+        'user_has_servers' => 'Nem törölhető olyan felhasználó, amihez aktív szerver van társítva. Kérlek előbb töröld az összes szerverét a folytatáshoz.',
         'user_is_self' => 'A saját felhasználói fiókod nem törölheted.',
     ],
     'notices' => [
