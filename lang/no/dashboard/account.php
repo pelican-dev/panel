c8971e7a3fa278186d15345124bab2178a419f15
Lance Pioch <lancepioch@gmail.com>
2024-05-06 01:41:10 -0400

    New translations account.php (Norwegian)

--- lang/no/dashboard/account.php
+++ lang/no/dashboard/account.php
@@ -3,14 +3,14 @@
 return [
     'title' => 'Account Overview',
     'email' => [
-        'title' => 'Update Email Address',
+        'title' => 'Oppdater e-postadressen din',
         'button' => 'Update Email',
-        'updated' => 'Your primary email has been updated.',
+        'updated' => 'Din epost har blitt oppdatert',
     ],
     'password' => [
-        'title' => 'Update Password',
+        'title' => 'Endre passoret ditt',
         'button' => 'Update Password',
-        'requirements' => 'Your new password should be at least 8 characters in length and unique to this website.',
+        'requirements' => 'Ditt nye passord må ha minst 8 tegn',
         'validation' => [
             'account_password' => 'You must provide your account password.',
             'current_password' => 'You must provide your current password.',
@@ -35,9 +35,9 @@ return [
             'button' => 'Disable Two-Step',
         ],
         'setup' => [
-            'title' => 'Enable Two-Step Verification',
+            'title' => 'Sett opp tofaktorautentisering',
             'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
-            'help' => 'Scan the QR code above using the two-step authentication app of your choice. Then, enter the 6-digit code generated into the field below.',
+            'help' => 'Can\'t scan the code? Enter the code below into your application:',
         ],
 
         'required' => [
