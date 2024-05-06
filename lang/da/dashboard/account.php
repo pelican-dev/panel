e4fb9021ae09ad073a5aca57b990ea62b410d226
Lance Pioch <lancepioch@gmail.com>
2024-05-06 01:40:53 -0400

    New translations account.php (Danish)

--- lang/da/dashboard/account.php
+++ lang/da/dashboard/account.php
@@ -3,14 +3,14 @@
 return [
     'title' => 'Konto Oversigt',
     'email' => [
-        'title' => 'Opdater din e-mail',
+        'title' => 'Update your email',
         'button' => 'Opdater Email',
-        'updated' => 'Din e-mailadresse er blevet opdateret.',
+        'updated' => 'Your email address has been updated.',
     ],
     'password' => [
-        'title' => 'Skift din adgangskode',
+        'title' => 'Change your password',
         'button' => 'Opdater Adgangskode',
-        'requirements' => 'Din nye adgangskode skal være mindst 8 tegn lang.',
+        'requirements' => 'Your new password should be at least 8 characters in length.',
         'validation' => [
             'account_password' => 'Du skal angive adgangskoden til din konto.',
             'current_password' => 'Du skal angive din nuværende adgangskode.',
@@ -35,9 +35,9 @@ return [
             'button' => 'Disable Two-Step',
         ],
         'setup' => [
-            'title' => 'Opsætning af 2-faktor godkendelse',
+            'title' => 'Opsætning af 2-factor godkendelse',
             'subtitle' => "Help protect your account from unauthorized access. You'll be prompted for a verification code each time you sign in.",
-            'help' => 'Kan ikke scanne koden? Indtast koden nedenfor i din applikation:',
+            'help' => 'Can\'t scan the code? Enter the code below into your application:',
         ],
 
         'required' => [
