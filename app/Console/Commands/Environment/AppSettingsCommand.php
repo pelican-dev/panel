72eb3ce4671c0a4059b4441e9a70c0c113c77e75
Lance Pioch <git@lance.sh>
2024-05-10 19:59:33 -0400

    Generate a new key if it doesn’t exist

--- app/Console/Commands/Environment/AppSettingsCommand.php
+++ app/Console/Commands/Environment/AppSettingsCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\Environment;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Kernel;
 use App\Traits\Commands\EnvironmentWriterTrait;
+use Illuminate\Support\Facades\Artisan;
 
 class AppSettingsCommand extends Command
 {
@@ -112,6 +113,10 @@ class AppSettingsCommand extends Command
         }
 
         $this->writeToEnvironment($this->variables);
+
+        if (!config('app.key')) {
+            Artisan::call('key:generate');
+        }
 
         $this->info($this->console->output());
 
