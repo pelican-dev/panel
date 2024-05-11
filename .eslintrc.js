f58c697d28dc7bc5541d56736c2f0d90066c4dc0
Lance Pioch <git@lance.sh>
2024-05-11 14:10:06 -0400

    Add this for the future

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
         'react/display-name': 0,
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
+        // 'react/no-unknown-property': ['error', { ignore: ['css'] }],
         // This setup is required to avoid a spam of errors when running eslint about React being
         // used before it is defined.
         //
