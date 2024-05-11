44cc5a8132b030029c00698466ab13ec5efab8d7
notCharles <charles@pelican.dev>
2024-05-10 21:31:32 -0400

    Sort server vars for front end

--- app/Http/Controllers/Api/Client/Servers/StartupController.php
+++ app/Http/Controllers/Api/Client/Servers/StartupController.php
@@ -31,7 +31,7 @@ class StartupController extends ClientApiController
         $startup = $this->startupCommandService->handle($server);
 
         return $this->fractal->collection(
-            $server->variables()->where('user_viewable', true)->get()
+            $server->variables()->orderBy('sort')->where('user_viewable', true)->get()
         )
             ->transformWith($this->getTransformer(EggVariableTransformer::class))
             ->addMeta([
