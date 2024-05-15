6bdd1b3ccb318826d20f0c261569e01af74567a4
Boy132 <Boy132@users.noreply.github.com>
2024-05-15 16:37:56 +0200

    Add api for server transfers (#153)
    * add application api endpoint to start server transfer
    
    * add basics for "cancel transfer" endpoint
    
    * wire up wings cancel
    
    * lint

--- routes/api-application.php
+++ routes/api-application.php
@@ -69,6 +69,8 @@ Route::prefix('/servers')->group(function () {
     Route::post('/{server:id}/suspend', [Application\Servers\ServerManagementController::class, 'suspend'])->name('api.application.servers.suspend');
     Route::post('/{server:id}/unsuspend', [Application\Servers\ServerManagementController::class, 'unsuspend'])->name('api.application.servers.unsuspend');
     Route::post('/{server:id}/reinstall', [Application\Servers\ServerManagementController::class, 'reinstall'])->name('api.application.servers.reinstall');
+    Route::post('/{server:id}/transfer', [Application\Servers\ServerManagementController::class, 'startTransfer'])->name('api.application.servers.transfer');
+    Route::post('/{server:id}/transfer/cancel', [Application\Servers\ServerManagementController::class, 'cancelTransfer'])->name('api.application.servers.transfer.cancel');
 
     Route::delete('/{server:id}', [Application\Servers\ServerController::class, 'delete']);
     Route::delete('/{server:id}/{force?}', [Application\Servers\ServerController::class, 'delete']);
