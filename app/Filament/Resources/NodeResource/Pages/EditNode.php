a077c6e2aca68a8db55d684cc09a4aa063a13c43
Boy132 <mail@boy132.de>
2024-05-16 09:18:48 +0200

    run pint

--- app/Filament/Resources/NodeResource/Pages/EditNode.php
+++ app/Filament/Resources/NodeResource/Pages/EditNode.php
@@ -318,46 +318,46 @@ class EditNode extends EditRecord
                                         ->suffix('%'),
                                 ]),
                             Forms\Components\Grid::make()
-                                    ->columns(6)
-                                    ->columnSpanFull()
-                                    ->schema([
-                                        Forms\Components\ToggleButtons::make('unlimited_cpu')
-                                            ->label('CPU')->inlineLabel()->inline()
-                                            ->live()
-                                            ->afterStateUpdated(fn (Forms\Set $set) => $set('cpu', 0))
-                                            ->afterStateUpdated(fn (Forms\Set $set) => $set('cpu_overallocate', 0))
-                                            ->formatStateUsing(fn (Forms\Get $get) => $get('cpu') == 0)
-                                            ->options([
-                                                true => 'Unlimited',
-                                                false => 'Limited',
-                                            ])
-                                            ->colors([
-                                                true => 'primary',
-                                                false => 'warning',
-                                            ])
-                                            ->columnSpan(2),
-                                        Forms\Components\TextInput::make('cpu')
-                                            ->dehydratedWhenHidden()
-                                            ->hidden(fn (Forms\Get $get) => $get('unlimited_cpu'))
-                                            ->label('CPU Limit')->inlineLabel()
-                                            ->suffix('%')
-                                            ->required()
-                                            ->columnSpan(2)
-                                            ->numeric()
-                                            ->minValue(0),
-                                        Forms\Components\TextInput::make('cpu_overallocate')
-                                            ->dehydratedWhenHidden()
-                                            ->hidden(fn (Forms\Get $get) => $get('unlimited_cpu'))
-                                            ->label('Overallocate')->inlineLabel()
-                                            ->hintIcon('tabler-question-mark')
-                                            ->hintIconTooltip('The % allowable to go over the set limit.')
-                                            ->columnSpan(2)
-                                            ->required()
-                                            ->numeric()
-                                            ->minValue(-1)
-                                            ->maxValue(100)
-                                            ->suffix('%'),
-                                    ]),
+                                ->columns(6)
+                                ->columnSpanFull()
+                                ->schema([
+                                    Forms\Components\ToggleButtons::make('unlimited_cpu')
+                                        ->label('CPU')->inlineLabel()->inline()
+                                        ->live()
+                                        ->afterStateUpdated(fn (Forms\Set $set) => $set('cpu', 0))
+                                        ->afterStateUpdated(fn (Forms\Set $set) => $set('cpu_overallocate', 0))
+                                        ->formatStateUsing(fn (Forms\Get $get) => $get('cpu') == 0)
+                                        ->options([
+                                            true => 'Unlimited',
+                                            false => 'Limited',
+                                        ])
+                                        ->colors([
+                                            true => 'primary',
+                                            false => 'warning',
+                                        ])
+                                        ->columnSpan(2),
+                                    Forms\Components\TextInput::make('cpu')
+                                        ->dehydratedWhenHidden()
+                                        ->hidden(fn (Forms\Get $get) => $get('unlimited_cpu'))
+                                        ->label('CPU Limit')->inlineLabel()
+                                        ->suffix('%')
+                                        ->required()
+                                        ->columnSpan(2)
+                                        ->numeric()
+                                        ->minValue(0),
+                                    Forms\Components\TextInput::make('cpu_overallocate')
+                                        ->dehydratedWhenHidden()
+                                        ->hidden(fn (Forms\Get $get) => $get('unlimited_cpu'))
+                                        ->label('Overallocate')->inlineLabel()
+                                        ->hintIcon('tabler-question-mark')
+                                        ->hintIconTooltip('The % allowable to go over the set limit.')
+                                        ->columnSpan(2)
+                                        ->required()
+                                        ->numeric()
+                                        ->minValue(-1)
+                                        ->maxValue(100)
+                                        ->suffix('%'),
+                                ]),
                         ]),
                     Tabs\Tab::make('Configuration File')
                         ->icon('tabler-code')
