2f5188829344fbdbe6fd8fd220bae858d06dc1e4
Poseidon281 <sennavaniterson1@gmail.com>
2024-05-17 15:48:41 +0200

    Added servers cluster

---
+++ app/Filament/Client/Clusters/Servers.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Client\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Servers extends Cluster
+{
+    protected static ?string $navigationIcon = 'tabler-server2';
+}
