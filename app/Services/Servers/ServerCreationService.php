c31b7b8c6a2a27bc62c2aeaf250525e4d77a27b2
notCharles <charles@pelican.dev>
2024-06-01 15:52:13 -0400

    Correctly save labels on create

--- app/Services/Servers/ServerCreationService.php
+++ app/Services/Servers/ServerCreationService.php
@@ -154,6 +154,7 @@ class ServerCreationService
             'database_limit' => Arr::get($data, 'database_limit') ?? 0,
             'allocation_limit' => Arr::get($data, 'allocation_limit') ?? 0,
             'backup_limit' => Arr::get($data, 'backup_limit') ?? 0,
+            'docker_labels' => Arr::get($data, 'docker_labels'),
         ]);
     }
 
