32e96dc0a695754989eb6e317daea079dd9456a2
Lance Pioch <git@lance.sh>
2024-06-06 15:49:36 -0400

    Wip

---
+++ database/migrations/2024_06_06_043350_modify_allocations.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('servers', function (Blueprint $table) {
+            $table->json('ports');
+        });
+
+        Schema::dropIfExists('allocations');
+
+        Schema::table('servers', function (Blueprint $table) {
+            $table->dropColumn(['allocation_id', 'allocation_limit']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('allocations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('node_id');
+            $table->string('ip');
+            $table->text('ip_alias');
+            $table->unsignedMediumInteger('port');
+            $table->unsignedInteger('server_id');
+            $table->string('notes')->default('');
+            $table->timestamps();
+
+            $table->unique(['node_id', 'ip', 'port']);
+        });
+    }
+};
