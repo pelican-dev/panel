46c5d5bd723e588bbcf7f7ec0f33f96e69f4abb4
Lance Pioch <git@lance.sh>
2024-06-09 11:51:28 -0400

    Add some basic resource pages

---
+++ app/Filament/App/Resources/ActivityResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\ActivityResource\Pages;
+use App\Filament\App\Resources\ActivityResource\RelationManagers;
+use App\Models\Activity;
+use App\Models\ActivityLog;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ActivityResource extends Resource
+{
+    protected static ?string $model = ActivityLog::class;
+
+    protected static ?string $navigationIcon = 'tabler-stack';
+
+    protected static ?string $tenantOwnershipRelationshipName = 'actor';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListActivities::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/App/Resources/UserResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use App\Filament\App\Resources\UserResource\Pages;
+use App\Filament\App\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'tabler-users';
+
+    protected static ?string $tenantOwnershipRelationshipName = 'subServers';
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->paginated(false)
+            ->searchable(false)
+            ->columns([
+                Tables\Columns\ImageColumn::make('picture')
+                    ->visibleFrom('lg')
+                    ->label('')
+                    ->extraImgAttributes(['class' => 'rounded-full'])
+                    ->defaultImageUrl(fn (User $user) => 'https://gravatar.com/avatar/' . md5(strtolower($user->email))),
+                Tables\Columns\TextColumn::make('username')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('use_totp')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+        ];
+    }
+}
