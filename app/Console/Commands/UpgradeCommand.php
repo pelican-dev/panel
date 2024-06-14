817dc87e7ed735a861ad3f94d2fc35d5f088d066
Poseidon281 <sennavaniterson1@gmail.com>
2024-06-14 13:44:03 +0200

    Delete modifications to upgrade command

--- app/Console/Commands/UpgradeCommand.php
+++ app/Console/Commands/UpgradeCommand.php
@@ -3,10 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Console\Kernel;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Helper\ProgressBar;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Config;
 
 class UpgradeCommand extends Command
 {
@@ -17,221 +16,174 @@ class UpgradeCommand extends Command
         {--group= : The group that PHP runs under. All files will be owned by this group.}
         {--url= : The specific archive to download.}
         {--release= : A specific version to download from GitHub. Leave blank to use latest.}
-        {--skip-download : If set no archive will be downloaded.}
-        {--skip-permissions : Skip setting file permissions.}
-        {--skip-dependencies : Skip updating dependencies.}
-        {--backup : Create a backup of the current state before upgrading.}';
+        {--skip-download : If set no archive will be downloaded.}';
 
     protected $description = 'Downloads a new archive from GitHub and then executes the normal upgrade commands.';
 
-    protected ProgressBar $progressBar;
-
+    /**
+     * Executes an upgrade command which will run through all of our standard
+     * Panel commands and enable users to basically just download
+     * the archive and execute this and be done.
+     *
+     * This places the application in maintenance mode as well while the commands
+     * are being executed.
+     *
+     * @throws \Exception
+     */
     public function handle(): void
     {
-        $this->info('Starting the Pelican Panel upgrade process.');
+        $skipDownload = $this->option('skip-download');
+        if (!$skipDownload) {
+            $this->output->warning(__('commands.upgrade.integrity'));
+            $this->output->comment(__('commands.upgrade.source_url'));
+            $this->line($this->getUrl());
+        }
 
-        try {
-            $skipDownload = $this->option('skip-download');
-            $skipPermissions = $this->option('skip-permissions');
-            $skipDependencies = $this->option('skip-dependencies');
-            $createBackup = $this->option('backup');
+        if (version_compare(PHP_VERSION, '7.4.0') < 0) {
+            $this->error(__('commands.upgrade.php_version') . ' [' . PHP_VERSION . '].');
+        }
 
-            if ($createBackup) {
-                $this->createBackup();
-            }
-
-            [$user, $group] = $this->askForUserAndGroup();
-
-            $this->enterMaintenanceMode();
-            $this->setProgressBar($skipDownload ? 9 : 10);
-
+        $user = 'www-data';
+        $group = 'www-data';
+        if ($this->input->isInteractive()) {
             if (!$skipDownload) {
-                $this->downloadAndExtractArchive();
+                $skipDownload = !$this->confirm(__('commands.upgrade.skipDownload'), true);
             }
 
-            if (!$skipPermissions) {
-                $this->setPermissions();
+            if (is_null($this->option('user'))) {
+                $userDetails = function_exists('posix_getpwuid') ? posix_getpwuid(fileowner('public')) : [];
+                $user = $userDetails['name'] ?? 'www-data';
+
+                $message = __('commands.upgrade.webserver_user', ['user' => $user]);
+                if (!$this->confirm($message, true)) {
+                    $user = $this->anticipate(
+                        __('commands.upgrade.name_webserver'),
+                        [
+                            'www-data',
+                            'nginx',
+                            'apache',
+                        ]
+                    );
+                }
             }
 
-            if (!$skipDependencies) {
-                $this->updateDependencies();
+            if (is_null($this->option('group'))) {
+                $groupDetails = function_exists('posix_getgrgid') ? posix_getgrgid(filegroup('public')) : [];
+                $group = $groupDetails['name'] ?? 'www-data';
+
+                $message = __('commands.upgrade.group_webserver', ['group' => $user]);
+                if (!$this->confirm($message, true)) {
+                    $group = $this->anticipate(
+                        __('commands.upgrade.group_webserver_question'),
+                        [
+                            'www-data',
+                            'nginx',
+                            'apache',
+                        ]
+                    );
+                }
             }
 
-            $this->clearCompiledTemplateCache();
-            $this->updateDatabaseSchema();
-            $this->setOwnership($user, $group);
-            $this->restartQueueWorkers();
-            $this->exitMaintenanceMode();
+            if (!$this->confirm(__('commands.upgrade.are_your_sure'))) {
+                $this->warn(__('commands.upgrade.terminated'));
 
-            $this->info('Pelican Panel upgrade completed successfully.');
-        } catch (\Exception $e) {
-            $this->error('Upgrade failed: ' . $e->getMessage());
-            Log::error('Upgrade failed', ['exception' => $e]);
-        }
-    }
-
-    protected function askForUserAndGroup(): array
-    {
-        $choices = [
-            '1' => 'NGINX/Apache (www-data:www-data)',
-            '2' => 'Rocky Linux NGINX (nginx:nginx)',
-            '3' => 'Rocky Linux Apache (apache:apache)',
-        ];
-
-        $choice = $this->choice('What webserver are you using?', $choices, '1');
-
-        switch ($choice) {
-            case '1':
-                return ['www-data', 'www-data'];
-            case '2':
-                return ['nginx', 'nginx'];
-            case '3':
-                return ['apache', 'apache'];
-            default:
-                return ['www-data', 'www-data'];
-        }
-    }
-
-    protected function createBackup(): void
-    {
-        $this->info('Creating backup...');
-
-        $this->line('Backing up files...');
-        $timestamp = date('YmdHis');
-        $backupDir = storage_path('backups/' . $timestamp);
-
-        $process = new Process(['mkdir', '-p', $backupDir]);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to create backup directory.');
+                return;
+            }
         }
 
-        $process = new Process(['cp', '-r', base_path(), $backupDir]);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to backup files.');
+        ini_set('output_buffering', '0');
+        $bar = $this->output->createProgressBar($skipDownload ? 9 : 10);
+        $bar->start();
+
+        if (!$skipDownload) {
+            $this->withProgress($bar, function () {
+                $this->line("\$upgrader> curl -L \"{$this->getUrl()}\" | tar -xzv");
+                $process = Process::fromShellCommandline("curl -L \"{$this->getUrl()}\" | tar -xzv");
+                $process->run(function ($type, $buffer) {
+                    $this->{$type === Process::ERR ? 'error' : 'line'}($buffer);
+                });
+            });
         }
 
-        $this->line('Files backed up to ' . $backupDir);
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan down');
+            $this->call('down');
+        });
 
-        $this->line('Backing up database...');
-        $databaseConfig = Config::get('database.connections.' . Config::get('database.default'));
-        $database = $databaseConfig['database'];
-        $username = $databaseConfig['username'];
-        $password = $databaseConfig['password'];
-        $host = $databaseConfig['host'];
-        $backupFile = $backupDir . '/database.sql';
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> chmod -R 755 storage bootstrap/cache');
+            $process = new Process(['chmod', '-R', '755', 'storage', 'bootstrap/cache']);
+            $process->run(function ($type, $buffer) {
+                $this->{$type === Process::ERR ? 'error' : 'line'}($buffer);
+            });
+        });
 
-        $process = new Process([
-            'mysqldump',
-            '--user=' . $username,
-            '--password=' . $password,
-            '--host=' . $host,
-            $database,
-            '--result-file=' . $backupFile,
-        ]);
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to backup database.');
-        }
+        $this->withProgress($bar, function () {
+            $command = ['composer', 'install', '--no-ansi'];
+            if (config('app.env') === 'production' && !config('app.debug')) {
+                $command[] = '--optimize-autoloader';
+                $command[] = '--no-dev';
+            }
 
-        $this->line('Database backed up to ' . $backupFile);
-        $this->info('Backup completed.');
+            $this->line('$upgrader> ' . implode(' ', $command));
+            $process = new Process($command);
+            $process->setTimeout(10 * 60);
+            $process->run(function ($type, $buffer) {
+                $this->line($buffer);
+            });
+        });
+
+        /** @var \Illuminate\Foundation\Application $app */
+        $app = require __DIR__ . '/../../../bootstrap/app.php';
+        /** @var \App\Console\Kernel $kernel */
+        $kernel = $app->make(Kernel::class);
+        $kernel->bootstrap();
+        $this->setLaravel($app);
+
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan view:clear');
+            $this->call('view:clear');
+        });
+
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan config:clear');
+            $this->call('config:clear');
+        });
+
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan migrate --force --seed');
+            $this->call('migrate', ['--force' => true, '--seed' => true]);
+        });
+
+        $this->withProgress($bar, function () use ($user, $group) {
+            $this->line("\$upgrader> chown -R {$user}:{$group} *");
+            $process = Process::fromShellCommandline("chown -R {$user}:{$group} *", $this->getLaravel()->basePath());
+            $process->setTimeout(10 * 60);
+            $process->run(function ($type, $buffer) {
+                $this->{$type === Process::ERR ? 'error' : 'line'}($buffer);
+            });
+        });
+
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan queue:restart');
+            $this->call('queue:restart');
+        });
+
+        $this->withProgress($bar, function () {
+            $this->line('$upgrader> php artisan up');
+            $this->call('up');
+        });
+
+        $this->newLine(2);
+        $this->info(__('commands.upgrade.success'));
     }
 
-    protected function enterMaintenanceMode(): void
+    protected function withProgress(ProgressBar $bar, \Closure $callback)
     {
-        $this->line('$ php artisan down');
-        $this->call('down');
-    }
-
-    protected function setProgressBar(int $steps): void
-    {
-        $this->progressBar = $this->output->createProgressBar($steps);
-        $this->progressBar->start();
-    }
-
-    protected function downloadAndExtractArchive(): void
-    {
-        $this->progressBar->advance();
-        $url = $this->getUrl();
-        $this->line('$ curl -L "' . $url . '" | tar -xzv');
-        $process = Process::fromShellCommandline('curl -L "' . $url . '" | tar -xzv');
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to download or extract the archive.');
-        }
-    }
-
-    protected function setPermissions(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ chmod -R 755 storage bootstrap/cache');
-        $process = new Process(['chmod', '-R', '755', 'storage', 'bootstrap/cache']);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to set permissions.');
-        }
-    }
-
-    protected function updateDependencies(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ composer install --no-dev --optimize-autoloader');
-        $process = new Process(['composer', 'install', '--no-dev', '--optimize-autoloader']);
-        $process->setTimeout(10 * 60);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to update dependencies.');
-        }
-    }
-
-    protected function clearCompiledTemplateCache(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ php artisan view:clear');
-        $this->call('view:clear');
-
-        $this->progressBar->advance();
-        $this->line('$ php artisan config:clear');
-        $this->call('config:clear');
-    }
-
-    protected function updateDatabaseSchema(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ php artisan migrate --seed --force');
-        $this->call('migrate', ['--force' => true, '--seed' => true]);
-    }
-
-    protected function setOwnership(string $user, string $group): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ chown -R ' . $user . ':' . $group . ' *');
-        $process = Process::fromShellCommandline('chown -R ' . $user . ':' . $group . ' *', base_path());
-        $process->setTimeout(10 * 60);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException('Failed to set ownership.');
-        }
-    }
-
-    protected function restartQueueWorkers(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ php artisan queue:restart');
-        $this->call('queue:restart');
-    }
-
-    protected function exitMaintenanceMode(): void
-    {
-        $this->progressBar->advance();
-        $this->line('$ php artisan up');
-        $this->call('up');
+        $bar->clear();
+        $callback();
+        $bar->advance();
+        $bar->display();
     }
 
     protected function getUrl(): string
