738707b251f9c873f26e274051024824c8328130
Lance Pioch <git@lance.sh>
2024-06-16 12:30:28 -0400

    Add validation rule

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -167,6 +167,7 @@ class Server extends Model
         'database_limit' => 'present|nullable|integer|min:0',
         'allocation_limit' => 'sometimes|nullable|integer|min:0',
         'backup_limit' => 'present|nullable|integer|min:0',
+        'ports' => 'nullable|array',
     ];
 
     protected function casts(): array
