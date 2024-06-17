707c2caf0124417c2ed4f08bada0ecf69122f745
MartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-06-17 05:19:27 +0200

    Update OAuth migration

--- database/migrations/2024_06_13_120409_add_oauth_column_to_users.php
+++ database/migrations/2024_06_13_120409_add_oauth_column_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('oauth')->after('totp_authenticated_at');
+            $table->json('oauth')->nullable()->after('totp_authenticated_at');
         });
     }
 
