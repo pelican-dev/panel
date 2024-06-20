9fb739f2ff8351a3d7651bc2ddca3a665ec28d30
Boy132 <mail@boy132.de>
2024-06-21 00:02:58 +0200

    add logic to DeleteFilesService

--- app/Services/Files/DeleteFilesService.php
+++ app/Services/Files/DeleteFilesService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\Files;
 
+use App\Exceptions\Http\Connection\DaemonConnectionException;
 use App\Models\Server;
 use App\Repositories\Daemon\DaemonFileRepository;
+use Illuminate\Support\Str;
 
 class DeleteFilesService
 {
@@ -11,18 +13,29 @@ class DeleteFilesService
      * DeleteFilesService constructor.
      */
     public function __construct(
-        private DaemonFileRepository $fileRepository
+        private DaemonFileRepository $daemonFileRepository
     ) {
     }
 
     /**
      * Deletes the given files.
+     * @throws DaemonConnectionException
      */
     public function handle(Server $server, array $files): void
     {
-        // TODO: convert $files
-        // see https://github.com/Boy132/panel/pull/2/files
+        $filesToDelete = collect();
+        foreach ($files as $line) {
+            $path = dirname($line);
+            $pattern = basename($line);
+            collect($this->daemonFileRepository->setServer($server)->getDirectory($path))->each(function ($item) use ($path, $pattern, $filesToDelete) {
+                if (Str::is($pattern, $item['name'])) {
+                    $filesToDelete->push($path . '/' . $item['name']);
+                }
+            });
+        }
 
-        $this->fileRepository->setServer($server)->deleteFiles('/', $files);
+        if ($filesToDelete->isNotEmpty()) {
+            $this->daemonFileRepository->setServer($server)->deleteFiles('/', $filesToDelete->toArray());
+        }
     }
 }
