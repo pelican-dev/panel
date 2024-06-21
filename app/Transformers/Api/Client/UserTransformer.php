d53efa5bc4586c25490c6619f745e6d8fd6394fe
Boy132 <mail@boy132.de>
2024-06-21 10:47:39 +0200

    backwards compat

--- app/Transformers/Api/Client/UserTransformer.php
+++ app/Transformers/Api/Client/UserTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers\Api\Client;
 
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class UserTransformer extends BaseClientTransformer
@@ -27,8 +28,10 @@ class UserTransformer extends BaseClientTransformer
             'first_name' => $user->name_first,
             'last_name' => $user->name_last,
             'language' => $user->language,
+            'image' => 'https://gravatar.com/avatar/' . md5(Str::lower($user->email)), // deprecated
+            'admin' => (bool) $user->root_admin, // deprecated, use "root_admin"
             'root_admin' => (bool) $user->root_admin,
-            '2fa' => (bool) $user->use_totp,
+            '2fa_enabled' => (bool) $user->use_totp,
             'created_at' => $this->formatTimestamp($user->created_at),
             'updated_at' => $this->formatTimestamp($user->updated_at),
         ];
