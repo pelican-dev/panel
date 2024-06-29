82c05681298913e25a48d5afbdd776cb732b8958
Charles <charles@pelican.dev>
2024-06-29 17:38:18 -0400

    Reduce Reuse (#443)
    * Reduce Reuse
    
    Reduce the repetitiveness of \Form\Component\Blah along with all the others...
    
    * PHPStan Fix

--- app/Filament/Resources/ApiKeyResource/Pages/ListApiKeys.php
+++ app/Filament/Resources/ApiKeyResource/Pages/ListApiKeys.php
@@ -6,8 +6,9 @@ use App\Filament\Resources\ApiKeyResource;
 use App\Models\ApiKey;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Tables;
 
 class ListApiKeys extends ListRecords
 {
@@ -19,37 +20,37 @@ class ListApiKeys extends ListRecords
             ->searchable(false)
             ->modifyQueryUsing(fn ($query) => $query->where('key_type', ApiKey::TYPE_APPLICATION))
             ->columns([
-                Tables\Columns\TextColumn::make('key')
+                TextColumn::make('key')
                     ->copyable()
                     ->icon('tabler-clipboard-text')
                     ->state(fn (ApiKey $key) => $key->identifier . $key->token),
 
-                Tables\Columns\TextColumn::make('memo')
+                TextColumn::make('memo')
                     ->label('Description')
                     ->wrap()
                     ->limit(50),
 
-                Tables\Columns\TextColumn::make('identifier')
+                TextColumn::make('identifier')
                     ->hidden()
                     ->searchable(),
 
-                Tables\Columns\TextColumn::make('last_used_at')
+                TextColumn::make('last_used_at')
                     ->label('Last Used')
                     ->placeholder('Not Used')
                     ->dateTime()
                     ->sortable(),
 
-                Tables\Columns\TextColumn::make('created_at')
+                TextColumn::make('created_at')
                     ->label('Created')
                     ->dateTime()
                     ->sortable(),
 
-                Tables\Columns\TextColumn::make('user.username')
+                TextColumn::make('user.username')
                     ->label('Created By')
                     ->url(fn (ApiKey $apiKey): string => route('filament.admin.resources.users.edit', ['record' => $apiKey->user])),
             ])
             ->actions([
-                Tables\Actions\DeleteAction::make(),
+                DeleteAction::make(),
             ]);
     }
 
