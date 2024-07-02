c843aee72f2e40497caae54ef338ae5e2ed3a74e
Poseidon_281 <sennavaniterson1@gmail.com>
2024-07-02 22:27:03 +0200

    Unbreak /legacy
    The last update from Lance broke the legacy side and this fixes that

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -95,12 +95,6 @@
                                 <i class="fa fa-th-large"></i> <span>Eggs</span>
                             </a>
                         </li>
-                        <li class="header">OTHER</li>
-                        <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.settings') ?: 'active' }}">
-                            <a href="{{ route('admin.settings')}}">
-                                <i class="fa fa-wrench"></i> <span>Settings</span>
-                            </a>
-                        </li>
                         <li class="{{ ! starts_with(Route::currentRouteName(), 'admin.api') ?: 'active' }}">
                             <a href="{{ route('admin.api.index')}}">
                                 <i class="fa fa-gamepad"></i> <span>Application API</span>
