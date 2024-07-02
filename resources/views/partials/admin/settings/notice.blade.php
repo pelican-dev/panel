f5ad4fc96bf604a4fba7a8e7918dc22cd3e8764f
Lance Pioch <git@lance.sh>
2024-07-02 15:27:56 -0400

    Remove old database settings

--- resources/views/partials/admin/settings/notice.blade.php
+++ resources/views/partials/admin/settings/notice.blade.php
@@ -1,11 +1,2 @@
 @section('settings::notice')
-    @if(config('panel.load_environment_only', false))
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="alert alert-danger">
-                    Your Panel is currently configured to read settings from the environment only. You will need to set <code>APP_ENVIRONMENT_ONLY=false</code> in your environment file in order to load settings dynamically.
-                </div>
-            </div>
-        </div>
-    @endif
 @endsection
