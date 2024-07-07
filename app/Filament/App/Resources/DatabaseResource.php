511dc34e4e74bacb137c216c02188b3a1264297f
notCharles <charles@pelican.dev>
2024-07-06 20:24:52 -0400

    WIP on issue/353

--- app/Filament/App/Resources/DatabaseResource.php
+++ app/Filament/App/Resources/DatabaseResource.php
@@ -4,84 +4,13 @@ namespace App\Filament\App\Resources;
 
 use App\Filament\App\Resources\DatabaseResource\Pages;
 use App\Models\Database;
-use Filament\Forms;
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
 
 class DatabaseResource extends Resource
 {
     protected static ?string $model = Database::class;
 
     protected static ?string $navigationIcon = 'tabler-database';
-
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('server_id')
-                    ->relationship('server', 'name')
-                    ->required(),
-                Forms\Components\TextInput::make('database_host_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('database')
-                    ->required(),
-                Forms\Components\TextInput::make('username')
-                    ->required(),
-                Forms\Components\TextInput::make('remote')
-                    ->required(),
-                Forms\Components\Textarea::make('password')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\TextInput::make('max_connections')
-                    ->numeric()
-                    ->default(0),
-            ]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('server.name')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('database_host_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('database')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('username')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('remote')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('max_connections')
-                    ->numeric()
-                    ->sortable(),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }
 
     public static function getRelations(): array
     {
@@ -94,7 +23,6 @@ class DatabaseResource extends Resource
     {
         return [
             'index' => Pages\ListDatabases::route('/'),
-            'create' => Pages\CreateDatabase::route('/create'),
             'view' => Pages\ViewDatabase::route('/{record}'),
             'edit' => Pages\EditDatabase::route('/{record}/edit'),
         ];
--- app/Filament/App/Resources/DatabaseResource/Pages/ListDatabases.php
+++ app/Filament/App/Resources/DatabaseResource/Pages/ListDatabases.php
@@ -3,17 +3,72 @@
 namespace App\Filament\App\Resources\DatabaseResource\Pages;
 
 use App\Filament\App\Resources\DatabaseResource;
-use Filament\Actions;
+use App\Services\Databases\DatabaseManagementService;
+use Filament\Actions\CreateAction;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 
 class ListDatabases extends ListRecords
 {
     protected static string $resource = DatabaseResource::class;
 
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('database')
+                    ->searchable(),
+                TextColumn::make('username')
+                    ->searchable(),
+                TextColumn::make('remote')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->actions([
+                ViewAction::make(),
+            ]);
+    }
+
     protected function getHeaderActions(): array
     {
+        $server = Filament::getTenant();
+
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make('new')
+                ->label(fn () => $server->database_limit <= $server->databases_count ? 'Database Limit Reached' : 'Create Database')
+                ->disabled(fn () => $server->database_limit <= $server->databases_count)
+                ->createAnother(false)
+                ->form([
+                    TextInput::make('database')
+                        ->label('Database Name')
+                        ->prefix('s'. $server->id . '_')
+                        ->hintIcon('tabler-question-mark')
+                        ->hintIconTooltip('FUCK YOURSELF'),
+                    TextInput::make('username')
+                        ->label('Database Username'),
+                ])
+                ->action(function ($data) use ($server) {
+                    if (empty($data['database'])) {
+                        $data['database'] = str_random(12);
+                    }
+
+                    $data['database_host_id'] =
+
+                    $data['database'] = 's'. $server->id . '_' . $data['database'];
+
+                    resolve(DatabaseManagementService::class)->create($server, dd($data));
+                }),
         ];
     }
 }
