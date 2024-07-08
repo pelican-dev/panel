22cc4dc517ce91dcf5e8a4d75e76db6350cdcbc8
Exotical <ItzExotical@gmail.com>
2024-07-08 23:34:41 +0200

    Add ProxyHandler middleware

---
+++ app/Http/Middleware/ProxyHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
+
+class ProxyHandler
+{
+    /**
+     * Handle an incoming request and set the request IP to X-Forwarded-For if exists.
+    */
+    public function handle(Request $request, \Closure $next)
+    {
+        if ($request->hasHeader('X-Forwarded-For')) {
+            $forwarded = $request->header('X-Forwarded-For');
+            $client = trim(explode(',', $forwarded)[0]);
+            $request->server->set('REMOTE_ADDR', $client);
+        }
+
+        return $next($request);
+    }
+}
