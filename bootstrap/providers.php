f2761954db5ccf1fddaaa365d44fc1af32f2adaf
Exotical <ItzExotical@gmail.com>
2024-07-08 11:23:39 +0200

    Revert removal of EventServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\ActivityLogServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\BackupsServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\RouteServiceProvider::class,
     App\Providers\ViewComposerServiceProvider::class,
