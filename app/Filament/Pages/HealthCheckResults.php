b92a81ec86e69c2b18dff8f1eee6568cde6e4d56
Boy132 <mail@boy132.de>
2024-07-09 10:51:40 +0200

    auto run health checks if they didn't run before

--- app/Filament/Pages/HealthCheckResults.php
+++ app/Filament/Pages/HealthCheckResults.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Artisan;
 use ShuvroRoy\FilamentSpatieLaravelHealth\Pages\HealthCheckResults as BaseHealthCheckResults;
+use Spatie\Health\Commands\RunHealthChecksCommand;
+use Spatie\Health\ResultStores\ResultStore;
 
 class HealthCheckResults extends BaseHealthCheckResults
 {
@@ -11,5 +15,21 @@ class HealthCheckResults extends BaseHealthCheckResults
     public static function getNavigationGroup(): ?string
     {
         return 'Advanced';
+    }
+
+    protected function getViewData(): array
+    {
+        $checkResults = app(ResultStore::class)->latestResults();
+
+        if ($checkResults === null) {
+            Artisan::call(RunHealthChecksCommand::class);
+
+            $this->dispatch('refresh-component');
+        }
+
+        return [
+            'lastRanAt' => new Carbon($checkResults?->finishedAt),
+            'checkResults' => $checkResults,
+        ];
     }
 }
