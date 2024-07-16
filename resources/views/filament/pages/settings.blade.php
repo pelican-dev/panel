25536e8a1d003a15d581c427fb4a1f39b943e63a
Boy132 <mail@boy132.de>
2024-07-16 11:29:54 +0200

    add class to view to allow customization

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,8 +1,19 @@
-<x-filament-panels::page>
-    <x-filament-panels::form wire:submit="save">
+<x-filament-panels::page 
+	@class([
+		'fi-page-settings'
+	])
+>
+    <x-filament-panels::form
+		id="form"
+		:wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
+		wire:submit="save"
+	>
         {{ $this->form }}
 
-        <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
+        <x-filament-panels::form.actions
+			:actions="$this->getCachedFormActions()"
+			:full-width="$this->hasFullWidthFormActions()"
+		/>
     </x-filament-panels::form>
 
     <x-filament-panels::page.unsaved-data-changes-alert />
