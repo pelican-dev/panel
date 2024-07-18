7c34104608ddbc78c2c02ab68100870fd08f0070
Poseidon_281 <sennavaniterson1@gmail.com>
2024-07-18 11:56:04 +0200

    Added more models

--- config/filament-webhook-server.php
+++ config/filament-webhook-server.php
@@ -6,16 +6,16 @@ use Marjose123\FilamentWebhookServer\Pages\Webhooks;
 return [
     /*
      *  Models that you want to be part of the webhooks options
-     */
+    */
     'models' => [
         \App\Models\User::class,
         \App\Models\Egg::class,
         \App\Models\Node::class,
         \App\Models\Server::class,
         \App\Models\DatabaseHost::class,
+        \App\Models\ApiKey::class,
+        \App\Models\Mount::class,
     ],
-    /*
-     */
     'polling' => '10s',
     'webhook' => [
         'keep_history' => true,
