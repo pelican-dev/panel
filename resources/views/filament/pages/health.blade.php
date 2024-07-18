76039c843e430d8eb4b04d1d9db22b2c6573d270
Boy132 <mail@boy132.de>
2024-07-18 15:35:22 +0200

    use health status indicator from spatie

--- resources/views/filament/pages/health.blade.php
+++ resources/views/filament/pages/health.blade.php
@@ -2,35 +2,21 @@
     @if (count($checkResults?->storedCheckResults ?? []))
 		<x-filament::grid :default="1" :sm="2" @class(['gap-6 mb-5'])>
 			@foreach ($checkResults->storedCheckResults as $result)
-				<x-filament::section
-					icon="{{
-						match ($result->status) {
-							Spatie\Health\Enums\Status::ok()->value => 'tabler-circle-check',
-							Spatie\Health\Enums\Status::warning()->value => 'tabler-exclamation-circle',
-							Spatie\Health\Enums\Status::skipped()->value => 'tabler-circle-arrow-right',
-							default => 'tabler-circle-x'
-						}
-					}}"
-					icon-color="{{
-						match ($result->status) {
-							Spatie\Health\Enums\Status::ok()->value => 'success',
-							Spatie\Health\Enums\Status::warning()->value => 'warning',
-							Spatie\Health\Enums\Status::skipped()->value => 'primary',
-							default => 'danger'
-						}
-					}}"
-				>
-					<x-slot name="heading">{{ $result->label }}</x-slot>
-
-					<p>
-						@if (!empty($result->notificationMessage))
-							{{ $result->notificationMessage }}
-						@else
-							{{ $result->shortSummary }}
-						@endif
-					</p>
-
-				</x-filament::section>
+				<div class="flex items-start px-4 space-x-2 overflow-hidden py-5 text-opacity-0 transition transform bg-white shadow-md shadow-gray-200 dark:shadow-black/25 dark:shadow-md dark:bg-gray-800 rounded-xl sm:p-6 md:space-x-3 md:min-h-[130px] dark:border-t dark:border-gray-700">
+					<x-health-status-indicator :result="$result" />
+					<div>
+						<dd class="-mt-1 font-bold text-gray-900 dark:text-white md:mt-1 md:text-xl">
+							{{ $result->label }}
+						</dd>
+						<dt class="mt-0 text-sm font-medium text-gray-600 dark:text-gray-300 md:mt-1">
+							@if (!empty($result->notificationMessage))
+								{{ $result->notificationMessage }}
+							@else
+								{{ $result->shortSummary }}
+							@endif
+						</dt>
+					</div>
+				</div>
 			@endforeach
 		</x-filament::grid>
     @endif
