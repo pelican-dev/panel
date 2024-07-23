e9953287672ebfe332ea9dc0ca963cee7254bd9f
Boy132 <mail@boy132.de>
2024-07-23 13:00:18 +0200

    improve requirements step

--- app/Filament/Pages/Installer/Steps/RequirementsStep.php
+++ app/Filament/Pages/Installer/Steps/RequirementsStep.php
@@ -2,74 +2,86 @@
 
 namespace App\Filament\Pages\Installer\Steps;
 
-use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Wizard\Step;
+use Filament\Notifications\Notification;
+use Filament\Support\Exceptions\Halt;
 
 class RequirementsStep
 {
     public static function make(): Step
     {
-        $phpVersion = version_compare(PHP_VERSION, '8.2.0') >= 0;
+        $correctPhpVersion = version_compare(PHP_VERSION, '8.2.0') >= 0;
 
         $fields = [
-            self::makeToggle('php_version')
-                ->label('PHP Version 8.2/ 8.3')
-                ->default($phpVersion)
-                ->validationMessages([
-                    'accepted' => 'Your PHP Version needs to be 8.2 or 8.3.',
+            Section::make('PHP Version')
+                ->description('8.2 or newer')
+                ->icon($correctPhpVersion ? 'tabler-check' : 'tabler-x')
+                ->iconColor($correctPhpVersion ? 'success' : 'danger')
+                ->schema([
+                    Placeholder::make('')
+                        ->content('Your PHP Version ' . ($correctPhpVersion ? 'is' : 'needs to be') .' 8.2 or newer.'),
                 ]),
         ];
 
         $phpExtensions = [
-            'GD' => extension_loaded('gd'),
-            'MySQL' => extension_loaded('pdo_mysql'),
-            'mbstring' => extension_loaded('mbstring'),
             'BCMath' => extension_loaded('bcmath'),
-            'XML' => extension_loaded('xml'),
             'cURL' => extension_loaded('curl'),
-            'Zip' => extension_loaded('zip'),
+            'GD' => extension_loaded('gd'),
             'intl' => extension_loaded('intl'),
+            'mbstring' => extension_loaded('mbstring'),
+            'MySQL' => extension_loaded('pdo_mysql'),
             'SQLite3' => extension_loaded('pdo_sqlite'),
+            'XML' => extension_loaded('xml'),
+            'Zip' => extension_loaded('zip'),
         ];
+        $allExtensionsInstalled = !in_array(false, $phpExtensions);
 
-        foreach ($phpExtensions as $extension => $loaded) {
-            $fields[] = self::makeToggle('ext_' . strtolower($extension))
-                ->label($extension . ' Extension')
-                ->default($loaded)
-                ->validationMessages([
-                    'accepted' => 'The ' . $extension . ' extension needs to be installed and enabled.',
-                ]);
-        }
+        $fields[] = Section::make('PHP Extensions')
+            ->description(implode(', ', array_keys($phpExtensions)))
+            ->icon($allExtensionsInstalled ? 'tabler-check' : 'tabler-x')
+            ->iconColor($allExtensionsInstalled ? 'success' : 'danger')
+            ->schema([
+                Placeholder::make('')
+                    ->content('All needed PHP Extensions are installed.')
+                    ->visible($allExtensionsInstalled),
+                Placeholder::make('')
+                    ->content('The following PHP Extensions are missing: ' . implode(', ', array_keys($phpExtensions, false)))
+                    ->visible(!$allExtensionsInstalled),
+            ]);
 
         $folderPermissions = [
-            'Storage' => substr(sprintf('%o', fileperms(base_path('storage/'))), -4),
-            'Cache' => substr(sprintf('%o', fileperms(base_path('bootstrap/cache/'))), -4),
+            'Storage' => substr(sprintf('%o', fileperms(base_path('storage/'))), -4) >= 755,
+            'Cache' => substr(sprintf('%o', fileperms(base_path('bootstrap/cache/'))), -4) >= 755,
         ];
+        $correctFolderPermissions = !in_array(false, $folderPermissions);
 
-        foreach ($folderPermissions as $folder => $permission) {
-            $correct = $permission >= 755;
-            $fields[] = self::makeToggle('folder_' . strtolower($folder))
-                ->label($folder . ' Folder writeable')
-                ->default($correct)
-                ->validationMessages([
-                    'accepted' => 'The ' . $folder . ' needs to be writable. (755)',
-                ]);
-        }
+        $fields[] = Section::make('Folder Permissions')
+            ->description(implode(', ', array_keys($folderPermissions)))
+            ->icon($correctFolderPermissions ? 'tabler-check' : 'tabler-x')
+            ->iconColor($correctFolderPermissions ? 'success' : 'danger')
+            ->schema([
+                Placeholder::make('')
+                    ->content('All Folders have the correct permissions.')
+                    ->visible($correctFolderPermissions),
+                Placeholder::make('')
+                    ->content('The following Folders have wrong permissions: ' . implode(', ', array_keys($folderPermissions, false)))
+                    ->visible(!$correctFolderPermissions),
+            ]);
 
         return Step::make('requirements')
             ->label('Server Requirements')
-            ->schema($fields);
-    }
+            ->schema($fields)
+            ->afterValidation(function () use ($correctPhpVersion, $allExtensionsInstalled, $correctFolderPermissions) {
+                if (!$correctPhpVersion || !$allExtensionsInstalled || !$correctFolderPermissions) {
+                    Notification::make()
+                        ->title('Some requirements are missing!')
+                        ->danger()
+                        ->send();
 
-    private static function makeToggle(string $name): Toggle
-    {
-        return Toggle::make($name)
-            ->required()
-            ->accepted()
-            ->disabled()
-            ->onIcon('tabler-check')
-            ->offIcon('tabler-x')
-            ->onColor('success')
-            ->offColor('danger');
+                    throw new Halt();
+                }
+            });
     }
 }
