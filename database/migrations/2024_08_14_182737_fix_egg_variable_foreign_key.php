a6bf9cba643d22c90e39a8e3b514e87765c02323
MartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-08-20 22:18:49 +0200

    Update 2024_08_14_182737_fix_egg_variable_foreign_key.php

--- database/migrations/2024_08_14_182737_fix_egg_variable_foreign_key.php
+++ database/migrations/2024_08_14_182737_fix_egg_variable_foreign_key.php
@@ -1,7 +1,7 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,40 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Only needed for sqlite
-        if (Schema::getConnection()->getDriverName() !== 'sqlite') {
-            return;
-        }
-
-        // Disable foreign checks
-        // legacy_alter_table needs to be 'ON' so existing foreign key table references aren't renamed when renaming the table, see https://www.sqlite.org/lang_altertable.html
-        DB::statement('PRAGMA foreign_keys = OFF');
-        DB::statement('PRAGMA legacy_alter_table = ON');
-
-        DB::transaction(function () {
-            DB::statement('ALTER TABLE egg_variables RENAME TO _egg_variables_old');
-            DB::statement('CREATE TABLE egg_variables 
-                ("id" integer primary key autoincrement not null, 
-                "egg_id" integer not null, 
-                "name" varchar not null, 
-                "description" text not null, 
-                "env_variable" varchar not null, 
-                "default_value" text not null, 
-                "user_viewable" integer not null, 
-                "user_editable" integer not null, 
-                "rules" text not null, 
-                "created_at" datetime, 
-                "updated_at" datetime, 
-                "sort" integer, 
-                foreign key("egg_id") references "eggs"("id") on delete cascade)');
-            DB::statement('INSERT INTO egg_variables SELECT * FROM _egg_variables_old');
-            DB::statement('DROP TABLE _egg_variables_old');
-            DB::statement('CREATE UNIQUE INDEX "egg_variables_env_variable_unique" on "egg_variables" ("egg_id", "env_variable")');
-            DB::statement('CREATE UNIQUE INDEX "egg_variables_name_unique" on "egg_variables" ("egg_id", "name")');
+        Schema::table('egg_variables', function (Blueprint $table) {
+            $table->unique(['egg_id', 'env_variable']);
+            $table->unique(['egg_id', 'name']);
         });
-
-        DB::statement('PRAGMA foreign_keys = ON');
-        DB::statement('PRAGMA legacy_alter_table = OFF');
     }
 
     /**
@@ -52,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Reverse not needed
+        Schema::table('egg_variables', function (Blueprint $table) {
+            $table->dropUnique(['egg_id', 'env_variable']);
+            $table->dropUnique(['egg_id', 'name']);
+        });
     }
 };
