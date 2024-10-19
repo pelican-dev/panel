7289c008403b23b00b657cbf4ce2c5c499bd2b42
Boy132 <mail@boy132.de>
2024-10-19 23:57:23 +0200

    add data to network chart

--- app/Filament/App/Widgets/ServerNetworkChart.php
+++ app/Filament/App/Widgets/ServerNetworkChart.php
@@ -3,8 +3,10 @@
 namespace App\Filament\App\Widgets;
 
 use App\Models\Server;
+use Carbon\Carbon;
 use Filament\Support\RawJs;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Number;
 
 class ServerNetworkChart extends ChartWidget
 {
@@ -16,8 +18,45 @@ class ServerNetworkChart extends ChartWidget
 
     protected function getData(): array
     {
-        // TODO
-        return [];
+        $rx = collect(cache()->get("servers.{$this->server->id}.rx_bytes"))
+            ->slice(-10)
+            ->map(fn ($value, $key) => [
+                'rx' => Number::format($value, maxPrecision: 2, locale: auth()->user()->language),
+                'timestamp' => Carbon::createFromTimestamp($key, (auth()->user()->timezone ?? 'UTC'))->format('H:i:s'),
+            ])
+            ->all();
+
+        $tx = collect(cache()->get("servers.{$this->server->id}.tx_bytes"))
+            ->slice(-10)
+            ->map(fn ($value, $key) => [
+                'tx' => Number::format($value, maxPrecision: 2, locale: auth()->user()->language),
+                'timestamp' => Carbon::createFromTimestamp($key, (auth()->user()->timezone ?? 'UTC'))->format('H:i:s'),
+            ])
+            ->all();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Inbound',
+                    'data' => array_column($rx, 'rx'),
+                    'backgroundColor' => [
+                        'rgba(96, 165, 250, 0.3)',
+                    ],
+                    'tension' => '0.3',
+                    'fill' => true,
+                ],
+                [
+                    'label' => 'Outbound',
+                    'data' => array_column($tx, 'tx'),
+                    'backgroundColor' => [
+                        'rgba(165, 96, 250, 0.3)',
+                    ],
+                    'tension' => '0.3',
+                    'fill' => true,
+                ],
+            ],
+            'labels' => array_column($rx, 'timestamp'),
+        ];
     }
 
     protected function getType(): string
