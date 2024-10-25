40fc57014404a2b0c25244ed836959ceba18b31d
Lance Pioch <git@lance.sh>
2024-10-25 13:29:57 -0400

    Pint fix

--- app/Jobs/NodeStatistics.php
+++ app/Jobs/NodeStatistics.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class NodeStatistics implements ShouldQueue, ShouldBeUnique
+class NodeStatistics implements ShouldBeUnique, ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
