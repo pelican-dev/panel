cde3546889c763a91437970b6b995e79de4a29f3
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-10-29 03:28:51 +0000

    Add poll & tooltip

--- resources/views/livewire/node-system-information.blade.php
+++ resources/views/livewire/node-system-information.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div wire:poll>
     @switch($node->systemInformation()['version'] ?? 'false')
         @case('false')
             <x-filament::icon
@@ -9,6 +9,10 @@
             @break
         @default
             <x-filament::icon
+                x-tooltip="{
+                    content: '{{ $node->systemInformation()['version'] }}',
+                    theme: $store.theme,
+                }"
                 :icon="'tabler-heartbeat'"
                 @class(['fi-ta-icon-item', $sizeClasses, 'fi-color-custom text-custom-500 dark:text-custom-400', 'fi-color-success' => true])
                 @style([\Filament\Support\get_color_css_variables('success', shades: [400, 500], alias: 'tables::columns.icon-column.item') => true])
