19548338ee2420243165304d8a651cdb633fb774
Lance Pioch <lancepioch@gmail.com>
2024-10-29 19:52:32 -0400

    Update app/Models/Node.php

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -307,7 +307,9 @@ class Node extends Model
 
                 if ($message->startsWith('cURL error 6: Could not resolve host')) {
                     $message = str('Could not resolve host');
-                } elseif ($message->startsWith('cURL error 28: Failed to connect to ')) {
+                }
+                
+                if ($message->startsWith('cURL error 28: Failed to connect to ')) {
                     $message = $message->after('cURL error 28: ')->before(' after ');
                 }
 
