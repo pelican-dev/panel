fa0fc397606079e4de97fc68032d10b1597f30e4
Vehikl <go@vehikl.com>
2024-10-31 17:02:18 -0400

    WIP

---
+++ database/migrations/2024_10_31_203540_change_database_hosts_to_belong_to_many_nodes.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('database_host_node', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('node_id')->references('id')->on('nodes');
+            $table->foreignId('database_host_id')->references('id')->on('database_hosts');
+            $table->timestamps();
+        });
+
+        $databaseNodes = DB::table('database_hosts')->whereNotNull('node_id')->get();
+        $newJoinEntries = $databaseNodes->map(fn ($record) => [
+            'node_id' => $record->node_id,
+            'database_host_id' => $record->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('database_host_node')->insert($newJoinEntries->toArray());
+
+        Schema::table('database_hosts', function (Blueprint $table) {
+            // $table->dropForeign()
+            $table->dropColumn('node_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('database_hosts', function (Blueprint $table) {
+            $table->foreignId('node_id')->nullable()->references('id')->on('nodes');
+        });
+
+        foreach (DB::table('database_host_node')->get() as $record) {
+            DB::table('database_hosts')
+                ->where('id', $record->database_host_id)
+                ->update(['node_id' => $record->node_id])
+            ;
+        }
+
+        Schema::drop('database_host_node');
+    }
+};
