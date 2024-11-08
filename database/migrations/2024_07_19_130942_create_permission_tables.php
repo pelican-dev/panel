a359ff34c5f5c6603792ab7d27cc4a6e6aa69855
Boy132 <mail@boy132.de>
2024-11-08 23:16:46 +0100

    remove cache forget from migration

--- database/migrations/2024_07_19_130942_create_permission_tables.php
+++ database/migrations/2024_07_19_130942_create_permission_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -114,10 +114,6 @@ return new class extends Migration
 
             $table->primary([$pivotPermission, $pivotRole], 'role_has_permissions_permission_id_role_id_primary');
         });
-
-        app('cache')
-            ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
-            ->forget(config('permission.cache.key'));
     }
 
     /**
