475d40489e8cba7cfc930da55cade5fd048048a1
Boy132 <mail@boy132.de>
2024-11-15 11:46:09 +0100

    use `APP_TIMEZONE` as default timezone for new users

--- app/Models/User.php
+++ app/Models/User.php
@@ -188,6 +188,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         static::creating(function (self $user) {
             $user->uuid = Str::uuid()->toString();
 
+            $user->timezone = env('APP_TIMEZONE', 'UTC');
+
             return true;
         });
 
@@ -211,9 +213,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     {
         $rules = parent::getRules();
 
-        $rules['language'][] = new In(array_keys((new self())->getAvailableLanguages()));
+        $rules['language'][] = new In(array_keys((new self)->getAvailableLanguages()));
         $rules['timezone'][] = new In(array_values(DateTimeZone::listIdentifiers()));
-        $rules['username'][] = new Username();
+        $rules['username'][] = new Username;
 
         return $rules;
     }
