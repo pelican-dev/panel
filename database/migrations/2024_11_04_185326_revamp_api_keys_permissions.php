2c1ee0df9d820d63f462d32f4e42ce20b7c3c5eb
Rosox-hosting-account <git@natsu-dragnir.be>
2024-11-15 16:45:52 +0100

    Update 2024_11_04_185326_revamp_api_keys_permissions.php
    I try to download this update but this can't do the update so I tell you the solvation

--- database/migrations/2024_11_04_185326_revamp_api_keys_permissions.php
+++ database/migrations/2024_11_04_185326_revamp_api_keys_permissions.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('api_keys', function (Blueprint $table) {
-            $table->json('permissions');
+            $table->json('permissions')->nullable();
         });
 
         foreach (ApiKey::query() as $apiKey) {
