2b1ce024fd1c6b5324a288b6eae16ea5181a40c7
Boy132 <mail@boy132.de>
2024-11-18 10:36:31 +0100

    add auth frontend

--- config/auth.php
+++ config/auth.php
@@ -26,11 +26,15 @@ return [
     'oauth' => [
         'github' => [
             'enabled' => true,
+            'icon' => 'tabler-brand-github',
+            'color' => \Filament\Support\Colors\Color::hex('#4078c0'),
             'client_id' => env('OAUTH_GITHUB_CLIENT_ID'),
             'client_secret' => env('OAUTH_GITHUB_CLIENT_SECRET'),
         ],
         'discord' => [
             'enabled' => true,
+            'icon' => 'tabler-brand-discord',
+            'color' => \Filament\Support\Colors\Color::hex('#5865F2'),
             'client_id' => env('OAUTH_DISCORD_CLIENT_ID'),
             'client_secret' => env('OAUTH_DISCORD_CLIENT_SECRET'),
             'provider' => \SocialiteProviders\Discord\Provider::class,
