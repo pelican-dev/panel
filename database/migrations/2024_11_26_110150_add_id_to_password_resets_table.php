e4c6c0e1e7500dd05af655f21540389132c437bd
iriseden <adrien.chabod@resel.fr>
2024-11-26 13:41:39 +0100

    Adding migration

---
+++ database/migrations/2024_11_26_110150_add_id_to_password_resets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            if (!Schema::hasColumn('password_resets', 'id')) {
+                $table->id()->first();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+    }
+};
