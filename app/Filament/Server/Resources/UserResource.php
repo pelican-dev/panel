39ba2053db3afd1d6dce2e2804c01b2eedeb37d3
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-12-02 01:05:32 +0000

    Prevent users from editing them selves

--- app/Filament/Server/Resources/UserResource.php
+++ app/Filament/Server/Resources/UserResource.php
@@ -103,7 +103,7 @@ class UserResource extends Resource
                     }),
                 EditAction::make()
                     ->label('Edit User')
-
+                    ->hidden(fn (User $user) => auth()->user()->id === $user->id)
                     ->authorize(fn () => auth()->user()->can(Permission::ACTION_USER_UPDATE, $server))
                     ->modalHeading(fn (User $user) => 'Editing ' . $user->email)
                     ->action(function (array $data, SubuserUpdateService $subuserUpdateService, User $user) use ($server) {
