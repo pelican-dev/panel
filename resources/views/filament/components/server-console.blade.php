98b0b2c6e34cbea4a67407d7956f24d873ef0905
notCharles <charles@pelican.dev>
2024-12-04 19:56:37 -0500

    Update Console
    Updates console to be more better <3.
    
    Light Mode still needs some love, haven't figured that out with filaments light/dark options yet as it does not use the "bright<color>" colors...

--- resources/views/filament/components/server-console.blade.php
+++ resources/views/filament/components/server-console.blade.php
@@ -1,19 +1,40 @@
 <x-filament::widget>
     @assets
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/xterm/5.5.0/xterm.js"
-            integrity="sha512-Gujw5GajF5is3nMoGv9X+tCMqePLL/60qvAv1LofUZTV9jK8ENbM9L+maGmOsNzuZaiuyc/fpph1KT9uR5w3CQ=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/xterm/5.5.0/xterm.css"
-          integrity="sha512-AbNrj/oSHJaILgcdnkYm+DQ08SqVbZ8jlkJbFyyS1WDcAaXAcAfxJnCH69el7oVgTwVwyA5u5T+RdFyUykrV3Q=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/xterm/css/xterm.css">
+    <script src="https://cdn.jsdelivr.net/npm/xterm/lib/xterm.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/xterm-addon-fit/lib/xterm-addon-fit.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/xterm-addon-web-links/lib/xterm-addon-web-links.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/xterm-addon-search/lib/xterm-addon-search.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/xterm-addon-search-bar@0.2.0/lib/xterm-addon-search-bar.min.js"></script>
+    <style>
+        #terminal {
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        .xterm .xterm-rows > div {
+            padding-left: 10px;
+            padding-top: 2px;
+            padding-right: 10px;
+        }
+    </style>
     @endassets
 
     <div id="terminal" wire:ignore></div>
 
-    <div>
+    <div class="flex items-center w-full bg-transparent border rounded">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             class="icon icon-tabler icons-tabler-outline icon-tabler-chevrons-right">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 7l5 5l-5 5" />
+            <path d="M13 7l5 5l-5 5" />
+        </svg>
         <input
-            class="w-full bg-transparent"
+            class="w-full bg-transparent p-2 focus:outline-none focus:ring-0 border-none"
             type="text"
+            {{--            TODO: Disable if server offline--}}
             placeholder="Type a command..."
             wire:model="input"
             wire:keydown.enter="enter"
@@ -24,20 +45,74 @@
 
     @script
     <script>
+        let theme = {
+            background: 'rgba(19,26,32,0.7)',
+            cursor: 'transparent',
+            black: '#000000',
+            red: '#E54B4B',
+            green: '#9ECE58',
+            yellow: '#FAED70',
+            blue: '#396FE2',
+            magenta: '#BB80B3',
+            cyan: '#2DDAFD',
+            white: '#d0d0d0',
+            brightBlack: 'rgba(255, 255, 255, 0.2)',
+            brightRed: '#FF5370',
+            brightGreen: '#C3E88D',
+            brightYellow: '#FFCB6B',
+            brightBlue: '#82AAFF',
+            brightMagenta: '#C792EA',
+            brightCyan: '#89DDFF',
+            brightWhite: '#ffffff',
+            selection: '#FAF089'
+        };
+
         let options = {
-            fontSize: 18,
-            // fontFamily: th('fontFamily.mono'),
+            fontSize: 16,
             disableStdin: true,
             cursorStyle: 'underline',
+            cursorInactiveStyle: 'none',
             allowTransparency: true,
+            letterSpacing: 0.75,
+            lineHeight: 1,
             rows: 35,
-            cols: 110
-            // theme: theme,
+            cols: 110,
+            theme: theme
         };
 
         const terminal = new Terminal(options);
-        // TODO: load addons
+        const fitAddon = new FitAddon.FitAddon();
+        const webLinksAddon = new WebLinksAddon.WebLinksAddon();
+        const searchAddon = new SearchAddon.SearchAddon();
+        const searchAddonBar = new SearchBarAddon.SearchBarAddon({ searchAddon });
+
+        terminal.loadAddon(fitAddon);
+        terminal.loadAddon(webLinksAddon);
+        terminal.loadAddon(searchAddon);
+        terminal.loadAddon(searchAddonBar);
+
+
         terminal.open(document.getElementById('terminal'));
+
+        fitAddon.fit(); //Fit on first load
+
+        window.addEventListener('resize', () => {
+            fitAddon.fit();
+        });
+
+        terminal.attachCustomKeyEventHandler((event) => {
+            if ((event.ctrlKey || event.metaKey) && event.key === 'c') {
+                document.execCommand('copy'); // navigator.clipboard.writeText() only works on ssl..
+                return false;
+            } else if ((event.ctrlKey || event.metaKey) && event.key === 'f') {
+                event.preventDefault();
+                searchAddonBar.show();
+                return false;
+            } else if (event.key === 'Escape') {
+                searchAddonBar.hidden();
+            }
+            return true;
+        });
 
         const TERMINAL_PRELUDE = '\u001b[1m\u001b[33mpelican@' + '{{ \Filament\Facades\Filament::getTenant()->name }}' + ' ~ \u001b[0m';
 
