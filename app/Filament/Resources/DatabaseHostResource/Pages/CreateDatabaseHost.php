d77766a2abbceaea9f76f86e21c1e1ec509ee20f
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2024-12-05 04:04:20 +0000

    Remove unique

--- app/Filament/Resources/DatabaseHostResource/Pages/CreateDatabaseHost.php
+++ app/Filament/Resources/DatabaseHostResource/Pages/CreateDatabaseHost.php
@@ -81,7 +81,6 @@ class CreateDatabaseHost extends CreateRecord
                         Select::make('node_id')
                             ->searchable()
                             ->preload()
-                            ->unique()
                             ->helperText('This setting only defaults to this database host when adding a database to a server on the selected node.')
                             ->label('Linked Node')
                             ->relationship('node', 'name'),
