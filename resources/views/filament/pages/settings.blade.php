3920347c8b6bcff94a2cd135c9d0815d012f98dd
notCharles <charles@pelican.dev>
2024-12-07 11:21:49 -0500

    Remove unsaved changes alert

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,15 +1,13 @@
 <x-filament-panels::page
-	@class([
-		'fi-page-settings'
-	])
+    @class([
+        'fi-page-settings'
+    ])
 >
     <x-filament-panels::form
-		id="form"
-		:wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
-		wire:submit="save"
-	>
+        id="form"
+        :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
+        wire:submit="save"
+    >
         {{ $this->form }}
     </x-filament-panels::form>
-
-    <x-filament-panels::page.unsaved-data-changes-alert />
 </x-filament-panels::page>
