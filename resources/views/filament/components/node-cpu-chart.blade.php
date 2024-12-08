6b55df5f657a3702c5c2828ccecb75cdc92527c8
Lance Pioch <git@lance.sh>
2024-12-07 20:33:21 -0500

    Fix path to moved component

--- resources/views/filament/components/node-cpu-chart.blade.php
+++ resources/views/filament/components/node-cpu-chart.blade.php
@@ -1,3 +1,3 @@
 <x-filament::widget>
-        @livewire(\App\Filament\Resources\NodeResource\Widgets\NodeCpuChart::class, ['record'=> $getRecord()])
+        @livewire(\App\Filament\Admin\Resources\NodeResource\Widgets\NodeCpuChart::class, ['record'=> $getRecord()])
 </x-filament::widget>
