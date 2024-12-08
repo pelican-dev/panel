839ff96271c663939e12995483fe38e6923397cc
Lance Pioch <git@lance.sh>
2024-12-08 16:19:15 -0500

    Fix power buttons (#799)

--- app/Filament/Server/Pages/Console.php
+++ app/Filament/Server/Pages/Console.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Server\Pages;
 
+use App\Enums\ContainerStatus;
 use App\Filament\Server\Widgets\ServerConsole;
 use App\Filament\Server\Widgets\ServerCpuChart;
 use App\Filament\Server\Widgets\ServerMemoryChart;
@@ -12,6 +13,7 @@ use Filament\Actions\Action;
 use Filament\Facades\Filament;
 use Filament\Pages\Page;
 use Filament\Support\Enums\ActionSize;
+use Livewire\Attributes\On;
 
 class Console extends Page
 {
@@ -20,6 +22,8 @@ class Console extends Page
     protected static ?int $navigationSort = 1;
 
     protected static string $view = 'filament.server.pages.console';
+
+    public ContainerStatus $status = ContainerStatus::Missing;
 
     public function getWidgetData(): array
     {
@@ -50,6 +54,16 @@ class Console extends Page
         return 3;
     }
 
+    #[On('powerChanged')]
+    public function powerChanged(string $state): void
+    {
+        $this->status = ContainerStatus::from($state);
+
+        $this->cachedHeaderActions = [];
+
+        $this->cacheHeaderActions();
+    }
+
     protected function getHeaderActions(): array
     {
         /** @var Server $server */
@@ -60,17 +74,18 @@ class Console extends Page
                 ->color('primary')
                 ->size(ActionSize::ExtraLarge)
                 ->action(fn () => $this->dispatch('setServerState', state: 'start'))
-                ->disabled(fn () => $server->isInConflictState()),
+                ->disabled(fn () => $server->isInConflictState() || in_array($this->status, [ContainerStatus::Running, ContainerStatus::Starting, ContainerStatus::Stopping, ContainerStatus::Restarting])),
             Action::make('restart')
                 ->color('gray')
                 ->size(ActionSize::ExtraLarge)
                 ->action(fn () => $this->dispatch('setServerState', state: 'restart'))
-                ->disabled(fn () => $server->isInConflictState() || $server->retrieveStatus() == 'offline'),
+                ->disabled(fn () => $server->isInConflictState() || $this->status !== ContainerStatus::Running),
             Action::make('stop')
                 ->color('danger')
                 ->size(ActionSize::ExtraLarge)
                 ->action(fn () => $this->dispatch('setServerState', state: 'stop'))
-                ->disabled(fn () => $server->isInConflictState() || $server->retrieveStatus() == 'offline'),
+                ->hidden(fn () => in_array($this->status, [ContainerStatus::Stopping, ContainerStatus::Restarting, ContainerStatus::Starting]))
+                ->disabled(fn () => $server->isInConflictState() || in_array($this->status, [ContainerStatus::Starting, ContainerStatus::Stopping, ContainerStatus::Restarting, ContainerStatus::Exited, ContainerStatus::Offline])),
             Action::make('kill')
                 ->color('danger')
                 ->requiresConfirmation()
@@ -79,7 +94,8 @@ class Console extends Page
                 ->modalSubmitActionLabel('Kill Server')
                 ->size(ActionSize::ExtraLarge)
                 ->action(fn () => $this->dispatch('setServerState', state: 'kill'))
-                ->disabled(fn () => $server->isInConflictState() || $server->retrieveStatus() == 'offline'),
+                ->hidden(fn () => $server->isInConflictState() || in_array($this->status, [ContainerStatus::Running, ContainerStatus::Restarting, ContainerStatus::Offline, ContainerStatus::Removing, ContainerStatus::Dead, ContainerStatus::Exited, ContainerStatus::Created]))
+                ->disabled(fn () => $server->isInConflictState() || $this->status === ContainerStatus::Offline),
         ];
     }
 }
