1358c01aabf6c8ad1398a7fffdd213505c3d3d72
Boy132 <mail@boy132.de>
2024-12-10 16:18:35 +0100

    add listener for Installed event

---
+++ app/Listeners/Server/ServerInstalledListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners\Server;
+
+use App\Events\Server\Installed;
+use App\Filament\Server\Pages\Console;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
+
+class ServerInstalledListener
+{
+    public function handle(Installed $event): void
+    {
+        Notification::make()
+            ->status($event->successful ? 'success' : 'danger')
+            ->title('Server Installation ' . ($event->successful ? 'completed' : 'failed'))
+            ->body($event->server->name)
+            ->actions([
+                Action::make('view')
+                    ->button()
+                    ->label('Open Server')
+                    ->markAsRead()
+                    ->url(fn () => Console::getUrl(panel: 'server', tenant: $event->server)),
+            ])
+            ->sendToDatabase($event->server->user);
+    }
+}
