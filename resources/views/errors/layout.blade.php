e851d6e0f5b34909696304a95b2e4c0b8799d621
Lance Pioch <lancepioch@gmail.com>
2024-12-10 17:28:35 -0500

    Update resources/views/errors/layout.blade.php

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link rel="icon" href="{{ config('app.favicon', '/pelican.ico') }}" />
+    <link rel="icon" href="{{ config('app.favicon') }}" />
 
     <title>
         {{ $title }} - {{ config('app.name') }}
