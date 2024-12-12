d4fa1097344f9cefac36ae04ad2130cb1acbdac2
Boy132 <mail@boy132.de>
2024-12-12 14:36:21 +0100

    remove view action from SubUserRemoved notification

--- app/Listeners/Server/SubUserRemovedListener.php
+++ app/Listeners/Server/SubUserRemovedListener.php
@@ -3,8 +3,6 @@
 namespace App\Listeners\Server;
 
 use App\Events\Server\SubUserRemoved;
-use App\Filament\Server\Pages\Console;
-use Filament\Notifications\Actions\Action;
 use Filament\Notifications\Notification;
 
 class SubUserRemovedListener
@@ -14,13 +12,6 @@ class SubUserRemovedListener
         Notification::make()
             ->title('Removed from Server')
             ->body('You have been removed as a subuser from ' . $event->server->name . '.')
-            ->actions([
-                Action::make('view')
-                    ->button()
-                    ->label('Open Server')
-                    ->markAsRead()
-                    ->url(fn () => Console::getUrl(panel: 'server', tenant: $event->server)),
-            ])
             ->sendToDatabase($event->user);
     }
 }
