a3ecf3994b8017f7b5854564599e385e9c2b94cf
Scai <59282365+alexevladgabriel@users.noreply.github.com>
2025-01-06 15:46:43 +0200

    feat: set filament main client ui

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -25,7 +25,7 @@ class AppPanelProvider extends PanelProvider
     {
         return $panel
             ->id('app')
-            ->path('app')
+            ->path('')
             ->spa()
             ->breadcrumbs(false)
             ->brandName(config('app.name', 'Pelican'))
