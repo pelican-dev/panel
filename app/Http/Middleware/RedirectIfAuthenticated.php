c5100aefa02d3e1fd60020ce577c4ec1a404617f
Lance Pioch <git@lance.sh>
2025-01-07 17:01:05 -0500

    Filament hasn’t been loaded yet

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,24 +2,17 @@
 
 namespace App\Http\Middleware;
 
-use App\Filament\App\Resources\ServerResource\Pages\ListServers;
 use Illuminate\Http\Request;
 use Illuminate\Auth\AuthManager;
 
-class RedirectIfAuthenticated
+readonly class RedirectIfAuthenticated
 {
-    /**
-     * RedirectIfAuthenticated constructor.
-     */
     public function __construct(private AuthManager $authManager) {}
 
-    /**
-     * Handle an incoming request.
-     */
     public function handle(Request $request, \Closure $next, ?string $guard = null): mixed
     {
         if ($this->authManager->guard($guard)->check()) {
-            return redirect(ListServers::getUrl());
+            return redirect('/');
         }
 
         return $next($request);
