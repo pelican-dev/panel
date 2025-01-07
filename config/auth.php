6fcf4173d3b8ec6941f2f2903a0c55fda6c5f9db
Boy132 <Boy132@users.noreply.github.com>
2025-01-07 22:47:23 +0100

    Strip http/ https from steam oauth `allowed_host` (#889)
    * strip http/ https from steam oauth allowed_host
    
    * fix param order

--- config/auth.php
+++ config/auth.php
@@ -130,7 +130,7 @@ return [
                 'client_id' => null,
                 'client_secret' => env('OAUTH_STEAM_CLIENT_SECRET'),
                 'allowed_hosts' => [
-                    env('APP_URL'),
+                    str_replace(['http://', 'https://'], '', env('APP_URL')),
                 ],
             ],
             'provider' => \SocialiteProviders\Steam\Provider::class,
