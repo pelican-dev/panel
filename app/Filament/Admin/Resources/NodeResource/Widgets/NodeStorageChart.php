1d360238f194401869ebc7dc7c543fc9587234c6
notCharles <charles@pelican.dev>
2025-01-12 11:04:10 -0500

    Wings gives us bytes, use helper function

--- app/Filament/Admin/Resources/NodeResource/Widgets/NodeStorageChart.php
+++ app/Filament/Admin/Resources/NodeResource/Widgets/NodeStorageChart.php
@@ -43,8 +43,8 @@ class NodeStorageChart extends ChartWidget
         /** @var Node $node */
         $node = $this->record;
 
-        $total = Number::format(config('panel.use_binary_prefix') ? ($node->statistics()['disk_total'] ?? 0) / 1024 / 1024 / 1024 : ($node->statistics()['disk_total'] ?? 0) / 1000 / 1000 / 1000, maxPrecision: 2, locale: auth()->user()->language);
-        $used = Number::format(config('panel.use_binary_prefix') ? ($node->statistics()['disk_used'] ?? 0) / 1024 / 1024 / 1024 : ($node->statistics()['disk_used'] ?? 0) / 1000 / 1000 / 1000, maxPrecision: 2, locale: auth()->user()->language);
+        $total = Number::format(($node->statistics()['disk_total'] ?? 0) / 1024 / 1024 / 1024, maxPrecision: 2, locale: auth()->user()->language);
+        $used = Number::format(($node->statistics()['disk_used'] ?? 0) / 1024 / 1024 / 1024, maxPrecision: 2, locale: auth()->user()->language);
 
         $unused = $total - $used;
 
@@ -73,17 +73,6 @@ class NodeStorageChart extends ChartWidget
         /** @var Node $node */
         $node = $this->record;
 
-        $diskTotal = $node->statistics()['disk_total'];
-        $diskUsed = $node->statistics()['disk_used'];
-
-        $used = config('panel.use_binary_prefix')
-            ? Number::format($diskUsed / 1024 / 1024 / 1024, maxPrecision: 2, locale: auth()->user()->language) .' GiB'
-            : Number::format($diskUsed / 1000 / 1000 / 1000, maxPrecision: 2, locale: auth()->user()->language) . ' GB';
-
-        $total = config('panel.use_binary_prefix')
-            ? Number::format($diskTotal / 1024 / 1024 / 1024, maxPrecision: 2, locale: auth()->user()->language) .' GiB'
-            : Number::format($diskTotal / 1000 / 1000 / 1000, maxPrecision: 2, locale: auth()->user()->language) . ' GB';
-
-        return 'Storage - ' . $used . ' Of ' . $total;
+        return 'Storage - ' . convert_bytes_to_readable($node->statistics()['disk_used'] ?? 0) . ' Of ' . convert_bytes_to_readable($node->statistics()['disk_total'] ?? 0);
     }
 }
