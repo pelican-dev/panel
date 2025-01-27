b345769dc36218f21133bf23ec0233d46c6612dc
notCharles <charles@pelican.dev>
2025-01-27 15:34:04 -0500

    Init

--- lang/hr/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Pretraži servere...',
-    'no_matches' => 'Nisu pronađeni serveri koji odgovaraju navedenim kriterijima pretraživanja.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Memorija',
-];
--- lang/sk/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Hľadať servery...',
-    'no_matches' => 'Nenašli sa žiadne servery zodpovedajúce zadaným kritériám vyhľadávania.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'Pamäť',
-];
