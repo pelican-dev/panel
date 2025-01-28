3362e969e8b917df6aea9b8a96fce6b92a8e0820
notCharles <charles@pelican.dev>
2025-01-27 23:01:31 -0500

    Webhooks

---
+++ lang/en/admin/webhook.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'create_action' => ':action Webhook',
+    'endpoint' => 'Endpoint',
+    'description' => 'Description',
+    'events' => 'Events',
+    'table' => [
+        'description' => 'Description',
+        'endpoint' => 'Endpoint',
+    ],
+];
