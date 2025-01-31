da36e96e99aad6b92089adb5713b493c09779d81
Boy132 <mail@boy132.de>
2025-01-31 09:13:25 +0100

    update ApiKeyResource

--- app/Filament/Admin/Resources/ApiKeyResource.php
+++ app/Filament/Admin/Resources/ApiKeyResource.php
@@ -3,8 +3,20 @@
 namespace App\Filament\Admin\Resources;
 
 use App\Filament\Admin\Resources\ApiKeyResource\Pages;
+use App\Filament\Admin\Resources\UserResource\Pages\EditUser;
+use App\Filament\Components\Tables\Columns\DateTimeColumn;
 use App\Models\ApiKey;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class ApiKeyResource extends Resource
 {
@@ -22,7 +34,107 @@ class ApiKeyResource extends Resource
 
     public static function getNavigationBadge(): ?string
     {
-        return static::getModel()::where('key_type', ApiKey::TYPE_APPLICATION)->count() ?: null;
+        return static::getEloquentQuery()->count() ?: null;
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $query = parent::getEloquentQuery();
+
+        return $query->where('key_type', ApiKey::TYPE_APPLICATION);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('key')
+                    ->icon('tabler-clipboard-text')
+                    ->state(fn (ApiKey $key) => $key->identifier . $key->token)
+                    ->copyable(),
+                TextColumn::make('memo')
+                    ->label('Description')
+                    ->wrap()
+                    ->limit(50),
+                DateTimeColumn::make('last_used_at')
+                    ->label('Last Used')
+                    ->placeholder('Not Used')
+                    ->sortable(),
+                DateTimeColumn::make('created_at')
+                    ->label('Created')
+                    ->sortable(),
+                TextColumn::make('user.username')
+                    ->label('Created By')
+                    ->icon('tabler-user')
+                    ->url(fn (ApiKey $apiKey) => auth()->user()->can('update user', $apiKey->user) ? EditUser::getUrl(['record' => $apiKey->user]) : null),
+            ])
+            ->actions([
+                DeleteAction::make(),
+            ])
+            ->emptyStateIcon('tabler-key')
+            ->emptyStateDescription('')
+            ->emptyStateHeading('No API Keys')
+            ->emptyStateActions([
+                CreateAction::make('create')
+                    ->label('Create API Key')
+                    ->button(),
+            ]);
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Fieldset::make('Permissions')
+                    ->columns([
+                        'default' => 1,
+                        'sm' => 1,
+                        'md' => 2,
+                    ])
+                    ->schema(
+                        collect(ApiKey::getPermissionList())->map(fn ($resource) => ToggleButtons::make('permissions_' . $resource)
+                            ->label(str($resource)->replace('_', ' ')->title())->inline()
+                            ->options([
+                                0 => 'None',
+                                1 => 'Read',
+                                // 2 => 'Write',
+                                3 => 'Read & Write',
+                            ])
+                            ->icons([
+                                0 => 'tabler-book-off',
+                                1 => 'tabler-book',
+                                2 => 'tabler-writing',
+                                3 => 'tabler-writing',
+                            ])
+                            ->colors([
+                                0 => 'success',
+                                1 => 'warning',
+                                2 => 'danger',
+                                3 => 'danger',
+                            ])
+                            ->required()
+                            ->columnSpan([
+                                'default' => 1,
+                                'sm' => 1,
+                                'md' => 1,
+                            ])
+                            ->default(0),
+                        )->all(),
+                    ),
+                TagsInput::make('allowed_ips')
+                    ->placeholder('Example: 127.0.0.1 or 192.168.1.1')
+                    ->label('Whitelisted IPv4 Addresses')
+                    ->helperText('Press enter to add a new IP address or leave blank to allow any IP address')
+                    ->columnSpanFull(),
+                Textarea::make('memo')
+                    ->required()
+                    ->label('Description')
+                    ->helperText('
+                        Once you have assigned permissions and created this set of credentials you will be unable to come back and edit it.
+                        If you need to make changes down the road you will need to create a new set of credentials.
+                    ')
+                    ->columnSpanFull(),
+            ]);
     }
 
     public static function getPages(): array
