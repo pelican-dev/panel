88d61b33ba51b2f071425466375630e4aa8f9382
Boy132 <mail@boy132.de>
2025-01-31 10:01:55 +0100

    update UserResource

--- app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
@@ -3,57 +3,21 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
-use App\Models\Role;
 use App\Models\User;
-use App\Services\Helpers\LanguageService;
+use App\Services\Users\UserUpdateService;
 use Filament\Actions\DeleteAction;
-use Filament\Forms\Components\CheckboxList;
-use Filament\Forms\Components\Hidden;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Model;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
-    public function form(Form $form): Form
+    private UserUpdateService $service;
+
+    public function boot(UserUpdateService $service): void
     {
-        return $form
-            ->schema([
-                Section::make()->schema([
-                    TextInput::make('username')
-                        ->required()
-                        ->minLength(3)
-                        ->maxLength(255),
-                    TextInput::make('email')
-                        ->email()
-                        ->required()
-                        ->maxLength(255),
-                    TextInput::make('password')
-                        ->dehydrateStateUsing(fn (string $state): string => Hash::make($state))
-                        ->dehydrated(fn (?string $state): bool => filled($state))
-                        ->password(),
-                    Select::make('language')
-                        ->required()
-                        ->hidden()
-                        ->default('en')
-                        ->options(fn (LanguageService $languageService) => $languageService->getAvailableLanguages()),
-                    Hidden::make('skipValidation')
-                        ->default(true),
-                    CheckboxList::make('roles')
-                        ->disabled(fn (User $user) => $user->id === auth()->user()->id)
-                        ->disableOptionWhen(fn (string $value): bool => $value == Role::getRootAdmin()->id)
-                        ->relationship('roles', 'name')
-                        ->label('Admin Roles')
-                        ->columnSpanFull()
-                        ->bulkToggleable(false),
-                ])
-                    ->columns(['default' => 1, 'lg' => 3]),
-            ]);
+        $this->service = $service;
     }
 
     protected function getHeaderActions(): array
@@ -69,5 +33,10 @@ class EditUser extends EditRecord
     protected function getFormActions(): array
     {
         return [];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        return $this->service->handle($record, $data);
     }
 }
