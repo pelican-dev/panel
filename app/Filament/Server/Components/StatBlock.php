60ac04df69a913325daa6f83fe2b1bfe38f2b60b
Charles <charles@pelican.dev>
2025-02-25 06:17:37 -0500

    Removals

--- app/Filament/Server/Components/StatBlock.php
+++ app/Filament/Server/Components/StatBlock.php
@@ -2,42 +2,19 @@
 
 namespace App\Filament\Server\Components;
 
-use Filament\Support\Concerns\Macroable;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Str;
-use Illuminate\View\ComponentAttributeBag;
 
 class StatBlock extends Stat
 {
-    use Macroable;
-
-    protected array $extraAttributes = [];
-
-    protected ?string $id = null;
-
     protected string|Htmlable $label;
 
     protected $value;
 
-    public function extraAttributes(array $attributes): static
-    {
-        $this->extraAttributes = $attributes;
-
-        return $this;
-    }
-
     public function label(string|Htmlable $label): static
     {
         $this->label = $label;
-
-        return $this;
-    }
-
-    public function id(string $id): static
-    {
-        $this->id = $id;
 
         return $this;
     }
@@ -49,24 +26,9 @@ class StatBlock extends Stat
         return $this;
     }
 
-    public function getExtraAttributes(): array
-    {
-        return $this->extraAttributes;
-    }
-
-    public function getExtraAttributeBag(): ComponentAttributeBag
-    {
-        return new ComponentAttributeBag($this->getExtraAttributes());
-    }
-
     public function getLabel(): string|Htmlable
     {
         return $this->label;
-    }
-
-    public function getId(): string
-    {
-        return $this->id ?? Str::slug($this->getLabel());
     }
 
     public function getValue()
