82815570dea21912221ef122963ef0d41aa2496d
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2025-02-25 17:29:18 +0100

    style(api-docs): use Blade template and Tailwind for styling

--- routes/docs.php
+++ routes/docs.php
@@ -10,10 +10,7 @@ Route::group(['prefix' => 'api'], function () {
     Scramble::registerUiRoute(path: 'client', api: 'client');
     Scramble::registerJsonSpecificationRoute(path: 'client.json', api: 'client');
 
-    Route::get('', fn () => '
-        <h2>API Docs</h2>
-        <li><a href="/docs/api/application">Application API</a></li>
-        <li><a href="/docs/api/client">Client API</a></li>
-        <p>Note: You need to be logged in to view the api docs!</p>
-    ');
+    Route::get('', function () {
+        return view('docs.api-index');
+    });
 });
