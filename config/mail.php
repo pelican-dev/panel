6d5df198503d48b6e7c0be87fd8e5f0fb030a2e0
Lance Pioch <git@lance.sh>
2025-02-28 19:22:02 -0500

    Add old key as backup

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => env('MAIL_SCHEME', env('MAIL_ENCRYPTION')),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
