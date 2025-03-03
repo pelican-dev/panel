36a38ab947a42590db98e6b3dadcb96835d8cd35
Lance Pioch <git@lance.sh>
2025-03-03 15:22:12 -0500

    Basic two factor auth implementation (#1050)
    * Basic two factor auth
    
    * Remove unused import
    
    * Add translation

---
+++ lang/en/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'failed-two-factor' => 'Incorrect 2FA Code',
+    'two-factor-code' => 'Two Factor Code',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
