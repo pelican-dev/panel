54461a57f93a670aceb16b788bc8751d6aa0f578
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-03-08 17:58:13 +0000

    Requested changes

--- app/Filament/Components/Actions/ImportEggAction.php
+++ app/Filament/Components/Actions/ImportEggAction.php
@@ -45,15 +45,16 @@ class ImportEggAction extends Action
                     Tab::make(trans('admin/egg.import.url'))
                         ->icon('tabler-world-upload')
                         ->schema([
-                            Repeater::make('url')
-                                ->label('Egg')
+                            Repeater::make('urls')
+                                ->itemLabel(fn (array $state) => $state['url'] ? str($state['url'])->afterLast('/')->before('.json') : null)
                                 ->hint(trans('admin/egg.import.url_help'))
                                 ->addActionLabel(trans('admin/egg.import.add_url'))
-                                ->unique()
+                                ->grid()
                                 ->reorderable(false)
                                 ->schema([
                                     TextInput::make('url')
                                         ->default(fn (Egg $egg) => $egg->update_url)
+                                        ->live()
                                         ->label(trans('admin/egg.import.url'))
                                         ->placeholder('https://raw.githubusercontent.com/pelican-eggs/generic/main/nodejs/egg-node-js-generic.json')
                                         ->url(),
@@ -63,38 +64,52 @@ class ImportEggAction extends Action
         ]);
 
         $this->action(function (array $data, EggImporterService $eggImportService): void {
-            try {
-                if (!empty($data['egg'])) {
-                    $eggFiles = $data['egg'];
+            [$success, $failed] = [collect(), collect()];
 
-                    foreach ($eggFiles as $file) {
+            if (!empty($data['egg'])) {
+                $eggFiles = $data['egg'];
+
+                foreach ($eggFiles as $file) {
+                    $name = str($file)->afterLast('/')->before('.json');
+                    try {
                         $eggImportService->fromFile($file);
+                        $success->push($name);
+                    } catch (Exception $exception) {
+                        $failed->push($name);
+                        report($exception);
                     }
                 }
-
-                if (!empty($data['url'])) {
-                    $eggUrls = collect($data['url'])->flatten()->all();
-
-                    foreach ($eggUrls as $url) {
-                        $eggImportService->fromUrl($url);
-                    }
-                }
-            } catch (Exception $exception) {
-                Notification::make()
-                    ->title(trans('admin/egg.import.import_failed'))
-                    ->body($exception->getMessage())
-                    ->danger()
-                    ->send();
-
-                report($exception);
-
-                return;
             }
 
-            Notification::make()
-                ->title(trans('admin/egg.import.import_success'))
-                ->success()
-                ->send();
+            if (!empty($data['urls'])) {
+                $eggUrls = collect($data['urls'])->flatten()->unique()->all();
+
+                foreach ($eggUrls as $url) {
+                    $name = str($url)->afterLast('/')->before('.json');
+                    try {
+                        $eggImportService->fromUrl($url);
+                        $success->push($name);
+                    } catch (Exception $exception) {
+                        $failed->push($name);
+                        report($exception);
+                    }
+                }
+            }
+
+            if (count($failed) > 0) {
+                Notification::make()
+                    ->title(trans('admin/egg.import.import_failed'))
+                    ->body($failed->join(', '))
+                    ->danger()
+                    ->send();
+            }
+            if (count($success) > 0) {
+                Notification::make()
+                    ->title(trans('admin/egg.import.import_success'))
+                    ->body($success->join(', '))
+                    ->success()
+                    ->send();
+            }
         });
     }
 }
