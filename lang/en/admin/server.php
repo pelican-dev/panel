96f894b7ef09627acdf8548f34696f4058026985
Charles <charles@pelican.dev>
2025-03-08 15:18:07 -0500

    Update lang/en/admin/server.php
    Co-authored-by: Boy132 <Boy132@users.noreply.github.com>

--- lang/en/admin/server.php
+++ lang/en/admin/server.php
@@ -36,7 +36,7 @@ return [
     'disabled' => 'Disabled',
     'memory' => 'Memory',
     'memory_limit' => 'Memory Limit',
-    'memory_helper' => 'Wings will add overhead to this value when creating the container. <=2048MB = +10%, <=4069MB = +15%, anything else = +5%',
+    'memory_helper' => 'Wings will add overhead to this value when creating the container to make sure it doesn't starve out when using max memory.',
     'disk' => 'Disk Space',
     'disk_limit' => 'Disk Space Limit',
     'advanced_limits' => 'Advanced Limits',
