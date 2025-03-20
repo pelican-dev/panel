cd670430ef693908a0049ec8be97f8e4a55c4575
Vehikl <pelican.vehikl@gmail.com>
2025-03-20 16:59:37 -0400

    Test Filament

--- app/Enums/RolePermissionModels.php
+++ app/Enums/RolePermissionModels.php
@@ -14,4 +14,24 @@ enum RolePermissionModels: string
     case Server = 'server';
     case User = 'user';
     case Webhook = 'webhook';
+
+    public function viewAny(): string
+    {
+        return RolePermissionPrefixes::ViewAny->value . ' ' . $this->value;
+    }
+
+    public function view(): string
+    {
+        return RolePermissionPrefixes::View->value . ' ' . $this->value;
+    }
+
+    public function create(): string
+    {
+        return RolePermissionPrefixes::Create->value . ' ' . $this->value;
+    }
+
+    public function update(): string
+    {
+        return RolePermissionPrefixes::Update->value . ' ' . $this->value;
+    }
 }
