43f5cfeb9f06e908ed83c1fce7c49287ffe0e163
Boy132 <mail@boy132.de>
2025-03-21 13:16:39 +0100

    update `next_run_at` when editing & show notification if cron is invalid

--- app/Filament/Server/Resources/ScheduleResource/Pages/EditSchedule.php
+++ app/Filament/Server/Resources/ScheduleResource/Pages/EditSchedule.php
@@ -4,9 +4,13 @@ namespace App\Filament\Server\Resources\ScheduleResource\Pages;
 
 use App\Facades\Activity;
 use App\Filament\Server\Resources\ScheduleResource;
+use App\Helpers\Utilities;
 use App\Models\Schedule;
+use Exception;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Exceptions\Halt;
 
 class EditSchedule extends EditRecord
 {
@@ -20,6 +24,28 @@ class EditSchedule extends EditRecord
         Activity::event('server:schedule.update')
             ->property('name', $schedule->name)
             ->log();
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        try {
+            $data['next_run_at'] = Utilities::getScheduleNextRunDate(
+                $data['cron_minute'],
+                $data['cron_hour'],
+                $data['cron_day_of_month'],
+                $data['cron_month'],
+                $data['cron_day_of_week']
+            );
+        } catch (Exception) {
+            Notification::make()
+                ->title('The cron data provided does not evaluate to a valid expression')
+                ->danger()
+                ->send();
+
+            throw new Halt();
+        }
+
+        return $data;
     }
 
     protected function getHeaderActions(): array
