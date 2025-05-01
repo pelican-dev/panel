305374192fe9064d7abfdc4e5373d1b80020c5ed
Vehikl <pelican.vehikl@gmail.com>
2025-05-01 16:55:43 -0400

    Add `OAuthServiceProvider`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,13 +18,6 @@ use App\Extensions\Features\JavaVersion;
 use App\Extensions\Features\MinecraftEula;
 use App\Extensions\Features\PIDLimit;
 use App\Extensions\Features\SteamDiskSpace;
-use App\Extensions\OAuth\OAuthProvider;
-use App\Extensions\OAuth\Schemas\AuthentikSchema;
-use App\Extensions\OAuth\Schemas\CommonSchema;
-use App\Extensions\OAuth\Schemas\DiscordSchema;
-use App\Extensions\OAuth\Schemas\GithubSchema;
-use App\Extensions\OAuth\Schemas\GitlabSchema;
-use App\Extensions\OAuth\Schemas\SteamSchema;
 use App\Models;
 use App\Services\Helpers\SoftwareVersionService;
 use Dedoc\Scramble\Scramble;
@@ -104,26 +97,6 @@ class AppServiceProvider extends ServiceProvider
         $bearerTokens = fn (OpenApi $openApi) => $openApi->secure(SecurityScheme::http('bearer'));
         Scramble::registerApi('application', ['api_path' => 'api/application', 'info' => ['version' => '1.0']])->afterOpenApiGenerated($bearerTokens);
         Scramble::registerApi('client', ['api_path' => 'api/client', 'info' => ['version' => '1.0']])->afterOpenApiGenerated($bearerTokens);
-
-        $this->app->singleton(OAuthProvider::class, function ($app) {
-            $provider = new OAuthProvider();
-            // Default OAuth providers included with Socialite
-            $provider->register(new CommonSchema('facebook', null, 'tabler-brand-facebook-f', '#1877f2'));
-            $provider->register(new CommonSchema('x', null, 'tabler-brand-x-f', '#1da1f2'));
-            $provider->register(new CommonSchema('linkedin', null, 'tabler-brand-linkedin-f', '#0a66c2'));
-            $provider->register(new CommonSchema('google', null, 'tabler-brand-google-f', '#4285f4'));
-            $provider->register(new GithubSchema());
-            $provider->register(new GitlabSchema());
-            $provider->register(new CommonSchema('bitbucket', null, 'tabler-brand-bitbucket-f', '#205081'));
-            $provider->register(new CommonSchema('slack', null, 'tabler-brand-slack', '#6ecadc'));
-
-            // Additional OAuth providers from socialiteproviders.com
-            $provider->register(new AuthentikSchema());
-            $provider->register(new DiscordSchema());
-            $provider->register(new SteamSchema());
-
-            return $provider;
-        });
 
         // Default Captcha provider
         TurnstileProvider::register($app);
