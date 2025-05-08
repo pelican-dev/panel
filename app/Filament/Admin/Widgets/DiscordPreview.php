4c8d1616cb3fab33b4012523dcc2a5cf115e46eb
JoanFo <161775222+JoanFo1456@users.noreply.github.com>
2025-05-08 20:23:33 +0000

    Working DiscordWebhooks

---
+++ app/Filament/Admin/Widgets/DiscordPreview.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use App\Models\WebhookConfiguration;
+use Filament\Widgets\Widget;
+
+class DiscordPreview extends Widget
+{
+    protected static string $view = 'filament.admin.widgets.discord-preview';
+
+    /** @var array<string, string> */
+    protected $listeners = [
+        'refresh-widget' => '$refresh',
+    ];
+
+    protected static bool $isDiscovered = false; // Without this its shown on every Admin Pages
+
+    protected int|string|array $columnSpan = 1;
+
+    public ?WebhookConfiguration $record = null;
+    
+    public function getViewData(): array
+    {
+        if (!$this->record || !$this->record->payload) {
+            return [
+                'link' => fn ($href, $child) => $href ? sprintf('<a href="%s" target="_blank" class="link">%s</a>', $href, $child) : $child,
+                'content' => null,
+                'sender' => $this->easterEgg(null),
+                'embeds' => [],
+                'getTime' => WebhookConfiguration::getTime(),
+            ];
+        }
+        
+        $data = $this->record->run(true);
+
+        // TODO: Change processPayload to json_encode, as this is a temporal fix
+        $payload = $this->processPayload($this->record->payload ?? [], $data);
+
+        $embeds = data_get($payload, 'embeds', []);
+
+        foreach ($embeds as &$embed) {
+            if (data_get($embed, 'has_timestamp')) {
+                unset($embed['has_timestamp']);
+                $embed['timestamp'] = $this->record->getTime();
+            }
+        }
+
+        return [
+            'link' => fn ($href, $child) => $href ? sprintf('<a href="%s" target="_blank" class="link">%s</a>', $href, $child) : $child,
+            'content' => data_get($payload, 'content'),
+            'sender' => $this->easterEgg(data_get($payload, 'username')),
+            'embeds' => $embeds,
+            'getTime' => $this->record->getTime(),
+        ];
+    }
+
+    /**
+     *
+     * @param array|string $payload
+     * @param array $data
+     * @return array|string
+     */
+    private function processPayload(array|string $payload, array $data): array|string
+    {
+        if (!$this->record) {
+            return is_array($payload) ? $payload : (string) $payload;
+        }
+        
+        if (is_string($payload)) {
+            return $this->record->replaceVars($data, $payload);
+        }
+
+        foreach ($payload as $key => $value) {
+            $payload[$key] = is_array($value) || is_string($value)
+                ? $this->processPayload($value, $data)
+                : $value;
+        }
+
+        return $payload;
+    }
+    
+
+    /** @return array<string, mixed> */
+    private function easterEgg(?string $author): array
+    {
+        $avatar = $this->record ? data_get($this->record->payload, 'avatar_url') : null;
+
+
+        // If this is approved, add the other pelican contributors.
+        return match ($author) {
+            'Lance' => [
+                'name' => $author,
+                'avatar' => 'https://cdn.discordapp.com/avatars/108350949411532800/5c0366c62ccb4263734f9decebf4944d.png',
+                'decoration' => 'https://cdn.discordapp.com/avatar-decoration-presets/a_b3d5743ff7a2cda95d28fd984f82a5f8.png?size=96&amp;amp;passthrough=false',
+                'human' => true,
+            ],
+            'notCharles' => [
+                'name' => $author,
+                'avatar' => 'https://cdn.discordapp.com/avatars/168955129830178816/d6de49de0ff5f3f3338c8cad510825cf.png',
+                'decoration' => null,
+                'human' => true,
+            ],
+            'JoanFo' => [
+                'name' => $author,
+                'avatar' => 'https://www.gravatar.com/avatar/8a50b66d9270c58d382cc3c840ec8078',
+                'decoration' => 'https://cdn.discordapp.com/avatar-decoration-presets/a_af5ee420e5f860ff2cdbb5fa4633f2cf.png?size=96&amp;amp;passthrough=false',
+                'human' => true,
+            ],
+            default => [
+                'name' => $author ?? 'Pelican',
+                'avatar' => $avatar ?? 'https://cdn.discordapp.com/avatars/1222179499253170307/d4d6873acc8a0d5fb5eaa5aa81572cf3.png',
+                'decoration' => null,
+                'human' => false,
+            ]
+        };
+    }
+}
---
+++ resources/views/filament/admin/widgets/discord-preview.blade.php
@@ -0,0 +1,121 @@
+<x-filament-widgets::widget>
+    @assets
+        <link rel="stylesheet" href="{{ asset('/css/filament/admin/discord-preview.css') }}">
+    @endassets
+    <div class="container mx-auto p-4 bg-gray-800 p-4 rounded-lg shadow-lg w-full max-w-full flex items-start mb-4 sender">
+        <div class="relative" style="width: 44px; min-width: 44px; height: 44px; margin-right: 12px;">
+            @if($avatar = data_get($sender, 'avatar'))
+                <img
+                    src="{{ $avatar }}"
+                    alt="Avatar"
+                    class="w-full h-full rounded-full object-cover absolute top-0 left-0 z-10 avatar"
+                    >
+            @endif
+            @if($decoration = data_get($sender, 'decoration'))
+            <img
+                src="{{ $decoration }}"
+                alt="Discord Decoration"
+                class="decoration-overlay absolute decoration"
+                >
+            @endif
+        </div>
+
+        <div class="flex flex-col flex-grow">
+            <div class="flex items-center space-x-2">
+                <h1 class="font-bold text-white name">{{ data_get($sender, 'name') }}</h1>
+                @if(!data_get($sender, 'human'))
+                    <span class="text-white text-xs rounded-md tag">app</span>
+                @endif
+                <span class="timestamp text-xs">{{ $getTime }}</span>
+            </div>
+
+            @if(filled($content))
+                <p class="text-gray-300 break-words">{!! nl2br($content) !!}</p>
+            @endif
+
+            @foreach($embeds as $embed)
+                @php
+                    $name = $name = data_get($embed, 'author.name');
+                    $thumbnail = data_get($embed, 'thumbnail.url');
+                    $author_icon_url = data_get($embed, 'author.icon_url');
+                    $author_url = data_get($embed, 'author.url');
+
+                    $footer_icon_url = data_get($embed, 'footer.icon_url');
+                    $footer_text = data_get($embed, 'footer.text');
+                    $footer_timestamp = data_get($embed, 'timestamp');
+                @endphp
+                <div class="p-3 mt-3 rounded-lg w-full max-w-full embed relative" style="border-color: #{{ dechex(data_get($embed, 'color')) }}">
+                    @if($name || $thumbnail)
+                        <div class="flex items-start mb-0 relative" style="height: auto; overflow: visible;">
+                            @if($author_icon_url || $name)
+                                <div class="flex items-center">
+                                    @if($author_icon_url)
+                                        <img src="{{ $author_icon_url }}" alt="Author Avatar" class="w-8 h-8 rounded-full mr-2 object-cover avatar">
+                                    @endif
+                                        {!! $link($author_url, <<<HTML
+                                            @if($name)
+                                                <h2 class="font-bold text-lg whitespace-nowrap">$name</h2>
+                                            @endif
+                                        HTML) !!}
+                                </div>
+                            @endif
+                            @if($thumbnail)
+                                <img src="{{ $thumbnail }}" alt="Embed Thumbnail" class="thumbnail rounded-lg">
+                            @endif
+                        </div>
+                    @endif
+
+                    @if($title = data_get($embed, 'title'))
+                        {!! $link($url = data_get($embed, 'url'), <<<HTML
+                            <h3 class="font-bold text-lg break-words mb-0">$title</h3>
+                        HTML) !!}
+                    @endif
+
+                    @if($description = data_get($embed, 'description'))
+                        <p class="break-words description mt-0">{!! nl2br($description) !!}</p>
+                    @endif
+
+                    @if($fields = data_get($embed, 'fields'))
+                        <div class="mt-2 w-full">
+                            @foreach($fields as $field)
+                                <div class="mb-2 w-full">
+                                    <strong class="break-words mt-2">{{ data_get($field, 'name') }}</strong>
+                                    <span class="break-words field-value">{{ data_get($field, 'value') }}</span>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
+
+                    @if($image = data_get($embed, 'image.url'))
+                        <img src="{{ $image }}" alt="Embed Image" class="object-contain mt-3 w-full">
+                    @endif
+
+                    @if($footer_text || $footer_timestamp)
+                        <div class="flex items-center text-sm mt-4 footer">
+                            @if($footer_icon_url)
+                                <img src="{{ $footer_icon_url }}" alt="Footer Icon" class="w-5 h-5 rounded-full mr-2 object-cover footer-icon">
+                            @endif
+
+                            @if($footer_text || $footer_timestamp)
+                                <div class="flex space-x-1">
+                                    @if($footer_text)
+                                        <p class="break-words">{!! nl2br($footer_text) !!}</p>
+                                    @endif
+
+                                    @if($footer_timestamp)
+                                        <span class="timestamp">
+                                            @if($footer_text)
+                                                <span class="spacer">•</span>
+                                            @endif
+                                            {{ $footer_timestamp }}
+                                        </span>
+                                    @endif
+                                </div>
+                            @endif
+                        </div>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    </div>
+</x-filament-widgets::widget>
