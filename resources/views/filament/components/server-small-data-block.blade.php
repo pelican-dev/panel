17555a1d0973fcfbdac971dfc6553c665ad1f220
Boy132 <Boy132@users.noreply.github.com>
2025-05-27 19:30:07 +0200

    Make server name and server address clickable (and copyable) (#1395)

--- resources/views/filament/components/server-small-data-block.blade.php
+++ resources/views/filament/components/server-small-data-block.blade.php
@@ -1,9 +1,14 @@
-<div
-    class="grid grid-flow-row w-full p-3 rounded-lg bg-white shadow-sm overflow-hidden ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10">
+<div class="fi-small-stat-block grid grid-flow-row w-full p-3 rounded-lg bg-white shadow-sm overflow-hidden overflow-x-auto ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10">
+@if ($shouldCopyOnClick())
+    <span class="cursor-pointer" wire:click="copyClick('{{ $getValue() }}')">
+@else
     <span>
+@endif
         <span class="text-md font-medium text-gray-500 dark:text-gray-400">
             {{ $getLabel() }}
         </span>
-        <span class="text-md font-semibold">{{ $getValue() }}</span>
+        <span class="text-md font-semibold">
+            {{ $getValue() }}
+        </span>
     </span>
 </div>
