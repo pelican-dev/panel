a4109cad9de07c00d3f1fc7eb3966bfe87856cd6
Boy132 <mail@boy132.de>
2025-07-04 11:15:08 +0200

    add phpdocs to Plugins facade

--- app/Facades/Plugins.php
+++ app/Facades/Plugins.php
@@ -2,9 +2,23 @@
 
 namespace App\Facades;
 
+use App\Models\Plugin;
 use Illuminate\Support\Facades\Facade;
 use App\Services\Helpers\PluginService;
+use Filament\Panel;
 
+/**
+ * @method static void loadPlugins()
+ * @method static void loadPanelPlugins(Panel $panel)
+ * @method static void requireComposerPackages(Plugin $plugin)
+ * @method static void runPluginMigrations(Plugin $plugin)
+ * @method static void installPlugin(Plugin $plugin)
+ * @method static void enablePlugin(string|Plugin $plugin)
+ * @method static void disablePlugin(string|Plugin $plugin)
+ * @method static void updateLoadOrder(array $order)
+ *
+ * @see \App\Services\Helpers\PluginService
+ */
 class Plugins extends Facade
 {
     protected static function getFacadeAccessor(): string
