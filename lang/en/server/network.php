9f13c194fd416db051c29dcf6db1552ff344a72e
notCharles <charles@pelican.dev>
2025-07-22 20:10:42 -0400

    Network

--- lang/en/server/network.php
+++ lang/en/server/network.php
@@ -1,1 +1,15 @@
 <?php
+
+return [
+    'title' => 'Network',
+    'add' => 'Add Allocation',
+    'limit' => 'Allocation limit reached',
+    'address' => 'Address',
+    'port' => 'Port',
+    'notes' => 'Notes',
+    'no_notes' => 'No Notes',
+    'primary' => 'Primary',
+    'already' => 'Already',
+    'make' => 'Make',
+    'delete' => 'Delete',
+];
