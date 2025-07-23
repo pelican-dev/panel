7758ea2a5445fc235083203a1f840614162e3e67
notCharles <charles@pelican.dev>
2025-07-22 20:54:27 -0400

    Startup

--- app/Filament/Components/Forms/Actions/PreviewStartupAction.php
+++ app/Filament/Components/Forms/Actions/PreviewStartupAction.php
@@ -12,7 +12,7 @@ class PreviewStartupAction extends Action
 {
     public static function getDefaultName(): ?string
     {
-        return 'preview';
+        return trans('server/startup.preview');
     }
 
     protected function setUp(): void
