977129e24ed5d03fcf2da1c1d2408e39bc34b20d
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-08-30 12:05:48 +0000

    Use admin panel not app

--- app/Livewire/Installer/PanelInstaller.php
+++ app/Livewire/Installer/PanelInstaller.php
@@ -2,7 +2,6 @@
 
 namespace App\Livewire\Installer;
 
-use App\Filament\Admin\Pages\Dashboard;
 use App\Livewire\Installer\Steps\CacheStep;
 use App\Livewire\Installer\Steps\DatabaseStep;
 use App\Livewire\Installer\Steps\EnvironmentStep;
@@ -14,6 +13,7 @@ use App\Services\Users\UserCreationService;
 use App\Traits\CheckMigrationsTrait;
 use App\Traits\EnvironmentWriterTrait;
 use Exception;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Concerns\InteractsWithForms;
@@ -106,7 +106,7 @@ class PanelInstaller extends SimplePage implements HasForms
             $this->writeToEnv('env_session');
 
             // Redirect to admin panel
-            $this->redirect(Dashboard::getUrl(panel: 'app'));
+            $this->redirect(Filament::getPanel('admin')->getUrl());
         } catch (Halt) {
         }
     }
