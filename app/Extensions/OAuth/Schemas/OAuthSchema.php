179f174107cfb57562ce7fe62c84e439dd5766bf
Boy132 <mail@boy132.de>
2025-09-04 11:45:44 +0200

    update columnSpan for oauth toggles

--- app/Extensions/OAuth/Schemas/OAuthSchema.php
+++ app/Extensions/OAuth/Schemas/OAuthSchema.php
@@ -57,7 +57,7 @@ abstract class OAuthSchema implements OAuthSchemaInterface
                 ->default(env("OAUTH_{$id}_CLIENT_SECRET")),
             Toggle::make("OAUTH_{$id}_SHOULD_CREATE_MISSING_USERS")
                 ->label(trans('admin/setting.oauth.create_missing_users'))
-                ->columnSpanFull()
+                ->columnSpan(2)
                 ->inline(false)
                 ->onIcon('tabler-check')
                 ->offIcon('tabler-x')
@@ -68,7 +68,7 @@ abstract class OAuthSchema implements OAuthSchemaInterface
                 ->default(env("OAUTH_{$id}_SHOULD_CREATE_MISSING_USERS")),
             Toggle::make("OAUTH_{$id}_SHOULD_LINK_MISSING_USERS")
                 ->label(trans('admin/setting.oauth.link_missing_users'))
-                ->columnSpanFull()
+                ->columnSpan(2)
                 ->inline(false)
                 ->onIcon('tabler-check')
                 ->offIcon('tabler-x')
