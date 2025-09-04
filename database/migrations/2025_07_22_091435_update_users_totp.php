e7f945885d6cd743e73fb4dab3d57699b17c897f
Boy132 <mail@boy132.de>
2025-09-04 09:53:59 +0200

    remove db transaction in failing migration

--- database/migrations/2025_07_22_091435_update_users_totp.php
+++ database/migrations/2025_07_22_091435_update_users_totp.php
@@ -3,7 +3,6 @@
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,30 +12,28 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::transaction(function () {
-            Schema::table('users', function (Blueprint $table) {
-                $table->text('mfa_app_secret')->nullable();
-                $table->text('mfa_app_recovery_codes')->nullable();
-                $table->boolean('mfa_email_enabled')->default(false);
-            });
-
-            $users = User::all();
-            foreach ($users as $user) {
-                $user->update([
-                    'mfa_app_secret' => $user->use_totp ? $user->totp_secret : null,
-                    'mfa_app_recovery_codes' => null,
-                    'mfa_email_enabled' => false,
-                ]);
-            }
-
-            Schema::table('users', function (Blueprint $table) {
-                $table->dropColumn('use_totp');
-                $table->dropColumn('totp_secret');
-                $table->dropColumn('totp_authenticated_at');
-            });
-
-            Schema::dropIfExists('recovery_tokens');
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('mfa_app_secret')->nullable();
+            $table->text('mfa_app_recovery_codes')->nullable();
+            $table->boolean('mfa_email_enabled')->default(false);
         });
+
+        $users = User::all();
+        foreach ($users as $user) {
+            $user->update([
+                'mfa_app_secret' => $user->use_totp ? $user->totp_secret : null,
+                'mfa_app_recovery_codes' => null,
+                'mfa_email_enabled' => false,
+            ]);
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('use_totp');
+            $table->dropColumn('totp_secret');
+            $table->dropColumn('totp_authenticated_at');
+        });
+
+        Schema::dropIfExists('recovery_tokens');
     }
 
     /**
