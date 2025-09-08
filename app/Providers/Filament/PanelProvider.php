690b06f655ed56da4757815232b97f8029d3e738
Boy132 <mail@boy132.de>
2025-09-08 11:49:11 +0200

    prepare mfa requirement

--- app/Providers/Filament/PanelProvider.php
+++ app/Providers/Filament/PanelProvider.php
@@ -6,6 +6,7 @@ use App\Enums\CustomizationKey;
 use App\Filament\Pages\Auth\Login;
 use App\Filament\Pages\Auth\EditProfile;
 use App\Http\Middleware\LanguageMiddleware;
+use App\Models\User;
 use Filament\Actions\Action;
 use Filament\Auth\MultiFactor\App\AppAuthentication;
 use Filament\Auth\MultiFactor\Email\EmailAuthentication;
@@ -46,6 +47,30 @@ abstract class PanelProvider extends BasePanelProvider
                 AppAuthentication::make()->recoverable(),
                 EmailAuthentication::make(),
             ])
+            ->requiresMultiFactorAuthentication(function () {
+                /** @var ?User $user */
+                $user = auth()->user(); // TODO: get user, see https://github.com/filamentphp/filament/discussions/17695
+                if ($user) {
+                    $level = (int) config('panel.auth.2fa_required');
+
+                    // Not required
+                    if ($level === 0) {
+                        return false;
+                    }
+
+                    // Only admins
+                    if ($level === 1) {
+                        return $user->isAdmin();
+                    }
+
+                    // All users
+                    if ($level === 2) {
+                        return true;
+                    }
+                }
+
+                return false;
+            })
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
