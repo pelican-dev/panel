dbdb1b24f7e6d84e785afa97b6a1c26d8c57790f
JoanFo1456 <161775222+JoanFo1456@users.noreply.github.com>
2025-09-13 13:48:22 +0000

    Pint

--- app/Models/WebhookConfiguration.php
+++ app/Models/WebhookConfiguration.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Livewire\Features\SupportEvents\HandlesEvents;
