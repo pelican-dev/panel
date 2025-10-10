a7c0659a27f4893243ced401805e180b11164551
Boy132 <mail@boy132.de>
2025-10-10 08:46:13 +0200

    always enable topbar on app panel because we have no navigation here

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -16,6 +16,7 @@ class AppPanelProvider extends PanelProvider
             ->default()
             ->breadcrumbs(false)
             ->navigation(false)
+            ->topbar(true)
             ->userMenuItems([
                 Action::make('to_admin')
                     ->label(trans('profile.admin'))
