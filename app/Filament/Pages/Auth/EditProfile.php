d735e858a223aeb2484d6a527925b48648dec591
MartinOscar <40749467+rmartinoscar@users.noreply.github.com>
2025-10-13 00:58:22 +0200

    Rename Create actions in EditProfile (#1804)

--- app/Filament/Pages/Auth/EditProfile.php
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -236,7 +236,7 @@ class EditProfile extends BaseEditProfile
                                                     ->columnSpanFull(),
                                             ])
                                             ->headerActions([
-                                                Action::make('create')
+                                                Action::make('create_api_key')
                                                     ->label(trans('filament-actions::create.single.modal.actions.create.label'))
                                                     ->disabled(fn (Get $get) => empty($get('description')))
                                                     ->successRedirectUrl(self::getUrl(['tab' => 'api-keys::data::tab'], panel: 'app'))
@@ -325,7 +325,7 @@ class EditProfile extends BaseEditProfile
                                                 ->live(),
                                         ])
                                         ->headerActions([
-                                            Action::make('create')
+                                            Action::make('create_ssh_key')
                                                 ->label(trans('filament-actions::create.single.modal.actions.create.label'))
                                                 ->disabled(fn (Get $get) => empty($get('name')) || empty($get('public_key')))
                                                 ->successRedirectUrl(self::getUrl(['tab' => 'ssh-keys::data::tab'], panel: 'app'))
