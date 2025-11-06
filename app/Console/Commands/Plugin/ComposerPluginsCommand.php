7f74afcd6f08a79814b9761f5cc35a860b22fb15
Boy132 <mail@boy132.de>
2025-11-06 12:55:35 +0100

    revert change
    not needed

--- app/Console/Commands/Plugin/ComposerPluginsCommand.php
+++ app/Console/Commands/Plugin/ComposerPluginsCommand.php
@@ -4,24 +4,17 @@ namespace App\Console\Commands\Plugin;
 
 use App\Facades\Plugins;
 use App\Models\Plugin;
-use App\Traits\Commands\RequiresDatabaseMigrations;
 use Exception;
 use Illuminate\Console\Command;
 
 class ComposerPluginsCommand extends Command
 {
-    use RequiresDatabaseMigrations;
-
     protected $signature = 'p:plugin:composer';
 
     protected $description = 'Runs "composer require" on all installed plugins.';
 
     public function handle(): void
     {
-        if (!$this->hasCompletedMigrations()) {
-            return;
-        }
-
         $plugins = Plugin::all();
         foreach ($plugins as $plugin) {
             if (!$plugin->shouldLoad()) {
