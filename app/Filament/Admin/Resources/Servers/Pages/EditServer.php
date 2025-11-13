1a66b3fab466723ac1a34e5d05dd91341d9008b8
Boy132 <Boy132@users.noreply.github.com>
2025-11-13 19:05:23 +0100

    Encode file contents to utf-8 (#1896)

--- app/Filament/Admin/Resources/Servers/Pages/EditServer.php
+++ app/Filament/Admin/Resources/Servers/Pages/EditServer.php
@@ -155,7 +155,9 @@ class EditServer extends EditRecord
                                                     ->hiddenLabel()
                                                     ->formatStateUsing(function (Server $server, DaemonServerRepository $serverRepository) {
                                                         try {
-                                                            return $serverRepository->setServer($server)->getInstallLogs();
+                                                            $logs = $serverRepository->setServer($server)->getInstallLogs();
+
+                                                            return mb_convert_encoding($logs, 'UTF-8', ['UTF-8', 'UTF-16', 'ISO-8859-1', 'Windows-1252', 'ASCII']);
                                                         } catch (ConnectionException) {
                                                             Notification::make()
                                                                 ->title(trans('admin/server.notifications.error_connecting', ['node' => $server->node->name]))
