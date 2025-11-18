d03ba6a1c46c54b3a2ab576e8db4edf4c810150e
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-11-18 23:49:52 +0000

    Oups

--- app/Filament/Server/Pages/Settings.php
+++ app/Filament/Server/Pages/Settings.php
@@ -277,7 +277,7 @@ class Settings extends ServerFormPage
 
     public function updateDescription(string $description, Server $server): void
     {
-        abort_unless(user()?->can(Permission::ACTION_SETTINGS_RENAME, $server) && config('panel.editable_server_descriptions'), 403);
+        abort_unless(user()?->can(Permission::ACTION_SETTINGS_DESCRIPTION, $server) && config('panel.editable_server_descriptions'), 403);
 
         $original = $server->description;
 
