e27757a1c43084965fcde23019d014b81c544585
notCharles <charles@pelican.dev>
2025-11-18 17:00:41 -0500

    Move this

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,13 +26,6 @@ use App\Services\Helpers\SoftwareVersionService;
 use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
-use Filament\Forms\View\FormsIconAlias;
-use Filament\Notifications\View\NotificationsIconAlias;
-use Filament\Schemas\View\SchemaIconAlias;
-use Filament\Support\Facades\FilamentIcon;
-use Filament\Support\View\SupportIconAlias;
-use Filament\Tables\View\TablesIconAlias;
-use Filament\View\PanelsIconAlias;
 use Illuminate\Config\Repository;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Foundation\Application;
@@ -134,44 +127,5 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         Scramble::ignoreDefaultRoutes();
-
-        FilamentIcon::register([
-            PanelsIconAlias::USER_MENU_LOGOUT_BUTTON => 'tabler-logout-2',
-            PanelsIconAlias::USER_MENU_PROFILE_ITEM => 'tabler-user',
-            PanelsIconAlias::THEME_SWITCHER_LIGHT_BUTTON => 'tabler-sun',
-            PanelsIconAlias::THEME_SWITCHER_DARK_BUTTON => 'tabler-moon',
-            PanelsIconAlias::THEME_SWITCHER_SYSTEM_BUTTON => 'tabler-device-desktop',
-            PanelsIconAlias::SIDEBAR_OPEN_DATABASE_NOTIFICATIONS_BUTTON => 'tabler-bell',
-            PanelsIconAlias::TOPBAR_OPEN_DATABASE_NOTIFICATIONS_BUTTON => 'tabler-bell',
-            PanelsIconAlias::GLOBAL_SEARCH_FIELD => 'tabler-search',
-            PanelsIconAlias::SIDEBAR_EXPAND_BUTTON => 'tabler-arrow-right-dashed',
-            PanelsIconAlias::SIDEBAR_COLLAPSE_BUTTON => 'tabler-arrow-left-dashed',
-
-            TablesIconAlias::ACTIONS_FILTER => 'tabler-filters',
-            TablesIconAlias::SEARCH_FIELD => 'tabler-search',
-            TablesIconAlias::ACTIONS_COLUMN_MANAGER => 'tabler-columns',
-            TablesIconAlias::ACTIONS_OPEN_BULK_ACTIONS => 'tabler-box-multiple',
-
-            NotificationsIconAlias::DATABASE_MODAL_EMPTY_STATE => 'tabler-bell-off',
-            NotificationsIconAlias::NOTIFICATION_CLOSE_BUTTON => 'tabler-x',
-            NotificationsIconAlias::NOTIFICATION_INFO => 'tabler-info-circle',
-            NotificationsIconAlias::NOTIFICATION_SUCCESS => 'tabler-check-circle',
-            NotificationsIconAlias::NOTIFICATION_WARNING => 'tabler-alert-triangle',
-            NotificationsIconAlias::NOTIFICATION_DANGER => 'tabler-alert-circle',
-
-            SupportIconAlias::MODAL_CLOSE_BUTTON => 'tabler-x',
-            SupportIconAlias::BREADCRUMBS_SEPARATOR => 'tabler-chevrons-right',
-            SupportIconAlias::PAGINATION_NEXT_BUTTON => 'tabler-arrow-right',
-            SupportIconAlias::PAGINATION_PREVIOUS_BUTTON => 'tabler-arrow-left',
-            SupportIconAlias::SECTION_COLLAPSE_BUTTON => 'tabler-arrow-up',
-
-            FormsIconAlias::COMPONENTS_KEY_VALUE_ACTIONS_DELETE => 'tabler-trash',
-            FormsIconAlias::COMPONENTS_REPEATER_ACTIONS_DELETE => 'tabler-trash',
-            FormsIconAlias::COMPONENTS_REPEATER_ACTIONS_EXPAND => 'tabler-arrow-down',
-            FormsIconAlias::COMPONENTS_REPEATER_ACTIONS_COLLAPSE => 'tabler-arrow-up',
-            FormsIconAlias::COMPONENTS_REPEATER_ACTIONS_REORDER => 'tabler-arrows-sort',
-
-            SchemaIconAlias::COMPONENTS_WIZARD_COMPLETED_STEP => 'tabler-check',
-        ]);
     }
 }
