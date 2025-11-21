71bb2c5b1e911f33266724ef90b76aee5c518894
Charles <charles@pelican.dev>
2025-11-21 12:22:59 -0500

    update comment

--- app/Filament/Server/Pages/Settings.php
+++ app/Filament/Server/Pages/Settings.php
@@ -144,7 +144,7 @@ class Settings extends ServerFormPage
                                                                             ],
                                                                         ]);
 
-                                                                        $imageContent = @file_get_contents($state, false, $context, 0, 256000); // 1024KB
+                                                                        $imageContent = @file_get_contents($state, false, $context, 0, 256000); //256KB
 
                                                                         if (!$imageContent) {
                                                                             throw new \Exception(trans('admin/egg.import.image_error'));
