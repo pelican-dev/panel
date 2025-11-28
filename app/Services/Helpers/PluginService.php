a37adc94bc0ded6b8317068910b4264b54ff88e8
UndercoverNL <mail@undercovernl.com>
2025-11-28 17:04:19 +0100

    fix: child folder detection in ZIP for plugin extraction
    `locateName` does not support directory detection, fixed with new changes

--- app/Services/Helpers/PluginService.php
+++ app/Services/Helpers/PluginService.php
@@ -179,7 +179,7 @@ class PluginService
         }
 
         $oldPackages = collect($oldPackages)
-            ->filter(fn ($version, $package) => !array_key_exists($package, $newPackages))
+            ->filter(fn($version, $package) => !array_key_exists($package, $newPackages))
             ->keys()
             ->unique()
             ->toArray();
@@ -192,7 +192,7 @@ class PluginService
         }
 
         $newPackages = collect($newPackages)
-            ->map(fn ($version, $package) => "$package:$version")
+            ->map(fn($version, $package) => "$package:$version")
             ->flatten()
             ->unique()
             ->toArray();
@@ -338,7 +338,12 @@ class PluginService
             File::deleteDirectory(plugin_path($pluginName));
         }
 
-        $extractPath = $zip->locateName($pluginName . '/') !== false ? base_path('plugins') : plugin_path($pluginName);
+        $extractPath = (bool) array_filter(
+            array_map(fn($i) => $zip->getNameIndex($i), range(0, $zip->numFiles - 1)),
+            fn($name) => str_starts_with($name, $pluginName)
+        )
+            ? base_path('plugins')
+            : plugin_path($pluginName);
 
         if (!$zip->extractTo($extractPath)) {
             $zip->close();
@@ -442,7 +447,7 @@ class PluginService
                 continue;
             }
 
-            $languages = array_merge($languages, collect(File::directories(plugin_path($plugin->id, 'lang')))->map(fn ($path) => basename($path))->toArray());
+            $languages = array_merge($languages, collect(File::directories(plugin_path($plugin->id, 'lang')))->map(fn($path) => basename($path))->toArray());
         }
 
         return array_unique($languages);
