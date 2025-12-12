f19461a6c7d30fe66e468c208af2a1c57f3e48ce
notCharles <charles@pelican.dev>
2025-12-12 17:59:42 -0500

    move run now button
    the edit view is the first view and view requires a url edit to see the run now button..

--- app/Filament/Server/Resources/Schedules/Pages/EditSchedule.php
+++ app/Filament/Server/Resources/Schedules/Pages/EditSchedule.php
@@ -2,15 +2,19 @@
 
 namespace App\Filament\Server\Resources\Schedules\Pages;
 
+use App\Enums\ScheduleStatus;
+use App\Enums\SubuserPermission;
 use App\Facades\Activity;
 use App\Filament\Components\Actions\ExportScheduleAction;
 use App\Filament\Server\Resources\Schedules\ScheduleResource;
 use App\Models\Schedule;
+use App\Services\Schedules\ProcessScheduleService;
 use App\Traits\Filament\CanCustomizeHeaderActions;
 use App\Traits\Filament\CanCustomizeHeaderWidgets;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
 use Filament\Actions\DeleteAction;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Support\Enums\IconSize;
 
@@ -48,6 +52,21 @@ class EditSchedule extends EditRecord
     protected function getDefaultHeaderActions(): array
     {
         return [
+            Action::make('run_now')
+                ->authorize(fn () => user()?->can(SubuserPermission::ScheduleUpdate, Filament::getTenant()))
+                ->label(fn (Schedule $schedule) => $schedule->tasks->count() === 0 ? trans('server/schedule.no_tasks') : ($schedule->status === ScheduleStatus::Processing ? ScheduleStatus::Processing->getLabel() : trans('server/schedule.run_now')))
+                ->color(fn (Schedule $schedule) => $schedule->tasks->count() === 0 || $schedule->status === ScheduleStatus::Processing ? 'warning' : 'primary')
+                ->disabled(fn (Schedule $schedule) => $schedule->tasks->count() === 0 || $schedule->status === ScheduleStatus::Processing)
+                ->action(function (ProcessScheduleService $service, Schedule $schedule) {
+                    $service->handle($schedule, true);
+
+                    Activity::event('server:schedule.execute')
+                        ->subject($schedule)
+                        ->property('name', $schedule->name)
+                        ->log();
+
+                    $this->fillForm();
+                }),
             DeleteAction::make()
                 ->hiddenLabel()
                 ->iconButton()->iconSize(IconSize::ExtraLarge)
