375bcc3c3742cc6d8faa37239b5e7c026ffb6ca0
Charles <charles@pelican.dev>
2025-12-29 08:35:46 -0500

    add missing import

--- app/Jobs/InstallEgg.php
+++ app/Jobs/InstallEgg.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class InstallEgg implements ShouldQueue
