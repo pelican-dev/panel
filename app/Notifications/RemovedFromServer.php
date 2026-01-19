d9f272e048cd01c60294c83581946bfbe7a883f4
Lance Pioch <git@lance.sh>
2026-01-19 12:17:48 -0500

    auto-claude: subtask-1-4 - Add locale detection to RemovedFromServer notification

--- app/Notifications/RemovedFromServer.php
+++ app/Notifications/RemovedFromServer.php
@@ -21,6 +21,11 @@ class RemovedFromServer extends Notification implements ShouldQueue
         return ['mail'];
     }
 
+    public function locale(User $notifiable): string
+    {
+        return $notifiable->language ?? 'en';
+    }
+
     public function toMail(User $notifiable): MailMessage
     {
         return (new MailMessage())
