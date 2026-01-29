88ae5e26e5183dd87fa78f74712d6a9e38ab4ec6
Frogperson <frgprsn@gmail.com>
2026-01-29 13:16:07 -0500

    Add "Daemon Base Directory" field to the node edit and creation pages.

--- app/Filament/Admin/Resources/Nodes/Pages/EditNode.php
+++ app/Filament/Admin/Resources/Nodes/Pages/EditNode.php
@@ -329,12 +329,23 @@ class EditNode extends EditRecord
                         ->required()
                         ->minValue(1)
                         ->suffix(config('panel.use_binary_prefix') ? 'MiB' : 'MB'),
+                   TextInput::make('daemon_base')
+                        ->label(trans('admin/node.daemon_base'))
+                        ->hintIcon(TablerIcon::QuestionMark, trans('admin/node.daemon_base_help'))
+                        ->columnSpan([
+                            'default' => 1,
+                            'sm' => 1,
+                            'md' => 2,
+                            'lg' => 3,
+                        ])
+                        ->required()
+                        ->rule('regex:/^([\/][\d\w.\-\/]+)$/'),
                     TextInput::make('daemon_sftp')
                         ->columnSpan([
                             'default' => 1,
                             'sm' => 1,
-                            'md' => 1,
-                            'lg' => 3,
+                            'md' => 2,
+                            'lg' => 1,
                         ])
                         ->label(trans('admin/node.sftp_port'))
                         ->minValue(1)
@@ -346,8 +357,8 @@ class EditNode extends EditRecord
                         ->columnSpan([
                             'default' => 1,
                             'sm' => 1,
-                            'md' => 1,
-                            'lg' => 3,
+                            'md' => 2,
+                            'lg' => 2,
                         ])
                         ->label(trans('admin/node.sftp_alias'))
                         ->helperText(trans('admin/node.sftp_alias_help')),
@@ -356,7 +367,7 @@ class EditNode extends EditRecord
                             'default' => 1,
                             'sm' => 1,
                             'md' => 1,
-                            'lg' => 3,
+                            'lg' => 2,
                         ])
                         ->label(trans('admin/node.use_for_deploy'))
                         ->inline()
@@ -374,7 +385,7 @@ class EditNode extends EditRecord
                             'default' => 1,
                             'sm' => 1,
                             'md' => 1,
-                            'lg' => 3,
+                            'lg' => 2,
                         ])
                         ->label(trans('admin/node.maintenance_mode'))
                         ->inline()
