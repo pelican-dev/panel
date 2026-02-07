d87aa2dba79179c72d7a7ce1db345f1ddf1feb4f
JoanFo <161775222+JoanFo1456@users.noreply.github.com>
2026-02-07 16:29:04 +0100

    Apply suggestion from @lancepioch
    Co-authored-by: Lance Pioch <lancepioch@gmail.com>

--- routes/auth.php
+++ routes/auth.php
@@ -9,4 +9,5 @@ Route::prefix('oauth')->group(function () {
     Route::get('/redirect/{driver}', [OAuthController::class, 'redirect'])->name('auth.oauth.redirect');
     Route::get('/callback/{driver}', [OAuthController::class, 'callback'])->name('auth.oauth.callback')->withoutMiddleware('guest');
 });
+
 Route::passkeys();
