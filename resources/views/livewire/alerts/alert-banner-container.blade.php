fb0d7f44b99e54c276dc30446c4546a4119768bc
Lance Pioch <git@lance.sh>
2026-02-07 23:32:25 -0500

    Replace ineffective session polling with navigate-based refresh
    Session-based polling doesn't work because nothing replenishes session
    data during poll requests. Instead, listen for Livewire's navigated
    event to re-pull session data after SPA page transitions, which catches
    banners pushed by the new page's mount lifecycle.

--- resources/views/livewire/alerts/alert-banner-container.blade.php
+++ resources/views/livewire/alerts/alert-banner-container.blade.php
@@ -1,4 +1,9 @@
-<div wire:poll.visible.15s="pullFromSession" id="alert-banner-container" class="flex flex-col gap-4">
+<div
+    x-data
+    x-init="document.addEventListener('livewire:navigated', () => $wire.pullFromSession())"
+    id="alert-banner-container"
+    class="flex flex-col gap-4"
+>
     @foreach ($alertBanners as $alertBanner)
         {{ $alertBanner }}
     @endforeach
