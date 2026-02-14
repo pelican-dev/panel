b473069bee3eef28cbfa88ee0bd832a7959f54f0
Michael (Parker) Parker <parkervcp@gmail.com>
2026-02-13 21:38:49 -0500

    docker env fixes
    use shell check to validate entrypoint
    expand the loading of env vars for the install script
    entrypoint cleanup
    
    adds some security around loading env vars
    
    adds composer to the final docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,34 +1,48 @@
-#!/bin/ash -e
+#!/bin/ash
+# shellcheck shell=dash
+
 # check for .env file or symlink and generate app keys if missing
 if [ -f /var/www/html/.env ]; then
   echo "external vars exist."
   # load specific env vars from .env used in the entrypoint and they are not already set
-  for VAR in "APP_KEY" "APP_INSTALLED" "DB_CONNECTION" "DB_HOST" "DB_PORT"; do if ! (printenv | grep -q ${VAR}); then export $(grep ${VAR} .env | grep -ve "^#"); fi; done
+  for VAR in "APP_KEY" "APP_INSTALLED" "DB_CONNECTION" "DB_HOST" "DB_PORT"; do
+    echo "checking for ${VAR}"
+    ## skip if it looks like it might try to execute code
+    if (grep ${VAR} .env | grep -qE "\$\(|=\`|\$#"); then echo "var in .env may be executable or a comment, skipping"; continue; fi
+    # if the variable is in .env then set it
+    if (grep -q ${VAR} .env); then 
+      echo "${VAR} exists in .env loading"
+      export "$(grep ${VAR} .env | sed 's/"//g')"
+      continue
+    fi
+    ## variable wasn't loaded or in the env to set
+    echo "didn't find variable to set"
+  done
 else
   echo "external vars don't exist."
   # webroot .env is symlinked to this path
   touch /pelican-data/.env
 
   # manually generate a key because key generate --force fails
-  if [ -z ${APP_KEY} ]; then
-    echo -e "Generating key."
+  if [ "${APP_KEY:-}" ]; then
+    echo "Generating key."
     APP_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    echo -e "Generated app key: $APP_KEY"
-    echo -e "APP_KEY=$APP_KEY" > /pelican-data/.env
+    echo "Generated app key: $APP_KEY"
+    ech "APP_KEY=$APP_KEY" > /pelican-data/.env
   else
-    echo -e "APP_KEY exists in environment, using that."
-    echo -e "APP_KEY=$APP_KEY" > /pelican-data/.env
+    echo "APP_KEY exists in environment, using that."
+    echo "APP_KEY=$APP_KEY" > /pelican-data/.env
   fi
 
   # enable installer
-  echo -e "APP_INSTALLED=false" >> /pelican-data/.env
+  echo "APP_INSTALLED=false" >> /pelican-data/.env
 fi
 
 # create directories for volumes
 mkdir -p /pelican-data/database /pelican-data/storage/avatars /pelican-data/storage/fonts /pelican-data/storage/icons /pelican-data/plugins /var/www/html/storage/logs/supervisord 2>/dev/null
 
 # if the app is installed then we need to run migrations on start. New installs will run migrations when you run the installer.
-if [ "${APP_INSTALLED}" == "true" ];  then
+if [ "${APP_INSTALLED}" = "true" ];  then
   #if the db is anything but sqlite wait until it's accepting connections
   if [ "${DB_CONNECTION}" != "sqlite" ]; then
     # check for DB up before starting the panel
@@ -44,31 +58,31 @@ if [ "${APP_INSTALLED}" == "true" ];  then
   php artisan migrate --force
 fi
 
-echo -e "Optimizing Filament"
+echo "Optimizing Filament"
 php artisan filament:optimize
 
 # default to caddy not starting
 export SUPERVISORD_CADDY=false
-export PARSED_APP_URL=${APP_URL}
+export CADDY_APP_URL=${APP_URL}
 
 # checking if app url is using https
 if echo "${APP_URL}" | grep -qE '^https://'; then
   echo "https domain found setting email var"
-  export PARSED_LE_EMAIL="email ${LE_EMAIL}"
+  export CADDY_LE_EMAIL="email ${LE_EMAIL}"
 fi
 
 # when running behind a proxy
-if [ "${BEHIND_PROXY}" == "true" ]; then
+if [ "${BEHIND_PROXY}" = "true" ]; then
   echo "running behind proxy"
   echo "listening on port 80 internally"
-  export PARSED_LE_EMAIL=""
-  export PARSED_APP_URL=":80"
-  export PARSED_AUTO_HTTPS="auto_https off"
+  export CADDY_LE_EMAIL=""
+  export CADDY_APP_URL=":80"
+  export CADDY_AUTO_HTTPS="auto_https off"
   export ASSET_URL=${APP_URL}
 fi
 
 # disable caddy if SKIP_CADDY is set
-if [ "${SKIP_CADDY:-}" == "true" ]; then
+if [ "${SKIP_CADDY:-}" = "true" ]; then
   echo "Starting PHP-FPM only"
 else
   echo "Starting PHP-FPM and Caddy"
@@ -77,7 +91,8 @@ else
 
   # handle trusted proxies for caddy when variable has data
   if [ ! -z ${TRUSTED_PROXIES} ]; then
-    export CADDY_TRUSTED_PROXIES=$(echo "trusted_proxies static ${TRUSTED_PROXIES}" | sed 's/,/ /g')
+    FORMATED_PROXIES=$(echo "trusted_proxies static ${TRUSTED_PROXIES}" | sed 's/,/ /g')
+    export CADDY_TRUSTED_PROXIES=${FORMATED_PROXIES}
     export CADDY_STRICT_PROXIES="trusted_proxies_strict"
   fi
 fi
