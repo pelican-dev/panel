37eb6c0d05eb71e04950167f2a7ca0ff82b89e13
Lance Pioch <git@lance.sh>
2026-02-15 11:27:45 -0500

    Fix phpstan

--- app/Livewire/AlertBannerCollection.php
+++ app/Livewire/AlertBannerCollection.php
@@ -8,7 +8,7 @@ class AlertBannerCollection extends Collection
 {
     public static function fromLivewire($value): static
     {
-        return app(static::class, ['items' => $value])->transform(
+        return (new static($value))->transform(
             fn (array $alertBanner): AlertBanner => AlertBanner::fromArray($alertBanner),
         );
     }
