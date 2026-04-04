fde131db6272bd9ae0b94f696eddad59648533bc
Shift <shift@laravelshift.com>
2026-04-04 16:54:45 +0000

    Convert validation rules to array syntax

--- app/Http/Requests/Api/Remote/ReportBackupCompleteRequest.php
+++ app/Http/Requests/Api/Remote/ReportBackupCompleteRequest.php
@@ -10,13 +10,37 @@ class ReportBackupCompleteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'successful' => 'required|boolean',
-            'checksum' => 'nullable|string|required_if:successful,true',
-            'checksum_type' => 'nullable|string|required_if:successful,true',
-            'size' => 'nullable|numeric|required_if:successful,true',
-            'parts' => 'nullable|array',
-            'parts.*.etag' => 'required|string',
-            'parts.*.part_number' => 'required|numeric',
+            'successful' => [
+                'required',
+                'boolean',
+            ],
+            'checksum' => [
+                'nullable',
+                'string',
+                'required_if:successful,true',
+            ],
+            'checksum_type' => [
+                'nullable',
+                'string',
+                'required_if:successful,true',
+            ],
+            'size' => [
+                'nullable',
+                'numeric',
+                'required_if:successful,true',
+            ],
+            'parts' => [
+                'nullable',
+                'array',
+            ],
+            'parts.*.etag' => [
+                'required',
+                'string',
+            ],
+            'parts.*.part_number' => [
+                'required',
+                'numeric',
+            ],
         ];
     }
 }
