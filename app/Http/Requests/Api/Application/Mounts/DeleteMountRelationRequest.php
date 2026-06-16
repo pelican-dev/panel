2ac9e58483e00df13eb5d02f80eef5471325c460
Boy132 <mail@boy132.de>
2026-06-16 11:27:29 +0200

    add correct form requests for updating mount relations (eggs, nodes, servers)

---
+++ app/Http/Requests/Api/Application/Mounts/DeleteMountRelationRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests\Api\Application\Mounts;
+
+use App\Http\Requests\Api\Application\ApplicationApiRequest;
+use App\Models\Mount;
+use App\Services\Acl\Api\AdminAcl;
+
+class DeleteMountRelationRequest extends ApplicationApiRequest
+{
+    protected ?string $resource = Mount::RESOURCE_NAME;
+
+    protected int $permission = AdminAcl::WRITE;
+}
