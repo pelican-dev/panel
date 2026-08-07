2ef90f465b9c7cbdcd0d111a0f44459d4fed111a
Boy132 <mail@boy132.de>
2026-08-07 13:19:04 +0200

    fix invisible save button on CreateBackupHost page

--- app/Filament/Admin/Resources/BackupHosts/Pages/CreateBackupHost.php
+++ app/Filament/Admin/Resources/BackupHosts/Pages/CreateBackupHost.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Admin\Resources\BackupHosts\Pages;
 
+use App\Enums\TablerIcon;
 use App\Filament\Admin\Resources\BackupHosts\BackupHostResource;
 use App\Traits\Filament\CanCustomizeHeaderActions;
 use App\Traits\Filament\CanCustomizeHeaderWidgets;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBackupHost extends CreateRecord
@@ -15,4 +18,22 @@ class CreateBackupHost extends CreateRecord
     protected static string $resource = BackupHostResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    /** @return array<Action|ActionGroup> */
+    protected function getDefaultHeaderActions(): array
+    {
+        return [
+            Action::make('create')
+                ->hiddenLabel()
+                ->action('create')
+                ->keyBindings(['mod+s'])
+                ->tooltip(trans('filament-panels::resources/pages/create-record.form.actions.create.label'))
+                ->icon(TablerIcon::FilePlus),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 }
