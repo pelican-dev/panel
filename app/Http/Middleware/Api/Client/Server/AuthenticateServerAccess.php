fecda7974fe97dbf740328dfeb25fec93273cb91
lotus64 <lotus64santon@gmail.com>
2026-08-10 18:50:03 +0200

    fix: correctly bypass exceptions for admins on file routes

--- app/Http/Middleware/Api/Client/Server/AuthenticateServerAccess.php
+++ app/Http/Middleware/Api/Client/Server/AuthenticateServerAccess.php
@@ -50,8 +50,10 @@ class AuthenticateServerAccess
             // Still allow users to get information about their server if it is installing or
             // being transferred.
             if (!$request->routeIs('api:client:server.view')) {
-                throw_if(!$user->isAdmin() && ($server->isSuspended() || $server->node->isUnderMaintenance()) && !$request->routeIs('api:client:server.resources'), $exception);
-                throw_if($user->cannot('update server', $server) || !$request->routeIs($this->except), $exception);
+                $isExcepted = $request->routeIs($this->except) || ($user->isAdmin() && $request->is('api/client/servers/*/files*'));
+
+                throw_if(($server->isSuspended() || $server->node->isUnderMaintenance()) && !$request->routeIs('api:client:server.resources') && !$isExcepted, $exception);
+                throw_if($user->cannot('update server', $server) || !$isExcepted, $exception);
             }
         }
 
