b3486629b25c06682b73ab82435b149f50505fbe
Boy132 <mail@boy132.de>
2026-08-21 16:48:47 +0200

    also check return code of sub-commands

--- app/Console/Commands/Environment/AppSettingsCommand.php
+++ app/Console/Commands/Environment/AppSettingsCommand.php
@@ -19,31 +19,46 @@ class AppSettingsCommand extends Command
         $path = base_path('.env');
         if (!file_exists($path)) {
             $this->comment('Copying example .env file');
-            copy($path . '.example', $path);
+
+            if (!copy($path . '.example', $path)) {
+                return 1;
+            }
         }
 
         $appUrl = $this->option('url');
+
         if (blank($appUrl)) {
             $appUrl = $this->ask('Application URL', config('app.url'));
         }
+
         if (blank($appUrl)) {
             $this->error('Application URL is required.');
 
             return 1;
         }
+
         $this->comment('Writing APP_URL to .env file');
         $this->writeToEnvironment(['APP_URL' => $appUrl]);
 
         if (!config('app.key')) {
             $this->comment('Generating app key');
-            $this->call('key:generate');
+            $return = $this->call('key:generate');
+            if ($return !== 0) {
+                return $return;
+            }
         }
 
         $this->comment('Creating storage link');
-        $this->call('storage:link');
+        $return = $this->call('storage:link');
+        if ($return !== 0) {
+            return $return;
+        }
 
         $this->comment('Caching components & icons');
-        $this->call('filament:optimize');
+        $return = $this->call('filament:optimize');
+        if ($return !== 0) {
+            return $return;
+        }
 
         return 0;
     }
